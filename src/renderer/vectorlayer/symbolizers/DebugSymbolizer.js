eaac3cd928898af744250e15138c84ff49367e9e
fuzhen <fuzhen@maptalks.org>
2016-02-20 18:45:49 +0800

    draw outline in DebugSymbolizer

--- src/renderer/vectorlayer/symbolizers/DebugSymbolizer.js
+++ src/renderer/vectorlayer/symbolizers/DebugSymbolizer.js
@@ -26,22 +26,23 @@ Z.symbolizer.DebugSymbolizer = Z.symbolizer.PointSymbolizer.extend({
         if (!geometry.options['debug']) {
             return;
         }
-        /*var ext = geometry._getPainter().getPixelExtent();
-        var xmin = ext['xmin'],
-            ymin = ext['ymin'],
-            xmax = ext['xmax'],
-            ymax = ext['ymax'];
-        var points =[
-                new Z.Point(xmin, ymax), new Z.Point(xmax, ymax), new Z.Point(xmax, ymin),
-                new Z.Point(xmin, ymin), new Z.Point(xmin, ymax)
-            ];
-
-
-        Z.Canvas.path(ctx, points, op);*/
+         var map = this.getMap();
+        if (map._zooming) {
+            return;
+        }
         Z.Canvas.setDefaultCanvasSetting(ctx);
-        var points = this._getRenderContainerPoints();
         Z.Canvas.prepareCanvas(ctx, this.styles['stroke']);
         var op = this.styles['stroke']['stroke-opacity'];
+
+        //outline
+        var pixelExtent = geometry._getPainter().getPixelExtent(),
+        size = pixelExtent.getSize();
+        var nw = map.viewPointToContainerPoint(pixelExtent.getMin());
+        Z.Canvas.rectangle(ctx, nw, size, op, 0);
+
+        //center cross and id if have any.
+        var points = this._getRenderContainerPoints();
+
         var id = this.geometry.getId();
         var cross = Z.symbolizer.VectorMarkerSymbolizer._getVectorPoints('cross',10, 10);
         for (var i = 0; i < points.length; i++) {
@@ -55,7 +56,6 @@ Z.symbolizer.DebugSymbolizer = Z.symbolizer.PointSymbolizer.extend({
             }
             Z.Canvas.path(ctx,c.slice(0,2), op);
             Z.Canvas.path(ctx,c.slice(2,4), op);
-
         }
     }
 
