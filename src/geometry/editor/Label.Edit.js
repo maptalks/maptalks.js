f733f940dd8fe5043f88e36db81c11029f291477
FuZhenn <fuzhen@maptalks.org>
2016-07-02 11:14:39 +0800

    update Label.edit and add specs

--- src/geometry/editor/Label.Edit.js
+++ src/geometry/editor/Label.Edit.js
@@ -3,24 +3,34 @@ Z.Label.include(/** @lends maptalks.Label.prototype */{
      * Start to edit the label text
      * @return {maptalks.Label} this
      */
-    startEditText: function () {
+    startEditText: function() {
+        if (!this.getMap()) {
+            return;
+        }
         this.hide();
+        this.endEditText();
         this._prepareEditor();
+        this.fire('edittextstart', this);
         return this;
     },
 
     /**
      * End label text edit.
-     * @member maptalks.Label
      * @return {maptalks.Label} this
      */
-    endEditText: function () {
-        var content = this._textEditor.value;
-        this.setContent(content);
-        this.show();
-        Z.DomUtil.removeDomNode(this._container);
-        delete this._container;
-        delete this._textEditor;
+    endEditText: function() {
+        if(this._textEditor) {
+            this.getMap().off('click', this.endEditText, this);
+            var content = this._textEditor.innerText;
+            this.setContent(content);
+            this.show();
+            Z.DomUtil.off(this._textEditor, 'mousedown dblclick', Z.DomUtil.stopPropagation)
+                .off(this._textEditor, 'blur', this.endEditText, this);
+            Z.DomUtil.removeDomNode(this._container);
+            delete this._container;
+            delete this._textEditor;
+            this.fire('edittextend', this);
+        }
         return this;
     },
 
@@ -35,55 +45,68 @@ Z.Label.include(/** @lends maptalks.Label.prototype */{
         return false;
     },
 
+    getEditor: function() {
+        return this._textEditor;
+    },
+
     _prepareEditor:function () {
-        var map = this.getMap();
-        var zIndex = map._panels.control.style.zIndex + 1;
-        var viewPoint = this._computeViewPoint();
+        var map = this.getMap(),
+            zIndex = map._panels.control.style.zIndex + 1,
+            viewPoint = this._computeViewPoint();
         this._container = Z.DomUtil.createEl('div');
         this._container.style.cssText = 'position:absolute;top:' + viewPoint['y']
                                     + 'px;left:' + viewPoint['x'] + 'px;z-index:' + zIndex + ';';
         map._panels.ui.appendChild(this._container);
-        this._textEditor = this._createInputDom();
+        this._textEditor = this._createEditor();
         this._container.appendChild(this._textEditor);
     },
 
+
     _computeViewPoint: function () {
-        var map = this.getMap();
-        var symbol = this._getInternalSymbol();
-        var labelSize = this.getSize();
-        var dx = Z.Util.getValueOrDefault(symbol['textDx'], 0),
-            dy = Z.Util.getValueOrDefault(symbol['textDy'], 0);
-        var align = Z.StringUtil.getAlignPoint(labelSize, symbol['textHorizontalAlignment'], symbol['textVerticalAlignment'])
-                    .add(new Z.Point(dx, dy));
-        var viewPoint = map.coordinateToViewPoint(this.getCenter()).add(align);
+        var map = this.getMap(),
+            symbol = this._getInternalSymbol(),
+            labelSize = this.getSize(),
+            dx = symbol['textDx'] || 0,
+            dy = symbol['textDy'] || 0,
+            align = Z.StringUtil.getAlignPoint(labelSize, symbol['textHorizontalAlignment'], symbol['textVerticalAlignment'])
+                    .add(dx, dy),
+            viewPoint = map.coordinateToViewPoint(this.getCenter()).add(align);
         return viewPoint;
     },
 
-    _createInputDom: function () {
-        var labelSize = this.getSize();
-        var symbol = this._getInternalSymbol();
-        var width = labelSize['width'];
-        var height = labelSize['height'];
-        var textColor = symbol['textFill'];
-        var textSize = symbol['textSize'];
-        var fill = symbol['markerFill'];
-        var lineColor = symbol['markerLineColor'];
-        var spacing = Z.Util.getValueOrDefault(symbol['textLineSpacing'], 0);
-        var inputDom = Z.DomUtil.createEl('textarea');
-        inputDom.style.cssText = 'background:' + fill + ';' +
-            'border:1px solid ' + lineColor + ';' +
-            'color:' + textColor + ';' +
-            'font-size:' + textSize + 'px;' +
-            'width:' + (width - spacing) + 'px;' +
-            'height:' + (height - spacing) + 'px;';
+    _createEditor: function () {
+        var labelSize = this.getSize(),
+            symbol = this._getInternalSymbol() || {},
+            width = labelSize['width'] || 100,
+            height = labelSize['height'] || 100,
+            textColor = symbol['textFill'] || '#000000',
+            textSize = symbol['textSize'] || 12,
+            fill = symbol['markerFill'] || '#3398CC',
+            lineColor = symbol['markerLineColor'] || '#ffffff',
+            spacing = symbol['textLineSpacing'] || 0,
+            editor = Z.DomUtil.createEl('div');
+        editor.contentEditable = true;
+        editor.style.cssText ='background: '+fill+';'+
+            'border: 1px solid '+lineColor+';'+
+            'color: '+textColor+';'+
+            'font-size: '+textSize+'px;'+
+            'width: '+(width + 10)+'px;'+
+            // 'height: '+(height - spacing) +'px;'+
+            // 'min-height: '+(height - spacing)+'px;'+
+            // 'max-height: 300px;'+
+            'margin-left: auto;'+
+            'margin-right: auto;'+
+            'line-height: '+(textSize+spacing)+'px;'+
+            'outline: 0;'+
+            'word-wrap: break-word;'+
+            'overflow-x: hidden;'+
+            'overflow-y: auto;'+
+            '-webkit-user-modify: read-write-plaintext-only;';
         var content = this.getContent();
-        inputDom.value = content;
-        var me = this;
-        Z.DomUtil.on(inputDom, 'blur', function (param) {
-            me.endEditText();
-        });
-        return inputDom;
-
+        editor.innerText = content;
+        Z.DomUtil.on(editor, 'mousedown dblclick', Z.DomUtil.stopPropagation)
+            .on(editor, 'blur', this.endEditText, this);
+        return editor;
     }
 
 });
---
+++ test/geometry/edit/LabelEditSpec.js
@@ -0,0 +1,91 @@
+describe('#LabelEdit', function () {
+    var container, eventContainer;
+    var map;
+    var center = new Z.Coordinate(118.846825, 32.046534);
+    var layer;
+    function getLabel() {
+        var label = new maptalks.Label('I am a Text', map.getCenter()).addTo(layer);
+        return label;
+    }
+
+    beforeEach(function() {
+        var setups = commonSetupMap(center, null);
+        container = setups.container;
+        map = setups.map;
+        map.config('panAnimation', false);
+        eventContainer = map._panels.canvasContainer;
+        layer = new Z.VectorLayer('id', {'drawImmediate' : true});
+        map.addLayer(layer);
+    });
+
+    afterEach(function() {
+        removeContainer(container)
+    });
+
+    describe('edit label', function() {
+        it('edit content',function() {
+            var label = getLabel();
+            label.on('edittextstart', startEdit);
+            label.on('edittextend', endEdit);
+            label.startEditText();
+
+            function startEdit(param) {
+                expect(label.isEditingText()).to.be.ok();
+                var dom = label.getEditor();
+                Z.DomUtil.on(dom, 'keyup', function(ev){
+                    var oEvent = ev || event;
+                    var char = String.fromCharCode(oEvent.keyCode);
+                    if(oEvent.shiftKey) {
+                        if(char == '1') {
+                            char = '!';
+                        }
+                    }
+                    dom.innerText += char;
+                });
+                happen.keyup(dom, {
+                    shiftKey: true,
+                    keyCode: 49
+                });
+                label.endEditText();
+                expect(label.isEditingText()).to.not.be.ok();
+            }
+            function endEdit(param) {
+                expect(label.getContent()).to.eql('I am a Text!');
+            }
+        });
+
+        it('edit content with “Enter” key',function() {
+            var label = getLabel();
+            var size = label.getSize();
+            label.on('edittextstart', startEdit);
+            label.on('edittextend', endEdit);
+            label.startEditText();
+            expect(label.isEditingText()).to.be.ok;
+            function startEdit(param) {
+                var dom = label.getEditor();
+                Z.DomUtil.on(dom, 'keyup', function(ev){
+                    var oEvent = ev || event;
+                    if(oEvent.keyCode === 13) {
+                        dom.innerText += '\n';
+                    }
+                });
+                happen.keyup(dom, {
+                    keyCode: 13
+                });
+                label.endEditText();
+                expect(label.isEditingText()).to.not.be.ok;
+            }
+            function endEdit(param) {
+                var symbol = label._getInternalSymbol(),
+                    font = maptalks.symbolizer.TextMarkerSymbolizer.getFont(symbol);
+                    textSize = symbol['textSize'] || 12,
+                    spacing = symbol['textLineSpacing'] || 0;
+                var h = Z.StringUtil.stringLength('test', font).height;
+                var expected = h * 2 + spacing;
+                expect(label.getSize()['height'] >= expected).to.be.ok();
+            }
+        });
+
+    });
+
+});
