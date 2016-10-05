c2f19f468e28eb9ce0edddc7d50f988cb33a93cd
FuZhenn <fuzhen@maptalks.org>
2016-10-05 12:30:41 +0800

    update sprite specs

--- test/geometry/SpriteSpec.js
+++ test/geometry/SpriteSpec.js
@@ -16,17 +16,20 @@ describe('#Sprite', function() {
         image.onload = function () {
             var resources = new maptalks.renderer.Canvas.Resources();
             resources.addResource([url], image);
-            var sprite = marker._getPainter().getSprite(resources).canvas;
-            expect(sprite).to.be.ok();
-            expect(sprite.getContext('2d').getImageData(40, 30, 1, 1).data[3]).to.be.above(0);
-            expect(sprite.width).to.be.eql(symbol.markerWidth);
-            expect(sprite.height).to.be.eql(symbol.markerHeight);
+            var sprite = marker._getPainter().getSprite(resources);
+            var canvas = sprite.canvas;
+            expect(canvas).to.be.ok();
+            expect(sprite.offset.x).to.be.eql(0 + 10);
+            expect(sprite.offset.y).to.be.eql(-60 / 2 + 5);
+            expect(canvas.getContext('2d').getImageData(40, 30, 1, 1).data[3]).to.be.above(0);
+            expect(canvas.width).to.be.eql(symbol.markerWidth);
+            expect(canvas.height).to.be.eql(symbol.markerHeight);
             done();
         }
         image.src = url;
     });
 
-    it('vector marker sprite', function() {
+    it('vector marker sprite: ellipse', function() {
         var marker = new maptalks.Marker([0, 0], {
             symbol : {
                 'markerType' : 'ellipse',
@@ -37,11 +40,35 @@ describe('#Sprite', function() {
             }
         });
         var symbol = marker.getSymbol();
-        var sprite = marker._getPainter().getSprite().canvas;
-        expect(sprite).to.be.ok();
-        expect(sprite.getContext('2d').getImageData(40, 30, 1, 1).data[3]).to.be.above(0);
-        expect(sprite.width).to.be.eql(symbol.markerWidth + 1); // +1 cos of lineWidth
-        expect(sprite.height).to.be.eql(symbol.markerHeight + 1); // +1 cos of lineWidth
+        var sprite = marker._getPainter().getSprite();
+        var canvas = sprite.canvas;
+        expect(canvas).to.be.ok();
+        expect(canvas.getContext('2d').getImageData(40, 30, 1, 1).data[3]).to.be.above(0);
+        expect(sprite.offset.x).to.be.eql(10);
+        expect(sprite.offset.y).to.be.eql(5);
+        expect(canvas.width).to.be.eql(symbol.markerWidth + 1); // +1 cos of lineWidth
+        expect(canvas.height).to.be.eql(symbol.markerHeight + 1); // +1 cos of lineWidth
+    });
+
+    it('vector marker sprite: bar', function() {
+        var marker = new maptalks.Marker([0, 0], {
+            symbol : {
+                'markerType' : 'bar',
+                'markerWidth' : 80,
+                'markerHeight' : 70,
+                'markerDx' : 10,
+                'markerDy' : 5
+            }
+        });
+        var symbol = marker.getSymbol();
+        var sprite = marker._getPainter().getSprite();
+        var canvas = sprite.canvas;
+        expect(canvas).to.be.ok();
+        expect(canvas.getContext('2d').getImageData(40, 30, 1, 1).data[3]).to.be.above(0);
+        expect(sprite.offset.x).to.be.eql(10);
+        expect(sprite.offset.y).to.be.eql(-35 + 5);
+        expect(canvas.width).to.be.eql(symbol.markerWidth + 1); // +1 cos of lineWidth
+        expect(canvas.height).to.be.eql(symbol.markerHeight + 1); // +1 cos of lineWidth
     });
 
     it('vector path marker sprite', function(done) {
@@ -69,20 +96,25 @@ describe('#Sprite', function() {
             var url = maptalks.Geometry.getMarkerPathBase64(symbol);
             var img = new Image();
             img.onload = function () {
-                var sprite = marker._getPainter().getSprite().canvas;
-                expect(sprite).to.be.ok();
-                expect(sprite.getContext('2d').getImageData(40, 15, 1, 1).data[3]).to.be.above(0);
-                expect(sprite.width).to.be.eql(symbol.markerWidth);
-                expect(sprite.height).to.be.eql(symbol.markerHeight);
+                var sprite = marker._getPainter().getSprite();
+                var canvas = sprite.canvas;
+                expect(sprite.offset.x).to.be.eql(0 + 10);
+                expect(sprite.offset.y).to.be.eql(-70 / 2 + 5);
+                expect(canvas.getContext('2d').getImageData(40, 15, 1, 1).data[3]).to.be.above(0);
+                expect(canvas.width).to.be.eql(symbol.markerWidth);
+                expect(canvas.height).to.be.eql(symbol.markerHeight);
                 done();
             }
             img.src = url;
         } else{
-            var sprite = marker._getPainter().getSprite().canvas;
-            expect(sprite).to.be.ok();
-            expect(sprite.getContext('2d').getImageData(40, 15, 1, 1).data[3]).to.be.above(0);
-            expect(sprite.width).to.be.eql(symbol.markerWidth);
-            expect(sprite.height).to.be.eql(symbol.markerHeight);
+            var sprite = marker._getPainter().getSprite();
+            var canvas = sprite.canvas;
+            expect(sprite.offset.x).to.be.eql(0 + 10);
+            expect(sprite.offset.y).to.be.eql(-70 / 2 + 5);
+            expect(canvas).to.be.ok();
+            expect(canvas.getContext('2d').getImageData(40, 15, 1, 1).data[3]).to.be.above(0);
+            expect(canvas.width).to.be.eql(symbol.markerWidth);
+            expect(canvas.height).to.be.eql(symbol.markerHeight);
             done();
         }
     });
