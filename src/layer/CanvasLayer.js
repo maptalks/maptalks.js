a66b5e5609fc83c696dca7fb09167332d4970042
FuZhenn <fuzhen@maptalks.org>
2016-10-05 23:22:31 +0800

    add comments for CanvasLayer

--- src/layer/CanvasLayer.js
+++ src/layer/CanvasLayer.js
@@ -1,23 +1,78 @@
+/**
+ * @classdesc
+ * A layer to draw on HTML5 2D Canvas.
+ * @class
+ * @category layer
+ * @extends {maptalks.Layer}
+ * @param {String|Number} id - layer's id
+ */
 Z.CanvasLayer = Z.Layer.extend({
+
+    /**
+     * An interface function called only once before the first draw, useful for preparation.
+     * @param  {CanvasRenderingContext2D } context - CanvasRenderingContext2D of the layer canvas.
+     * @return {Object[]} objects that will be passed to function draw(context, ..) as parameters.
+     */
     prepareToDraw: function () {},
 
+    /**
+     * The interface function to draw things on the layer canvas.
+     * @param  {CanvasRenderingContext2D} context - CanvasRenderingContext2D of the layer canvas.
+     * @param  {*} params.. - parameters returned by function prepareToDraw(context).
+     */
     draw: function () {},
 
-    onZoomStart: function () {},
+    /**
+     * Ask the map to redraw the layer canvas
+     * @return {maptalks.CanvasLayer} this
+     */
+    requestMapToRender: function () {
+        if (this._getRenderer()) {
+            this._getRenderer().requestMapToRender();
+        }
+        return this;
+    },
 
-    onZoomEnd: function () {},
-
-    onMoveStart: function () {},
-
-    onMoveEnd: function () {},
-
-    onResize: function () {},
-
+    /**
+     * Ask the map to redraw the layer canvas and fire layerload event
+     * @return {maptalks.CanvasLayer} this
+     */
     completeRender: function () {
         if (this._getRenderer()) {
             this._getRenderer().completeRender();
         }
-    }
+        return this;
+    },
+
+    /**
+     * The event callback for map's zoomstart event
+     * @param  {Object} param - event parameter
+     */
+    onZoomStart: function () {},
+
+    /**
+     * The event callback for map's zoomend event
+     * @param  {Object} param - event parameter
+     */
+    onZoomEnd: function () {},
+
+    /**
+     * The event callback for map's movestart event
+     * @param  {Object} param - event parameter
+     */
+    onMoveStart: function () {},
+
+    /**
+     * The event callback for map's moveend event
+     * @param  {Object} param - event parameter
+     */
+    onMoveEnd: function () {},
+
+    /**
+     * The event callback for map's resize event
+     * @param  {Object} param - event parameter
+     */
+    onResize: function () {}
 
 });
 
@@ -36,8 +91,7 @@ Z.CanvasLayer.registerRenderer('canvas', Z.renderer.Canvas.extend({
             this._predrawed = true;
         }
         this.layer.draw.apply(this.layer, [this.context].concat(this._drawContext));
-        this.requestMapToRender();
-        this.fireLoadedEvent();
+        this.completeRender();
     },
 
     remove: function () {
