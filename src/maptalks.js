35cb797a1985d5870faa1a728a353b881fda6ba1
wsw <wsw0108@gmail.com>
2016-12-28 16:09:49 +0800

    remove 'Matrix.js'

--- src/maptalks.js
+++ src/maptalks.js
@@ -5,12 +5,10 @@ const maptalks = {};
 
 import Ajax from './utils/Ajax';
 import Canvas from './utils/Canvas';
-import Matrix from './utils/Matrix';
 import Promise from './utils/Promise';
 
 maptalks.Ajax = Ajax;
 maptalks.Canvas = Canvas;
-maptalks.Matrix = Matrix;
 maptalks.Promise = Promise;
 
 import * as util from 'core/util';
