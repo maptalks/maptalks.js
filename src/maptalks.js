52fc09fcc50b6f0a70918c568e882741e20afc9a
wsw <wsw0108@gmail.com>
2016-12-30 09:47:18 +0800

    fix build

--- src/maptalks.js
+++ src/maptalks.js
@@ -14,7 +14,7 @@ maptalks.Promise = Promise;
 import * as util from 'core/util';
 maptalks.Util = util;
 
-maptalks.Util.getJSON = Ajax.getJSON;
+// maptalks.Util.getJSON = Ajax.getJSON;
 
 import * as DomUtil from 'core/util/dom';
 maptalks.DomUtil = DomUtil;
@@ -43,26 +43,11 @@ maptalks.OverlayLayer = OverlayLayer;
 import { VectorLayer } from 'layer/VectorLayer';
 maptalks.VectorLayer = VectorLayer;
 
-import * as renderer from 'renderer';
-maptalks.renderer = renderer;
-
-import * as mapRenderer from 'renderer/map';
-maptalks.renderer.map = mapRenderer;
-
-import * as tilelayerRenderer from 'renderer/tilelayer';
-maptalks.renderer.tilelayer = tilelayerRenderer;
-
-import * as canvastilelayer from 'renderer/tilelayer/Renderer.CanvasTileLayer.Canvas';
-maptalks.renderer.canvastilelayer = canvastilelayer;
-
 import { TileCache } from 'renderer/tilelayer/TileCache';
 maptalks.TileLayer.TileCache = TileCache;
 
-import * as overlaylayerRenderer from 'renderer/overlaylayer';
-maptalks.renderer.overlaylayer = overlaylayerRenderer;
-
-import * as vectorlayerRenderer from 'renderer/vectorlayer';
-maptalks.renderer.vectorlayer = vectorlayerRenderer;
+import * as renderer from 'renderer';
+maptalks.renderer = renderer;
 
 import * as Symbolizers from 'renderer/vectorlayer/symbolizers';
 maptalks.symbolizers = Symbolizers;
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,2 +1,13 @@
 export * from './Renderable';
 export * from './Renderer';
+
+// TODO: can not place below in here
+
+// import * as mapRenderer from './map';
+// export const map = mapRenderer;
+
+// import * as tilelayerRenderer from './tilelayer';
+// export const tilelayer = tilelayerRenderer;
+
+// import * as canvastilelayerRenderer from './tilelayer/Renderer.CanvasTileLayer.Canvas';
+// export const canvastilelayer = canvastilelayerRenderer;
