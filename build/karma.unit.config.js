19e311652fb37c784185b532510e45b3d82f0af0
FuZhenn <fuzhen@maptalks.org>
2017-01-22 22:32:13 +0800

    update npmignore abd karma configs

--- build/karma.unit.config.js
+++ build/karma.unit.config.js
@@ -2,7 +2,7 @@ const base = require('./karma.base.config.js');
 
 module.exports = function (config) {
     config.set(Object.assign(base, {
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
         reporters: ['mocha'],
         singleRun: true
     }));
