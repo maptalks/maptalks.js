3ed65e18f70139a12b63e343e89687b6edbdfe6d
Fu Zhen <fuzhen@maptalks.org>
2017-01-30 10:44:03 +0800

    remove ThreeLayer and update CanvasLayer (#330)

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,6 +3,7 @@ export { default as CanvasRenderer } from './layer/CanvasRenderer';
 
 export * from './layer/tilelayer';
 export * from './layer/vectorlayer';
+export * from './layer/canvaslayer';
 export { default as MapRenderer } from './map/MapRenderer';
 export { default as MapCanvasRenderer } from './map/MapCanvasRenderer';
 
