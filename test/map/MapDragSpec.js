e07d37fcf2517ff780bca9da482b1d90b8bd8041
FuZhenn <fuzhen@maptalks.org>
2017-02-18 17:17:41 +0800

    fix spec

--- test/map/MapDragSpec.js
+++ test/map/MapDragSpec.js
@@ -6,32 +6,34 @@ describe('#MapDrag', function () {
     var map;
     var center = new maptalks.Coordinate(118.846825, 32.046534);
 
-    function dragMap() {
+    function dragMap(steps) {
+        steps = steps || 10;
         var center = map.getCenter();
         var spy = sinon.spy();
         map.on('mousedown', spy);
 
         var domPosition = maptalks.DomUtil.getPagePosition(container);
         var point = map.coordinateToContainerPoint(center).add(domPosition);
-        var requestAnimFn = maptalks.Util.requestAnimFrame;
-        //replace original requestAnimFrame to immediate execution.
-        maptalks.Util.requestAnimFrame = function (fn) {
-            fn();
-        };
+
         happen.mousedown(map._panels.front, {
             'clientX':point.x,
             'clientY':point.y
         });
         expect(spy.called).to.be.ok();
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < steps; i++) {
             happen.mousemove(document, {
                 'clientX':point.x + i,
                 'clientY':point.y + i
             });
-            expect(map.isMoving()).to.be.ok();
+            if (map.options['draggable'] && i > 0) {
+                expect(map.isMoving()).to.be.ok();
+            }
         }
         happen.mouseup(document);
-        maptalks.Util.requestAnimFrame = requestAnimFn;
+        if (!map.options['panAnimation']) {
+            expect(map.isMoving()).not.to.be.ok();
+        }
+
     }
 
     beforeEach(function () {
@@ -43,20 +45,36 @@ describe('#MapDrag', function () {
     afterEach(function () {
         REMOVE_CONTAINER(container);
     });
-    describe('drag the map', function () {
-        it('can be dragged', function () {
-            map.setZoom(7);
-            dragMap();
-            expect(map.getCenter()).not.to.be.eql(center);
-        });
+
+    it('can be dragged', function () {
+        map.options['panAnimation'] = false;
+        var center2;
+        map.setZoom(7);
+        dragMap();
+        center2 = map.getCenter();
+        expect(center2.toArray()).not.to.be.eql(center.toArray());
+        expect(map.isMoving()).not.to.be.ok();
     });
 
-    describe('drag can be disable', function () {
-        it('disables dragging', function () {
-            map.config('draggable', false);
-            dragMap();
-            expect(map.getCenter()).to.be.closeTo(center);
+    it('drag and pan animation', function (done) {
+        map.options['panAnimation'] = true;
+        var center2;
+        map.on('moveend', function () {
+            expect(map.isMoving()).not.to.be.ok();
+            expect(map.getCenter().toArray()).not.to.be.eql(center2.toArray());
+            done();
         });
+        dragMap(30);
+        center2 = map.getCenter();
+    });
+
+    it('disables dragging', function (done) {
+        map.config('draggable', false);
+        dragMap();
+        setTimeout(function () {
+            expect(map.getCenter().toArray()).to.be.closeTo(center.toArray());
+            done();
+        }, 20);
     });
 
 });
