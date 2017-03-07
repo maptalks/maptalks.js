2bfa377b16c498755c67449d4f367f02b7cab0a6
fuzhenn <fuzhen@maptalks.org>
2017-03-08 01:47:46 +0800

    restore painting of cubic bezier

--- src/geometry/Curve.js
+++ src/geometry/Curve.js
@@ -27,47 +27,19 @@ class Curve extends LineString {
         Canvas2d.quadraticCurve(ctx, points);
     }
 
-    _getCubicCurvePoints(points) {
-        var ctrlPts = [];
-        var f = 0.3;
-        var t = 0.6;
-
-        var m = 0;
-        var dx1 = 0;
-        var dy1 = 0;
-        var dx2, dy2;
-        var curP, nexP;
-        var preP = points[0];
-        for (var i = 1, len = points.length; i < len; i++) {
-            curP = points[i];
-            nexP = points[i + 1];
-            if (nexP) {
-                m = (nexP.y - preP.y) / (nexP.x - preP.x);
-                dx2 = (nexP.x - curP.x) * -f;
-                dy2 = dx2 * m * t;
-            } else {
-                dx2 = 0;
-                dy2 = 0;
-            }
-            // ctx.bezierCurveTo(preP.x - dx1, preP.y - dy1, curP.x + dx2, curP.y + dy2, curP.x, curP.y);
-            ctrlPts.push(preP.x - dx1, preP.y - dy1, curP.x + dx2, curP.y + dy2, curP.x, curP.y);
-            dx1 = dx2;
-            dy1 = dy2;
-            preP = curP;
-        }
-        return ctrlPts;
-    }
-
     _bezierCurve(ctx, points) {
-
-        if (points.length <= 2) {
+        if (points.length <= 3) {
             Canvas2d._path(ctx, points);
             return;
         }
-        var ctrlPts = this._getCubicCurvePoints(points);
-        var i, len = ctrlPts.length;
-        for (i = 0; i < len; i += 6) {
-            ctx.bezierCurveTo(ctrlPts[i], ctrlPts[i + 1], ctrlPts[i + 2], ctrlPts[i + 3], ctrlPts[i + 4], ctrlPts[i + 5]);
+        var i, l;
+        for (i = 1, l = points.length; i + 2 < l; i += 3) {
+            ctx.bezierCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, points[i + 2].x, points[i + 2].y);
+        }
+        if (i < l) {
+            for (;i < l; i++) {
+                ctx.lineTo(points[i].x, points[i].y);
+            }
         }
     }
 }
