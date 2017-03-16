323bd333884f330b0a22ba0a73d60b52375aaeef
fuzhenn <fuzhen@maptalks.org>
2017-03-16 22:44:26 +0800

    add specs for custom projection

--- test/map/ProjectionSpec.js
+++ test/map/ProjectionSpec.js
@@ -22,12 +22,49 @@ describe('#Projection', function () {
         REMOVE_CONTAINER(container);
     });
 
+    it('customize projection', function () {
+        var custom = {
+            project : function (c) {
+                return c.add(1, 1);
+            },
+
+            unproject : function (c) {
+                return c.sub(1, 1);
+            }
+        };
+
+        map.setView({
+            'projection': custom,
+            'resolutions': (function () {
+                var resolutions = [];
+                for (var i = 0; i < 10; i++) {
+                    resolutions[i] = 4 / Math.pow(2, i);
+                }
+                return resolutions;
+            })(),
+            'fullExtent': {
+                'top'   : 100000,
+                'left'  : 0,
+                'right' : 100000,
+                'bottom' : 0
+            }
+        });
+
+        var projection = map.getProjection();
+        var coords = [
+            new maptalks.Coordinate(0, 0),
+            new maptalks.Coordinate(1, 1)
+        ];
+        var expected = [[1, 1], [2, 2]];
+        var actual = maptalks.Coordinate.toNumberArrays(projection.projectCoords(coords));
+        expect(actual).to.be.eql(expected);
+    });
+
     describe('is default projection', function () {
         it('default projection is EPSG:3857', function () {
             var projection = map.getProjection();
             expect(projection.code).to.be.eql('EPSG:3857');
         });
-
     });
 
     describe('change to EPSG:4326', function () {
