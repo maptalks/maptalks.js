21115d3e81ba94b498365355463719780356ca0c
axmand <815966508@qq.com>
2017-05-08 22:31:06 +0800

    add vec2

--- src/matrix/mat3.js
+++ src/matrix/mat3.js
@@ -65,6 +65,7 @@ class mat3 {
     /**
      *  Copies the upper-left 3x3 values into the given mat3.
      *  construct from mat4
+     *  @method fromMat4
      *  @return {mat3}
      */
     fromMat4(mat) {
@@ -76,6 +77,7 @@ class mat3 {
     };
     /**
     * Set a mat3 to the identity matrix
+    * @method identity
     * @param {mat3} out the receiving matrix
     * @returns {mat3} out
     */
@@ -93,9 +95,159 @@ class mat3 {
     };
     /**
      * Transpose the values of a mat3
+     * @method transpose
      */
-    transpose=function(){
+    transpose = function () {
+        //temporary array
+        var a = new glMatrix.ARRAY_TYPE(9);
+        a[0] = this._out[0];
+        a[1] = this._out[3];
+        a[2] = this._out[6];
+        a[3] = this._out[1];
+        a[4] = this._out[4];
+        a[5] = this._out[7];
+        a[6] = this._out[2];
+        a[7] = this._out[5];
+        a[8] = a[8];
+        //
+        this._out[0] = a[0];
+        this._out[1] = a[1];
+        this._out[2] = a[2];
+        this._out[3] = a[3];
+        this._out[4] = a[4];
+        this._out[5] = a[5];
+        this._out[6] = a[6];
+        this._out[7] = a[7];
+        this._out[8] = a[8];
+        return this;
+    };
+    /**
+     * Inverts a mat3
+     * @method invert
+     */
+    invert() {
 
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        let b01 = a22 * a11 - a12 * a21,
+            b11 = -a22 * a10 + a12 * a20,
+            b21 = a21 * a10 - a11 * a20,
+            det = a00 * b01 + a01 * b11 + a02 * b21;
+        if (!det) return null;
+        det = 1.0 / det;
+
+        this._out[0] = b01 * det;
+        this._out[1] = (-a22 * a01 + a02 * a21) * det;
+        this._out[2] = (a12 * a01 - a02 * a11) * det;
+        this._out[3] = b11 * det;
+        this._out[4] = (a22 * a00 - a02 * a20) * det;
+        this._out[5] = (-a12 * a00 + a02 * a10) * det;
+        this._out[6] = b21 * det;
+        this._out[7] = (-a21 * a00 + a01 * a20) * det;
+        this._out[8] = (a11 * a00 - a01 * a10) * det;
+        return this;
+    };
+    /**
+     * Calculates the adjugate of a mat3
+     * 
+     */
+    adjoint() {
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        this._out[0] = (a11 * a22 - a12 * a21);
+        this._out[1] = (a02 * a21 - a01 * a22);
+        this._out[2] = (a01 * a12 - a02 * a11);
+        this._out[3] = (a12 * a20 - a10 * a22);
+        this._out[4] = (a00 * a22 - a02 * a20);
+        this._out[5] = (a02 * a10 - a00 * a12);
+        this._out[6] = (a10 * a21 - a11 * a20);
+        this._out[7] = (a01 * a20 - a00 * a21);
+        this._out[8] = (a00 * a11 - a01 * a10);
+        return this;
+    };
+    /**
+     * Calculates the determinant of a mat3
+     */
+    determinant() {
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        return a00 * (a22 * a11 - a12 * a21) + a01 * (-a22 * a10 + a12 * a20) + a02 * (a21 * a10 - a11 * a20);
+    };
+    /**
+     * Multiplies other mat3
+     * @param {mat3} mat a matrix 3*3 wait to multiply
+     */
+    multiply(mat) {
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        let [b00, b01, b02, b10, b11, b12, b20, b21, b22] = mat._out;
+        this._out[0] = b00 * a00 + b01 * a10 + b02 * a20;
+        this._out[1] = b00 * a01 + b01 * a11 + b02 * a21;
+        this._out[2] = b00 * a02 + b01 * a12 + b02 * a22;
+
+        this._out[3] = b10 * a00 + b11 * a10 + b12 * a20;
+        this._out[4] = b10 * a01 + b11 * a11 + b12 * a21;
+        this._out[5] = b10 * a02 + b11 * a12 + b12 * a22;
+
+        this._out[6] = b20 * a00 + b21 * a10 + b22 * a20;
+        this._out[7] = b20 * a01 + b21 * a11 + b22 * a21;
+        this._out[8] = b20 * a02 + b21 * a12 + b22 * a22;
+    };
+    /**
+     * Translate a mat3 by the given vector
+     * @param {vec2} vec vetor to translate by
+     * @return {mat3} 
+     */
+    translate(vec) {
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        let [x, y] = vec._out;
+        out[0] = a00;
+        this._out[1] = a01;
+        this._out[2] = a02;
+        this._out[3] = a10;
+        this._out[4] = a11;
+        this._out[5] = a12;
+        this._out[6] = x * a00 + y * a10 + a20;
+        this._out[7] = x * a01 + y * a11 + a21;
+        this._out[8] = x * a02 + y * a12 + a22;
+        return this;
+    };
+    /**
+     * Rotates a mat3 by the given angle
+     * @param {Number} rad the angle to rotate the matrix by
+     */
+    rotate(rad) {
+        let [a00, a01, a02, a10, a11, a12, a20, a21, a22] = this._out;
+        let s = Math.sin(rad),
+            c = Math.cos(rad);
+        this._out[0] = c * a00 + s * a10;
+        this._out[1] = c * a01 + s * a11;
+        this._out[2] = c * a02 + s * a12;
+        this._out[3] = c * a10 - s * a00;
+        this._out[4] = c * a11 - s * a01;
+        this._out[5] = c * a12 - s * a02;
+        this._out[6] = a20;
+        this._out[7] = a21;
+        this._out[8] = a22;
+        return this;
+    };
+    /**
+     * Scales the mat3 by the dimensions in the given vec2
+     * @param {vec2} v the vec2 to scale the matrix by
+     */
+    scale(vec) {
+        let [x,y]=vec._out;
+        this._out[0] = x *this._outa[0];
+        this._out[1] = x * this._out[1];
+        this._out[2] = x * this._out[2];
+        this._out[3] = y * this._out[3];
+        this._out[4] = y * this._out[4];
+        this._out[5] = y * this._out[5];
+        // this._out[6] = this._out[6];
+        // this._out[7] = this._out[7];
+        // this._out[8] = this._out[8];
+        return this;
+    };
+    /**
+     * Calculates a 3x3 matrix from the given quaternion
+     */
+    fromQuat(q){
 
     }
 
@@ -107,331 +259,9 @@ class mat3 {
 
 
 
-/**
- * Transpose the values of a mat3
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the source matrix
- * @returns {mat3} out
- */
-mat3.transpose = function (out, a) {
-    // If we are transposing ourselves we can skip a few steps but have to cache some values
-    if (out === a) {
-        var a01 = a[1], a02 = a[2], a12 = a[5];
-        out[1] = a[3];
-        out[2] = a[6];
-        out[3] = a01;
-        out[5] = a[7];
-        out[6] = a02;
-        out[7] = a12;
-    } else {
-        out[0] = a[0];
-        out[1] = a[3];
-        out[2] = a[6];
-        out[3] = a[1];
-        out[4] = a[4];
-        out[5] = a[7];
-        out[6] = a[2];
-        out[7] = a[5];
-        out[8] = a[8];
-    }
 
-    return out;
-};
 
-/**
- * Inverts a mat3
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the source matrix
- * @returns {mat3} out
- */
-mat3.invert = function (out, a) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8],
 
-        b01 = a22 * a11 - a12 * a21,
-        b11 = -a22 * a10 + a12 * a20,
-        b21 = a21 * a10 - a11 * a20,
-
-        // Calculate the determinant
-        det = a00 * b01 + a01 * b11 + a02 * b21;
-
-    if (!det) {
-        return null;
-    }
-    det = 1.0 / det;
-
-    out[0] = b01 * det;
-    out[1] = (-a22 * a01 + a02 * a21) * det;
-    out[2] = (a12 * a01 - a02 * a11) * det;
-    out[3] = b11 * det;
-    out[4] = (a22 * a00 - a02 * a20) * det;
-    out[5] = (-a12 * a00 + a02 * a10) * det;
-    out[6] = b21 * det;
-    out[7] = (-a21 * a00 + a01 * a20) * det;
-    out[8] = (a11 * a00 - a01 * a10) * det;
-    return out;
-};
-
-/**
- * Calculates the adjugate of a mat3
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the source matrix
- * @returns {mat3} out
- */
-mat3.adjoint = function (out, a) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8];
-
-    out[0] = (a11 * a22 - a12 * a21);
-    out[1] = (a02 * a21 - a01 * a22);
-    out[2] = (a01 * a12 - a02 * a11);
-    out[3] = (a12 * a20 - a10 * a22);
-    out[4] = (a00 * a22 - a02 * a20);
-    out[5] = (a02 * a10 - a00 * a12);
-    out[6] = (a10 * a21 - a11 * a20);
-    out[7] = (a01 * a20 - a00 * a21);
-    out[8] = (a00 * a11 - a01 * a10);
-    return out;
-};
-
-/**
- * Calculates the determinant of a mat3
- *
- * @param {mat3} a the source matrix
- * @returns {Number} determinant of a
- */
-mat3.determinant = function (a) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8];
-
-    return a00 * (a22 * a11 - a12 * a21) + a01 * (-a22 * a10 + a12 * a20) + a02 * (a21 * a10 - a11 * a20);
-};
-
-/**
- * Multiplies two mat3's
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the first operand
- * @param {mat3} b the second operand
- * @returns {mat3} out
- */
-mat3.multiply = function (out, a, b) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8],
-
-        b00 = b[0], b01 = b[1], b02 = b[2],
-        b10 = b[3], b11 = b[4], b12 = b[5],
-        b20 = b[6], b21 = b[7], b22 = b[8];
-
-    out[0] = b00 * a00 + b01 * a10 + b02 * a20;
-    out[1] = b00 * a01 + b01 * a11 + b02 * a21;
-    out[2] = b00 * a02 + b01 * a12 + b02 * a22;
-
-    out[3] = b10 * a00 + b11 * a10 + b12 * a20;
-    out[4] = b10 * a01 + b11 * a11 + b12 * a21;
-    out[5] = b10 * a02 + b11 * a12 + b12 * a22;
-
-    out[6] = b20 * a00 + b21 * a10 + b22 * a20;
-    out[7] = b20 * a01 + b21 * a11 + b22 * a21;
-    out[8] = b20 * a02 + b21 * a12 + b22 * a22;
-    return out;
-};
-
-/**
- * Alias for {@link mat3.multiply}
- * @function
- */
-mat3.mul = mat3.multiply;
-
-/**
- * Translate a mat3 by the given vector
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the matrix to translate
- * @param {vec2} v vector to translate by
- * @returns {mat3} out
- */
-mat3.translate = function (out, a, v) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8],
-        x = v[0], y = v[1];
-
-    out[0] = a00;
-    out[1] = a01;
-    out[2] = a02;
-
-    out[3] = a10;
-    out[4] = a11;
-    out[5] = a12;
-
-    out[6] = x * a00 + y * a10 + a20;
-    out[7] = x * a01 + y * a11 + a21;
-    out[8] = x * a02 + y * a12 + a22;
-    return out;
-};
-
-/**
- * Rotates a mat3 by the given angle
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the matrix to rotate
- * @param {Number} rad the angle to rotate the matrix by
- * @returns {mat3} out
- */
-mat3.rotate = function (out, a, rad) {
-    var a00 = a[0], a01 = a[1], a02 = a[2],
-        a10 = a[3], a11 = a[4], a12 = a[5],
-        a20 = a[6], a21 = a[7], a22 = a[8],
-
-        s = Math.sin(rad),
-        c = Math.cos(rad);
-
-    out[0] = c * a00 + s * a10;
-    out[1] = c * a01 + s * a11;
-    out[2] = c * a02 + s * a12;
-
-    out[3] = c * a10 - s * a00;
-    out[4] = c * a11 - s * a01;
-    out[5] = c * a12 - s * a02;
-
-    out[6] = a20;
-    out[7] = a21;
-    out[8] = a22;
-    return out;
-};
-
-/**
- * Scales the mat3 by the dimensions in the given vec2
- *
- * @param {mat3} out the receiving matrix
- * @param {mat3} a the matrix to rotate
- * @param {vec2} v the vec2 to scale the matrix by
- * @returns {mat3} out
- **/
-mat3.scale = function (out, a, v) {
-    var x = v[0], y = v[1];
-
-    out[0] = x * a[0];
-    out[1] = x * a[1];
-    out[2] = x * a[2];
-
-    out[3] = y * a[3];
-    out[4] = y * a[4];
-    out[5] = y * a[5];
-
-    out[6] = a[6];
-    out[7] = a[7];
-    out[8] = a[8];
-    return out;
-};
-
-/**
- * Creates a matrix from a vector translation
- * This is equivalent to (but much faster than):
- *
- *     mat3.identity(dest);
- *     mat3.translate(dest, dest, vec);
- *
- * @param {mat3} out mat3 receiving operation result
- * @param {vec2} v Translation vector
- * @returns {mat3} out
- */
-mat3.fromTranslation = function (out, v) {
-    out[0] = 1;
-    out[1] = 0;
-    out[2] = 0;
-    out[3] = 0;
-    out[4] = 1;
-    out[5] = 0;
-    out[6] = v[0];
-    out[7] = v[1];
-    out[8] = 1;
-    return out;
-}
-
-/**
- * Creates a matrix from a given angle
- * This is equivalent to (but much faster than):
- *
- *     mat3.identity(dest);
- *     mat3.rotate(dest, dest, rad);
- *
- * @param {mat3} out mat3 receiving operation result
- * @param {Number} rad the angle to rotate the matrix by
- * @returns {mat3} out
- */
-mat3.fromRotation = function (out, rad) {
-    var s = Math.sin(rad), c = Math.cos(rad);
-
-    out[0] = c;
-    out[1] = s;
-    out[2] = 0;
-
-    out[3] = -s;
-    out[4] = c;
-    out[5] = 0;
-
-    out[6] = 0;
-    out[7] = 0;
-    out[8] = 1;
-    return out;
-}
-
-/**
- * Creates a matrix from a vector scaling
- * This is equivalent to (but much faster than):
- *
- *     mat3.identity(dest);
- *     mat3.scale(dest, dest, vec);
- *
- * @param {mat3} out mat3 receiving operation result
- * @param {vec2} v Scaling vector
- * @returns {mat3} out
- */
-mat3.fromScaling = function (out, v) {
-    out[0] = v[0];
-    out[1] = 0;
-    out[2] = 0;
-
-    out[3] = 0;
-    out[4] = v[1];
-    out[5] = 0;
-
-    out[6] = 0;
-    out[7] = 0;
-    out[8] = 1;
-    return out;
-}
-
-/**
- * Copies the values from a mat2d into a mat3
- *
- * @param {mat3} out the receiving matrix
- * @param {mat2d} a the matrix to copy
- * @returns {mat3} out
- **/
-mat3.fromMat2d = function (out, a) {
-    out[0] = a[0];
-    out[1] = a[1];
-    out[2] = 0;
-
-    out[3] = a[2];
-    out[4] = a[3];
-    out[5] = 0;
-
-    out[6] = a[4];
-    out[7] = a[5];
-    out[8] = 1;
-    return out;
-};
 
 /**
 * Calculates a 3x3 matrix from the given quaternion
