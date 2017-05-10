1ccc9d1251e77c40d51438ddd3a284b6c4b30945
axmand <815966508@qq.com>
2017-05-10 14:01:22 +0800

    moidfy init index package

---
+++ src/matrix/init.js
@@ -0,0 +1,10 @@
+/**
+ * make a package of matrix
+ * 
+ */
+import mat3 from './mat3';
+import mat4 from './mat4';
+import vec2 from './vec2';
+import vec3 from './vec3';
+import vec4 from './vec4';
+import quat from './quat';
