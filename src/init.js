3bb6181853cbc26bd5a2c6e94d5a142f157b928f
axmand <815966508@qq.com>
2017-05-12 03:34:50 +0800

    modify

--- src/init.js
+++ src/init.js
@@ -1,2 +1,2 @@
-import f from './matrix/init';
-export default f;
+import { vec2, vec3, vec4, mat3, mat4, quat } from './matrix/init';
+export default { vec2, vec3, vec4, mat3, mat4, quat };
--- test/matrix/matrix.spec.js
+++ test/matrix/matrix.spec.js
@@ -1,9 +1,13 @@
+//import a from './../../dist/kiwi.gl';
 
 describe("vec2",()=>{
 
     describe("#add",()=>{
         it("the first test#add",()=>{
+            let vec=new vec2();
+            
             expect("1").to.be.equal("1");
+
         });
     });
 
