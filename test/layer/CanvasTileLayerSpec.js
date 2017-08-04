78279259e78c9479e4c58adac6788e726b244e2c
fuzhenn <fuzhen@maptalks.org>
2017-08-04 22:58:20 +0800

    add spec for CanvasTileLayer

---
+++ test/layer/CanvasTileLayerSpec.js
@@ -0,0 +1,43 @@
+describe('CanvasTileLayer', function () {
+
+    var container;
+    var map;
+    var center = new maptalks.Coordinate(118.846825, 32.046534);
+
+    beforeEach(function () {
+        container = document.createElement('div');
+        container.style.width = '3px';
+        container.style.height = '3px';
+        document.body.appendChild(container);
+        var option = {
+            zoom: 17,
+            center: center
+        };
+        map = new maptalks.Map(container, option);
+    });
+
+    afterEach(function () {
+        map.remove();
+        REMOVE_CONTAINER(container);
+    });
+
+    describe('add to map', function () {
+        it('add', function (done) {
+            var tile = new maptalks.CanvasTileLayer('tile', {
+                urlTemplate : '/resources/tile.png'
+            });
+            tile.drawTile = function (canvas, tileContext, onComplete) {
+                const ctx = canvas.getContext('2d');
+                ctx.fillStyle = 'Red';
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                onComplete(null);
+            };
+            tile.once('layerload', function () {
+                expect(tile).to.be.painted(0, 0, [255, 0, 0]);
+                done();
+            });
+            map.addLayer(tile);
+        });
+    });
+
+});
