78a44cf4b674bf010bab80473e4d86426fabba67
axmand <815966508@qq.com>
2017-09-01 15:49:08 +0800

    fix ticker autoStart bug

--- dist/js/index2.js
+++ dist/js/index2.js
@@ -30,28 +30,3 @@ var animate = function () {
 };
 
 animate();
-
-
-//var scene2 = new THREE.Scene();
-//var camera2 = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 1000);
-
-//var renderer2 = new THREE.WebGLRenderer({
-//    canvas:document.getElementById('cvs2')
-//});
-//renderer2.setSize(800, 600);
-
-//var geometry2 = new THREE.BoxGeometry(1, 1, 1);
-//var material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//var cube2 = new THREE.Mesh(geometry2, material2);
-//scene2.add(cube2);
-
-//camera2.position.z = 5;
-
-//var animate2 = function () {
-//    requestAnimationFrame(animate2);
-//    cube2.rotation.x += 0.1;
-//    cube2.rotation.y += 0.1;
-//    renderer2.render(scene2, camera2);
-//};
-
-//animate2();
