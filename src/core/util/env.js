c48e8cb159732ac2f9bc6590dbf82b779f7818ea
signhuwei <22626612@qq.com>
2017-11-02 12:36:28 +0800

    whether running in nodejs but neither on electron nor node-webkit

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -2,8 +2,8 @@
  * from detect-node
  * https://github.com/iliakan/detect-node
  *
- * @property {boolean} IS_NODE - whether running in nodejs but not on electron
+ * @property {boolean} IS_NODE - whether running in nodejs but not on electron,node-webkit
  * @global
  * @name IS_NODE
  */
-export const IS_NODE = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]' && !process.versions['electron'];
+export const IS_NODE = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]' && !process.versions['electron'] && !process.versions['nw'] && !process.versions['node-webkit'];
