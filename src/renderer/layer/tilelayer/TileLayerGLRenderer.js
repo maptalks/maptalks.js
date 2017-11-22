489679faef0500fbea853856ba5ac92f1d32ff0a
fuzhenn <fuzhen@maptalks.org>
2017-11-23 00:55:02 +0800

    change tile texture's min filter to be sharper

--- src/renderer/layer/tilelayer/TileLayerGLRenderer.js
+++ src/renderer/layer/tilelayer/TileLayerGLRenderer.js
@@ -221,7 +221,8 @@ class TileLayerGLRenderer extends TileLayerCanvasRenderer {
         // Bind the texture object to the target
         gl.bindTexture(gl.TEXTURE_2D, texture);
         // from mapbox-gl-js
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
