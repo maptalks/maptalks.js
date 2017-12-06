2c958d2fd89cd46f6578409ada1ebca2f3a7a4e1
fuzhenn <fuzhen@maptalks.org>
2017-12-06 14:34:37 +0800

    add preserveDrawingBuffer in options

--- src/renderer/layer/ImageGLRenderable.js
+++ src/renderer/layer/ImageGLRenderable.js
@@ -390,16 +390,18 @@ const ImageGLRenderable = Base => {
             return uSampler;
         }
 
-        _createGLContext(canvas) {
+        _createGLContext(canvas, options) {
             const attributes = {
-                'alpha': true
+                'alpha': true,
+                'preserveDrawingBuffer' : true,
+                'antialias' : false
             };
             const names = ['webgl', 'experimental-webgl'];
             let context = null;
             /* eslint-disable no-empty */
             for (let i = 0; i < names.length; ++i) {
                 try {
-                    context = canvas.getContext(names[i], attributes);
+                    context = canvas.getContext(names[i], options || attributes);
                 } catch (e) {}
                 if (context) {
                     break;
