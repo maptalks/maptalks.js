d58dce11bbc30badc85ff1fc901d2a0155d0bcfe
fuzhenn <fuzhen@maptalks.org>
2018-01-16 09:22:34 +0800

    add zIndex in geometry's comment

--- src/geometry/Geometry.js
+++ src/geometry/Geometry.js
@@ -32,6 +32,7 @@ import SpatialReference from '../map/spatial-reference/SpatialReference';
  * @property {Boolean} [options.draggable=false]    - whether the geometry can be dragged.
  * @property {Boolean} [options.dragShadow=false]   - if true, during geometry dragging, a shadow will be dragged before geometry was moved.
  * @property {Boolean} [options.dragOnAxis=null]    - if set, geometry can only be dragged along the specified axis, possible values: x, y
+ * @property {Number}  [options.zIndex=undefined]   - geometry's initial zIndex
  * @memberOf Geometry
  * @instance
  */
