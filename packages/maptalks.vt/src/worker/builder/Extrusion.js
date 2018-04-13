e88d0be2f606bfb151bcde75bd3ae1a74026c225
fuzhenn <fuzhen@maptalks.org>
2018-04-13 18:24:25 +0800

    fix triangulation of polygon's holes

--- packages/maptalks.vt/src/worker/builder/Extrusion.js
+++ packages/maptalks.vt/src/worker/builder/Extrusion.js
@@ -26,7 +26,9 @@ export function buildExtrudeFaces(features, EXTENT,
 
         //just ignore bottom indices never in sight
         const triangles = earcut(top, holes, 3); //vertices, holes, dimension(2|3)
-
+        if (triangles.length === 0) {
+            return offset + count;
+        }
         //TODO caculate earcut deviation
 
         //switch triangle's i + 1 and i + 2 to make it counter-clockwise (front face) from the top
@@ -48,13 +50,18 @@ export function buildExtrudeFaces(features, EXTENT,
         //side indices
         const startIdx = start / 3;
         const vertexCount = count / 3;
+        let ringStartIdx = startIdx;
         // debugger
         for (let i = startIdx, l = vertexCount + startIdx; i < l; i++) {
             if (i === l - 1 || holes.indexOf(i - startIdx + 1) >= 0) {
                 //top[l - 1], bottom[l - 1], top[0]
-                indices.push(i + vertexCount, i, startIdx);
+                indices.push(i + vertexCount, i, ringStartIdx);
                 //bottom[0], top[0], bottom[l - 1]
-                indices.push(startIdx, startIdx + vertexCount, i + vertexCount);
+                indices.push(ringStartIdx, ringStartIdx + vertexCount, i + vertexCount);
+                if (i < l - 1) {
+                    //it's a hole, relocate ringStartIdx to hole's start
+                    ringStartIdx = i + 1;
+                }
             } else if (i < l - 1) {
                 //top[i], bottom[i], top[i + 1]
                 indices.push(i + vertexCount, i, i + 1);
@@ -64,21 +71,26 @@ export function buildExtrudeFaces(features, EXTENT,
         }
         return offset + count;
     }
-
-    // debugger
-
     let offset = 0;
+
     for (let r = 0, n = features.length; r < n; r++) {
         const feature = features[r];
         const geometry = feature.geometry;
 
         const altitude = getHeightValue(feature.properties, altitudeProperty, defaultAltitude) * altitudeScale;
+        const height = heightProperty ? getHeightValue(feature.properties, heightProperty, defaultHeight) * altitudeScale : altitude;
+
         let start = offset;
         let holes = [];
-
         for (let i = 0, l = geometry.length; i < l; i++) {
-            //fill bottom vertexes
             const isHole = calculateSignedArea(geometry[i]) < 0;
+            //fill bottom vertexes
+            if (!isHole && i > 0) {
+                //an exterior ring (multi plygon)
+                offset = fillData(start, offset, holes, height * scale); //need to multiply with scale as altitude is
+                holes = [];
+                start = offset;
+            }
             const segStart = offset - start;
             let ring = geometry[i];
             if (feature.type === 3) ring = ring.slice(0, ring.length - 1);
@@ -87,15 +99,8 @@ export function buildExtrudeFaces(features, EXTENT,
             if (isHole) {
                 holes.push(segStart / 3);
             }
-            if (!isHole && i > 0 || i === l - 1) {
-                let height = altitude;
-                if (heightProperty) {
-                    height = getHeightValue(feature.properties, heightProperty, defaultHeight) * altitudeScale;
-                }
-                //an exterior ring
+            if (i === l - 1) {
                 offset = fillData(start, offset, holes, height * scale); //need to multiply with scale as altitude is
-                holes = [];
-                start = offset;
             }
         }
         indexes[r] = indices.length;
