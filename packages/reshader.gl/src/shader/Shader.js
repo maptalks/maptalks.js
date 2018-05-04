dca965a8ec5261d5dd6dc82056aaf1e178822c10
fuzhenn <fuzhen@maptalks.org>
2018-05-04 14:39:29 +0800

    update Shader

--- packages/reshader.gl/src/shader/Shader.js
+++ packages/reshader.gl/src/shader/Shader.js
@@ -1,15 +1,16 @@
 import { extend, isNumber, isString, isFunction } from '../common/Util.js';
+import ShaderLib from './ShaderLib.js';
 
 const TYPE_FUNC = 'function';
 
 class Shader {
-    constructor(vert, frag, context, shaderDefines, extraCommandProps) {
+    constructor({ vert, frag, uniforms, defines, includes, extraCommandProps }) {
         this.vert = vert;
         this.frag = frag;
         //defines besides meshes : lights, etc
-        this.shaderDefines = shaderDefines || {};
+        this.shaderDefines = defines || {};
 
-        context = context || [];
+        const context = uniforms || [];
         this.context = {};
         for (let i = 0, l = context.length; i < l; i++) {
             const p = context[i];
@@ -18,15 +19,19 @@ class Shader {
             } else {
                 // e.g.
                 // {
+                //     name : 'foo',
                 //     type : 'function',
                 //     fn : (context, props) => { ... }
                 // }
                 this.context[p.name] = p;
             }
         }
-
+        if (includes) {
+            this._insertIncludes(includes);
+        }
         this.extraCommandProps = extraCommandProps || {};
         this.commands = {};
+        this._compileSource();
     }
 
     /**
@@ -51,6 +56,7 @@ class Shader {
     }
 
     createREGLCommand(regl, materialDefines, attrProps, uniProps, elements) {
+        uniProps = uniProps || {};
         const defines = extend({}, this.shaderDefines || {}, materialDefines || {});
         const vert = this._insertDefines(this.vert, defines);
         const frag = this._insertDefines(this.frag, defines);
@@ -97,6 +103,24 @@ class Shader {
             }
         }
         return defineHeaders.join('') + source;
+    }
+
+    _insertIncludes(includes) {
+        const vertIncludes = includes['vert'],
+            fragIncludes = includes['frag'];
+        if (vertIncludes) {
+            // const includes = vertIncludes.map(inc => `#include <${inc}>\n`);
+            this.vert = vertIncludes + '\n' + this.vert;
+        }
+        if (fragIncludes) {
+            // const includes = fragIncludes.map(inc => `#include <${inc}>\n`);
+            this.frag = fragIncludes + '\n' + this.frag;
+        }
+    }
+
+    _compileSource() {
+        this.vert = ShaderLib.compile(this.vert);
+        this.frag = ShaderLib.compile(this.frag);
     }
 }
 
