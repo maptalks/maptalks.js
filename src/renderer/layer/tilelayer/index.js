4b39f5fe425b79bcf40bafbdc4e0e93279a24e71
fuzhenn <fuzhen@maptalks.org>
2018-05-04 22:32:12 +0800

    add QuadStencil

--- src/renderer/layer/tilelayer/index.js
+++ src/renderer/layer/tilelayer/index.js
@@ -1,10 +1,12 @@
 import TileLayerCanvasRenderer from './TileLayerCanvasRenderer';
 import TileLayerGLRenderer from './TileLayerGLRenderer';
 import { CanvasTileLayerCanvasRenderer, CanvasTileLayerGLRenderer } from './CanvasTileLayerRenderer';
+import QuadStencil from './QuadStencil';
 
 export {
     TileLayerCanvasRenderer,
     TileLayerGLRenderer,
     CanvasTileLayerCanvasRenderer,
-    CanvasTileLayerGLRenderer
+    CanvasTileLayerGLRenderer,
+    QuadStencil
 };
