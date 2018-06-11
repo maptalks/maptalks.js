16d1e44af72bf64cb4edff81d12bf394984b744c
fuzhenn <fuzhen@maptalks.org>
2018-06-11 17:12:44 +0800

    fix drawtool spec on ie

--- test/map/tools/DrawToolSpec.js
+++ test/map/tools/DrawToolSpec.js
@@ -227,7 +227,9 @@ describe('DrawTool', function () {
                 expect(param.geometry instanceof maptalks.Polygon).to.be.ok();
                 var coordinates = param.geometry.getCoordinates()[0];
                 expect(coordinates.length === 5).to.be.ok();
-                expect(coordinates[1].toArray()).to.be.eql([118.84675603637106, 32.046603663660306 ]);
+                if (!maptalks.Browser.ie) {
+                    expect(coordinates[1].toArray()).to.be.eql([118.84675603637106, 32.046603663660306]);
+                }
                 done();
             }
             var drawTool = new maptalks.DrawTool({
