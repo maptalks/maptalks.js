935221f045226159758a33b00810f1724a158ed5
fuzhenn <fuzhen@maptalks.org>
2018-08-23 19:48:04 +0800

    fix lint

--- src/core/util/dom.js
+++ src/core/util/dom.js
@@ -344,8 +344,8 @@ export function getEventContainerPoint(ev, dom) {
     }
     // div by scaleX, scaleY to fix #450
     return new Point(
-       (ev.clientX - domPos[0] - dom.clientLeft) / domPos[2] ,
-       (ev.clientY - domPos[1] - dom.clientTop) / domPos[3] 
+        (ev.clientX - domPos[0] - dom.clientLeft) / domPos[2],
+        (ev.clientY - domPos[1] - dom.clientTop) / domPos[3]
     );
 }
 
