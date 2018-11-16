776033e84ee55982c4243bdd49e092f3e2007c64
fuzhenn <fuzhen@maptalks.org>
2018-11-16 21:59:45 +0800

    update QuadStencil

--- src/renderer/layer/tilelayer/QuadStencil.js
+++ src/renderer/layer/tilelayer/QuadStencil.js
@@ -1,14 +1,12 @@
 import { createProgram, enableVertexAttrib } from '../../../core/util/gl';
 
-const quadVertices = typeof Float32Array !== 'undefined' ? new Float32Array([
+const quadVertices = typeof Int8Array !== 'undefined' ? new Int8Array([
     // positions
     -1,  1, 0,
     -1, -1, 0,
     1,  1, 0,
     1, -1, 0,
 ]) : [];
-
-const attributes = ['a_position', 3];
 
 const vert = `
     attribute vec3 a_position;
@@ -33,57 +31,51 @@ class QuadStencil {
     constructor(gl, vertices, debug) {
         this.gl = gl;
         this.quadVertices = vertices || quadVertices;
+        this.attributes = ['a_position', 3, getType(vertices)];
         this.debug = debug;
     }
 
     start() {
-        if (this.debug) {
-            return;
-        }
         const gl = this.gl;
-        gl.colorMask(false, false, false, false);
-        this.depthTest = gl.isEnabled(gl.DEPTH_TEST);
-        if (this.depthTest) {
-            gl.depthMask(false);
-            gl.disable(gl.DEPTH_TEST);
-        }
         gl.enable(gl.STENCIL_TEST);
         gl.stencilMask(0xFF);
-    }
-
-    end() {
-        if (this.debug) {
-            return;
-        }
-        const gl = this.gl;
-        gl.stencilMask(0x00);
-        gl.colorMask(true, true, true, true);
-        if (this.depthTest) {
-            gl.depthMask(true);
-            gl.enable(gl.DEPTH_TEST);
-        }
-    }
-
-    draw(transform) {
+        gl.stencilOp(gl.KEEP, gl.REPLACE, gl.REPLACE);
+        gl.depthMask(false);
+        this._save();
         if (!this.buffer) {
             this._createBuffer();
             this._createProgram();
         }
-        const gl = this.gl;
-        this._save();
         gl.useProgram(this.program);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-        enableVertexAttrib(gl, this.program, attributes);
+        enableVertexAttrib(gl, this.program, this.attributes);
         if (!this.transformLoc) {
             this.transformLoc = gl.getUniformLocation(this.program, 'transform');
         }
-        gl.uniformMatrix4fv(this.transformLoc, false, transform);
         if (!this.colorLoc) {
             this.colorLoc = gl.getUniformLocation(this.program, 'color');
         }
+        if (this.debug) {
+            return;
+        }
+        gl.colorMask(false, false, false, false);
+    }
+
+    end() {
+        const gl = this.gl;
+        gl.depthMask(true);
+        this._restore();
+        if (this.debug) {
+            return;
+        }
+        gl.colorMask(true, true, true, true);
+    }
+
+    draw(transform) {
+        const gl = this.gl;
+        gl.uniformMatrix4fv(this.transformLoc, false, transform);
         gl.uniform3fv(this.colorLoc, [Math.random(), Math.random(), Math.random()]);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-        this._restore();
     }
 
     remove() {
@@ -102,44 +94,30 @@ class QuadStencil {
     }
 
     stencilMask(mask) {
-        if (this.debug) {
-            return this;
-        }
         this.gl.stencilMask(mask);
         return this;
     }
 
     stencilFunc(func, ref, mask) {
-        if (this.debug) {
-            return this;
-        }
+        this.ref = ref;
         this.gl.stencilFunc(func, ref, mask);
         return this;
     }
 
     stencilOp(fail, zfail, zpass) {
-        if (this.debug) {
-            return this;
-        }
         this.gl.stencilOp(fail, zfail, zpass);
         return this;
     }
 
     resetFunc() {
-        if (this.debug) {
-            return this;
-        }
+        this.ref = 1;
         this.gl.stencilFunc(this.gl.ALWAYS, 1, 0xFF);
         return this;
     }
 
     _save() {
-        this._savedProgram = this.gl.program;
         const gl = this.gl;
-        if (gl.isEnabled(gl.CULL_FACE)) {
-            this._culling = true;
-            gl.disable(gl.CULL_FACE);
-        }
+        this._savedProgram = gl.program;
     }
 
     _restore() {
@@ -147,10 +125,6 @@ class QuadStencil {
         gl.program = this._savedProgram;
         if (gl.program) {
             gl.useProgram(gl.program);
-        }
-        if (this._culling) {
-            gl.enable(gl.CULL_FACE);
-            delete this._culling;
         }
     }
 
@@ -174,3 +148,18 @@ class QuadStencil {
 }
 
 export default QuadStencil;
+
+function getType(arr) {
+    if (arr instanceof Float32Array) {
+        return 'FLOAT';
+    } else if (arr instanceof Int16Array) {
+        return 'SHORT';
+    } else if (arr instanceof Uint16Array) {
+        return 'UNSIGNED_SHORT';
+    } else if (arr instanceof Int8Array) {
+        return 'BYTE';
+    } else if (arr instanceof Uint8Array || arr instanceof Uint8ClampedArray) {
+        return 'UNSIGNED_BYTE';
+    }
+    return 'FLOAT';
+}
