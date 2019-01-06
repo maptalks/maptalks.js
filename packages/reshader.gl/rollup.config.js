784ae09872e4c887d60d5179e20d5d0275893280
fuzhenn <fuzhen@maptalks.org>
2019-01-06 15:59:40 +0800

    prepare to publish

--- packages/reshader.gl/rollup.config.js
+++ packages/reshader.gl/rollup.config.js
@@ -1,17 +1,20 @@
 const resolve = require('rollup-plugin-node-resolve');
-const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
+const terser = require('rollup-plugin-terser').terser;
 const pkg = require('./package.json');
 
 function glsl() {
     return {
         transform(code, id) {
             if (/\.vert$/.test(id) === false && /\.frag$/.test(id) === false && /\.glsl$/.test(id) === false) return null;
-            var transformedCode = code.replace(/[ \t]*\/\/.*\n/g, '') // remove //
+            let transformedCode = code.replace(/[ \t]*\/\/.*\n/g, '') // remove //
                 .replace(/[ \t]*\/\*[\s\S]*?\*\//g, '') // remove /* */
-                .replace(/\n{2,}/g, '\n'); // # \n+ to \n
+                .replace(/\n{1,}/g, '\\n') // # \n+ to \n
+                .replace(/\r{1,}/g, '\\n') // # \r+ to \n
+                .replace(/"/g, '\\"');
+            transformedCode = `export default "${transformedCode}";`;
             return {
-                code: `export default \`${transformedCode}\`;`,
+                code: transformedCode,
                 map: { mappings: '' }
             };
         }
@@ -19,27 +22,50 @@ function glsl() {
 }
 const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${pkg.license}\n * (c) 2016-${new Date().getFullYear()} maptalks.org\n */`;
 
-module.exports = {
-    input: 'src/index.js',
-    plugins: [
-        glsl(),
-        resolve({
-            module : true,
-            jsnext : true,
-            main : true
-        }),
-        commonjs(),
-        babel({
-            exclude: ['node_modules/**']
-        }),
-    ],
-    output: [
-        {
-            'sourcemap': false,
-            'format': 'umd',
-            'name': 'reshader',
-            'banner': banner,
-            'file': pkg.main
-        }
-    ]
-};
+const plugins = [
+    glsl(),
+    resolve({
+        module : true,
+        jsnext : true,
+        main : true
+    }),
+    commonjs(),
+];
+
+module.exports = [
+    {
+        input: 'src/index.js',
+        plugins,
+        output: [
+            {
+                'sourcemap': true,
+                'format': 'umd',
+                'name': 'reshader',
+                'banner': banner,
+                'file': 'dist/reshadergl.js'
+            }
+        ]
+    },
+    {
+        input: 'src/index.js',
+        plugins : plugins.concat([terser({
+            // mangle: {
+            //     properties: {
+            //         'regex' : /^_/,
+            //         'keep_quoted' : true
+            //     }
+            // },
+            output : {
+                comments : '/^!/'
+            }
+        })]),
+        output: [
+            {
+                'sourcemap': false,
+                'format': 'es',
+                'banner': banner,
+                'file': pkg.module
+            }
+        ]
+    },
+];
