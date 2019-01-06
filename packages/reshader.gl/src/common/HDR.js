f8d1592995535c96b2c8453ee7719c7538b74e36
fuzhenn <fuzhen@maptalks.org>
2019-01-06 16:05:14 +0800

    fix lint

--- packages/reshader.gl/src/common/HDR.js
+++ packages/reshader.gl/src/common/HDR.js
@@ -8,8 +8,7 @@ function rgbe2float(rgbe, buffer, offset, exposure) {
         buffer[offset + 0] = rgbe[0] * f;
         buffer[offset + 1] = rgbe[1] * f;
         buffer[offset + 2] = rgbe[2] * f;
-    }
-    else {
+    } else {
         buffer[offset + 0] = 0;
         buffer[offset + 1] = 0;
         buffer[offset + 2] = 0;
@@ -44,7 +43,7 @@ function oldReadColors(scan, buffer, offset, xmax) {
         if (scan[x][0] === 1 && scan[x][1] === 1 && scan[x][2] === 1) {
             // exp is count of repeated pixels
             for (var i = (scan[x][3] << rshift) >>> 0; i > 0; i--) {
-                copyrgbe(scan[x-1], scan[x]);
+                copyrgbe(scan[x - 1], scan[x]);
                 x++;
                 len--;
             }
@@ -63,7 +62,7 @@ function readColors(scan, buffer, offset, xmax) {
         return oldReadColors(scan, buffer, offset, xmax);
     }
     var i = buffer[offset++];
-    if (i != 2) {
+    if (i !== 2) {
         return oldReadColors(scan, buffer, offset - 1, xmax);
     }
     scan[0][1] = buffer[offset++];
@@ -73,8 +72,8 @@ function readColors(scan, buffer, offset, xmax) {
     if ((((scan[0][2] << 8) >>> 0) | i) >>> 0 !== xmax) {
         return null;
     }
-    for (var i = 0; i < 4; i++) {
-        for (var x = 0; x < xmax;) {
+    for (let i = 0; i < 4; i++) {
+        for (let x = 0; x < xmax;) {
             var code = buffer[offset++];
             if (code > 128) {
                 code = (code & 127) >>> 0;
@@ -93,74 +92,74 @@ function readColors(scan, buffer, offset, xmax) {
 }
 
 
-    // http://www.graphics.cornell.edu/~bjw/rgbe.html
-    // Blender source
-    // http://radsite.lbl.gov/radiance/refer/Notes/picture_format.html
-    function parseRGBE(arrayBuffer, exposure) {
-        if (exposure == null) {
-            exposure = 0;
-        }
-        var data = new Uint8Array(arrayBuffer);
-        var size = data.length;
-        if (uint82string(data, 0, 2) !== '#?') {
-            return;
-        }
-        // find empty line, next line is resolution info
-        for (var i = 2; i < size; i++) {
-            if (toChar(data[i]) === '\n' && toChar(data[i+1]) === '\n') {
-                break;
-            }
-        }
-        if (i >= size) { // not found
-            return;
-        }
-        // find resolution info line
-        i += 2;
-        var str = '';
-        for (; i < size; i++) {
-            var _char = toChar(data[i]);
-            if (_char === '\n') {
-                break;
-            }
-            str += _char;
-        }
-        // -Y M +X N
-        var tmp = str.split(' ');
-        var height = parseInt(tmp[1]);
-        var width = parseInt(tmp[3]);
-        if (!width || !height) {
-            return;
-        }
-
-        // read and decode actual data
-        var offset = i+1;
-        var scanline = [];
-        // memzero
-        for (var x = 0; x < width; x++) {
-            scanline[x] = [];
-            for (var j = 0; j < 4; j++) {
-                scanline[x][j] = 0;
-            }
-        }
-        var pixels = new Array(width * height * 4);
-        var offset2 = 0;
-        for (var y = 0; y < height; y++) {
-            var offset = readColors(scanline, data, offset, width);
-            if (!offset) {
-                return null;
-            }
-            for (var x = 0; x < width; x++) {
-                rgbe2float(scanline[x], pixels, offset2, exposure);
-                offset2 += 4;
-            }
-        }
-
-        return {
-            width : width,
-            height : height,
-            pixels : pixels
+// http://www.graphics.cornell.edu/~bjw/rgbe.html
+// Blender source
+// http://radsite.lbl.gov/radiance/refer/Notes/picture_format.html
+function parseRGBE(arrayBuffer, exposure) {
+    if (exposure == null) {
+        exposure = 0;
+    }
+    var data = new Uint8Array(arrayBuffer);
+    var size = data.length;
+    if (uint82string(data, 0, 2) !== '#?') {
+        return null;
+    }
+    // find empty line, next line is resolution info
+    for (var i = 2; i < size; i++) {
+        if (toChar(data[i]) === '\n' && toChar(data[i + 1]) === '\n') {
+            break;
         }
     }
+    if (i >= size) { // not found
+        return null;
+    }
+    // find resolution info line
+    i += 2;
+    var str = '';
+    for (; i < size; i++) {
+        var _char = toChar(data[i]);
+        if (_char === '\n') {
+            break;
+        }
+        str += _char;
+    }
+    // -Y M +X N
+    var tmp = str.split(' ');
+    var height = parseInt(tmp[1]);
+    var width = parseInt(tmp[3]);
+    if (!width || !height) {
+        return null;
+    }
+
+    // read and decode actual data
+    var offset = i + 1;
+    var scanline = [];
+    // memzero
+    for (var x = 0; x < width; x++) {
+        scanline[x] = [];
+        for (var j = 0; j < 4; j++) {
+            scanline[x][j] = 0;
+        }
+    }
+    var pixels = new Array(width * height * 4);
+    var offset2 = 0;
+    for (let y = 0; y < height; y++) {
+        let colorOffset = readColors(scanline, data, offset, width);
+        if (!colorOffset) {
+            return null;
+        }
+        for (let x = 0; x < width; x++) {
+            rgbe2float(scanline[x], pixels, offset2, exposure);
+            offset2 += 4;
+        }
+    }
+
+    return {
+        width : width,
+        height : height,
+        pixels : pixels
+    };
+}
 
 
 export default parseRGBE;
