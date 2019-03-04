d79aa739d8c34feb6af61f97546ebd4402f47117
fuzhenn <fuzhen@maptalks.org>
2019-03-04 10:42:56 +0800

    fix lint

--- packages/vt/test/unit/Layer.GeoJSON.spec.js
+++ packages/vt/test/unit/Layer.GeoJSON.spec.js
@@ -4,9 +4,9 @@ const { GeoJSONVectorTileLayer } = require('../../dist/maptalks.vt.js');
 // const deepEqual = require('fast-deep-equal');
 
 const points = {
-    type : 'FeatureCollection',
-    features : [
-        { type : 'Feature', geometry : { type : 'Point', coordinates : [114.25814, 30.58595] }, properties : { type : 1 }}
+    type: 'FeatureCollection',
+    features: [
+        { type: 'Feature', geometry: { type: 'Point', coordinates: [114.25814, 30.58595] }, properties: { type: 1 } }
     ]
 };
 
@@ -18,8 +18,8 @@ describe('GeoJSONVectorTileLayer', () => {
         container.style.height = '300px';
         document.body.appendChild(container);
         map = new maptalks.Map(container, {
-            center : [114.25814, 30.58595],
-            zoom : 19
+            center: [114.25814, 30.58595],
+            zoom: 19
         });
     });
 
@@ -30,7 +30,7 @@ describe('GeoJSONVectorTileLayer', () => {
 
     it('should stringify input geojson data', () => {
         const layer = new GeoJSONVectorTileLayer('gvt', {
-            data : points
+            data: points
         }).addTo(map);
         const target = JSON.parse(JSON.stringify(points));
         target.features.forEach((f, index) => {
@@ -42,7 +42,7 @@ describe('GeoJSONVectorTileLayer', () => {
 
     it('should fire workerready event', (done) => {
         const layer = new GeoJSONVectorTileLayer('gvt', {
-            data : points
+            data: points
         }).addTo(map);
         layer.on('workerready', e => {
             assert.ok(e);
