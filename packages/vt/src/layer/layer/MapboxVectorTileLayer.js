a4366642f23a90bcf1f66a1b582c262d1ddfec92
fuzhenn <fuzhen@maptalks.org>
2019-06-05 22:13:19 +0800

    fix MapboxVectorTileLayer's fromJSON

--- packages/vt/src/layer/layer/MapboxVectorTileLayer.js
+++ packages/vt/src/layer/layer/MapboxVectorTileLayer.js
@@ -6,8 +6,17 @@ export default class MapboxVectorTileLayer extends VectorTileLayer {
         const res = this.getMap().getResolution(z);
         return super.getTileUrl(x, y, getMapBoxZoom(res));
     }
+
+    static fromJSON(layerJSON) {
+        if (!layerJSON || layerJSON['type'] !== 'MapboxVectorTileLayer') {
+            return null;
+        }
+
+        return new MapboxVectorTileLayer(layerJSON['id'], layerJSON['options']);
+    }
 }
 
+MapboxVectorTileLayer.registerJSONType('MapboxVectorTileLayer');
 
 const MAX_RES = 2 * 6378137 * Math.PI / (256 * Math.pow(2, 20));
 function getMapBoxZoom(res) {
