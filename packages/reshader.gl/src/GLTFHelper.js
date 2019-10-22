de0052f99ad3f8e025370860f87ec5af3a66d510
fuzhenn <fuzhen@maptalks.org>
2019-10-22 14:20:39 +0800

    fix GLTFHelper.load

--- packages/reshader.gl/src/GLTFHelper.js
+++ packages/reshader.gl/src/GLTFHelper.js
@@ -1,11 +1,11 @@
 import * as gltf from '@maptalks/gltf-loader';
 import GLTFPack from './gltf/GLTFPack';
 
-export function getJSON(url, options) {
+function getJSON(url, options) {
     return gltf.Ajax.getJSON(url, options);
 }
 
-export function getArrayBuffer(url, options) {
+function getArrayBuffer(url, options) {
     return gltf.Ajax.getArrayBuffer(url, options);
 }
 
@@ -15,11 +15,11 @@ export function load(url) {
     const postfix = url.slice(url.lastIndexOf('.')).toLowerCase();
     if (postfix === '.gltf') {
         return getJSON(url, {}).then(json => {
-            return load(root, json);
+            return loadGLTF(root, json);
         });
     } else if (postfix === '.glb') {
         return getArrayBuffer(url, {}).then(bin => {
-            return load(root, { buffer : bin.data, byteOffset : 0 });
+            return loadGLTF(root, { buffer : bin.data, byteOffset : 0 });
         });
     }
     return null;
@@ -30,7 +30,7 @@ export function exportGLTFPack(gltf, regl) {
     return gltfpack;
 }
 
-// export function load(root, options) {
-//     const loader = new gltf.GLTFLoader(root, options);
-//     return loader.load();
-// }
+function loadGLTF(root, options) {
+    const loader = new gltf.GLTFLoader(root, options);
+    return loader.load();
+}
