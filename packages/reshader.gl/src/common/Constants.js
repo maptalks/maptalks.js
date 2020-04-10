58426d2118a2aad5e2b3495e80d7ffbd27d1b36e
fuzhenn <fuzhen@maptalks.org>
2020-04-10 20:37:11 +0800

    gl extension constants

--- packages/reshader.gl/src/common/Constants.js
+++ packages/reshader.gl/src/common/Constants.js
@@ -1,1 +1,12 @@
 export const KEY_DISPOSED = '__reshader_disposed';
+export const WEBGL_EXTENSIONS = [
+    'ANGLE_instanced_arrays',
+    'OES_element_index_uint',
+    'OES_standard_derivatives'
+];
+export const WEBGL_OPTIONAL_EXTENSIONS = [
+    'OES_vertex_array_object',
+    'OES_texture_half_float', 'OES_texture_half_float_linear',
+    'OES_texture_float', 'OES_texture_float_linear',
+    'WEBGL_depth_texture', /*'WEBGL_draw_buffers', */'EXT_shader_texture_lod'
+];
