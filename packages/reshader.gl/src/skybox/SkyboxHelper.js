c3dcf88b14d5fa0d7e737077a8a1374dbd81985d
LiuBing <471071362@qq.com>
2020-05-21 10:55:41 +0800

    Skybox (#113)
    * 可视域分析的实现 (#84)
    
    * 1、实现可视域分析的ViewShedPass。2、增加相关demo
    
    * 修改demo
    
    * 将绘制可视域场景的逻辑以include的方式，放到绘制场景的shader逻辑中去
    
    * 修改pr中提到的一些问题
    
    * 增加与可视域相关的变量名的前缀
    
    * 去掉对modelMatrix，localPositionMatrix ，getPosition的依赖
    
    * 修改skybox，可以动态更改skybox模糊度
    
    * 增加ambient模式
    
    * 删除测试模型

--- packages/reshader.gl/src/skybox/SkyboxHelper.js
+++ packages/reshader.gl/src/skybox/SkyboxHelper.js
@@ -4,7 +4,8 @@ import skyboxFS from './skybox.frag';
 
 let command, commandHDR;
 let config;
-
+//sketchfab Spherical harmonics
+const uDiffuseSPH = [[0.1341, 0.1298, 0.1150], [0.0306, 0.0478, 0.0725], [-0.0145, -0.0110, -0.0054], [0.0437, 0.0570, 0.0679], [0.0492, 0.0562, 0.0677], [-0.0051, -0.0043, -0.0057], [-0.0160, -0.0171, -0.0167], [0.0305, 0.0283, 0.0209], [-0.0075, -0.0102, -0.0157]];
 /**
  * Draw skybox with given cubemap and camera position
  *
@@ -15,7 +16,7 @@ let config;
  * @param {Boolean} [useHDR=false] - whether export color using HDR
  * @param {REGLFramebuffer} [frameBuffer=false] - the framebuffer to render to
  */
-export function drawSkybox(regl, cubeMap, viewMatrix, projMatrix, useHDR, frameBuffer, decRGBM) {
+export function drawSkybox(regl, cubeMap, viewMatrix, projMatrix, useHDR, frameBuffer, decRGBM, useMipmap, blur, uSize, uBackgroundExposure, uEnvironmentExposure, useAmbient) {
     let drawCommand;
     config = config || {
         vert : skyboxVS,
@@ -24,6 +25,19 @@ export function drawSkybox(regl, cubeMap, viewMatrix, projMatrix, useHDR, frameB
         },
         uniforms : {
             'cubeMap' : regl.prop('cubeMap'),
+            'bias' : regl.prop('bias'),
+            'uSize': regl.prop('uSize'),
+            'uBackgroundExposure': regl.prop('uBackgroundExposure'),
+            'uEnvironmentExposure' : regl.prop('uEnvironmentExposure'),
+            'uDiffuseSPH[0]': regl.prop('uDiffuseSPH[0]'),
+            'uDiffuseSPH[1]': regl.prop('uDiffuseSPH[1]'),
+            'uDiffuseSPH[2]': regl.prop('uDiffuseSPH[2]'),
+            'uDiffuseSPH[3]': regl.prop('uDiffuseSPH[3]'),
+            'uDiffuseSPH[4]': regl.prop('uDiffuseSPH[4]'),
+            'uDiffuseSPH[5]': regl.prop('uDiffuseSPH[5]'),
+            'uDiffuseSPH[6]': regl.prop('uDiffuseSPH[6]'),
+            'uDiffuseSPH[7]': regl.prop('uDiffuseSPH[7]'),
+            'uDiffuseSPH[8]': regl.prop('uDiffuseSPH[8]'),
             'viewMatrix' : regl.prop('viewMatrix'),
             'projMatrix' : regl.prop('projMatrix')
         },
@@ -38,6 +52,11 @@ export function drawSkybox(regl, cubeMap, viewMatrix, projMatrix, useHDR, frameB
     if (decRGBM) {
         frag = '#define DEC_RGBM 1 \n' + frag;
     }
+    if (useMipmap) {
+        frag = '#define USE_MIPMAP 1 \n' + frag;
+    } else if (useAmbient) {
+        frag = '#define USE_AMBIENT 1 \n' + frag;
+    }
     if (useHDR) {
         config['frag'] = '#define USE_HDR 1\n' + frag;
         drawCommand = commandHDR = commandHDR || regl(config);
@@ -46,9 +65,13 @@ export function drawSkybox(regl, cubeMap, viewMatrix, projMatrix, useHDR, frameB
         drawCommand = command = command || regl(config);
     }
 
-
     drawCommand({
         cubeMap,
+        uBackgroundExposure,
+        uEnvironmentExposure,
+        bias: blur,
+        uSize,
+        uDiffuseSPH,
         viewMatrix,
         projMatrix,
         frameBuffer
--- packages/reshader.gl/src/skybox/skybox.frag
+++ packages/reshader.gl/src/skybox/skybox.frag
@@ -1,8 +1,26 @@
+#ifdef GL_EXT_shader_texture_lod
+    #extension GL_EXT_shader_texture_lod : enable
+    #define textureCubeLod(tex, uv, lod) textureCubeLodEXT(tex, uv, lod)
+#else
+    #define textureCubeLod(tex, uv, lod) textureCube(tex, uv, lod)
+#endif
+#extension GL_OES_standard_derivatives : enable
 precision highp float;
 
 varying vec3 vWorldPos;
 
 uniform samplerCube cubeMap;
+
+#ifdef USE_MIPMAP
+    uniform float bias;
+    uniform float uSize;
+    uniform float uEnvironmentExposure;
+    uniform float uBackgroundExposure;
+#endif
+
+#ifdef USE_AMBIENT
+    uniform vec3 uDiffuseSPH[9];
+#endif
 
 vec4 encodeRGBM(const in vec3 color, const in float range) {
     if(range <= 0.0) return vec4(color, 1.0);
@@ -19,11 +37,55 @@ vec3 decodeRGBM(const in vec4 color, const in float range) {
     return range * color.rgb * color.a;
 }
 
+vec4 textureCubeFixed(const in samplerCube tex, const in vec3 R, const in float size, const in float bias) {
+    vec3 dir = R;
+    float scale = 1.0 - 1.0 / size;
+    vec3 absDir = abs(dir);
+    float M = max(max(absDir.x, absDir.y), absDir.z);
+    if (absDir.x != M) dir.x *= scale;
+    if (absDir.y != M) dir.y *= scale;
+    if (absDir.z != M) dir.z *= scale;
+    return textureCubeLod(tex, dir, bias);
+}
+
+vec3 computeDiffuseSPH(const in vec3 normal, const in vec3 uDiffuseSPH[9]) {
+    float x = normal.x;
+    float y = normal.y;
+    float z = normal.z;
+    vec3 result = (
+        uDiffuseSPH[0] +
+
+        uDiffuseSPH[1] * x +
+        uDiffuseSPH[2] * y +
+        uDiffuseSPH[3] * z +
+
+        uDiffuseSPH[4] * z * x +
+        uDiffuseSPH[5] * y * z +
+        uDiffuseSPH[6] * y * x +
+        uDiffuseSPH[7] * (3.0 * z * z - 1.0) +
+        uDiffuseSPH[8] * (x*x - y*y)
+        );
+    return max(result, vec3(0.0));
+}
+
+float pseudoRandom(const in vec2 fragCoord) {
+    vec3 p3 = fract(vec3(fragCoord.xyx) * .1031);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
 void main()
 {
-
-    vec4 envColor = textureCube(cubeMap, vWorldPos);
-
+    vec4 envColor;
+    #ifdef USE_AMBIENT
+        vec3 normal = normalize(vWorldPos + mix(-0.5/255.0, 0.5/255.0, pseudoRandom(gl_FragCoord.xy))*2.0);
+        envColor = vec4(computeDiffuseSPH(normal, uDiffuseSPH), 1.0);
+    #elif defined(USE_MIPMAP)
+        envColor = textureCubeFixed(cubeMap, vWorldPos, uSize, bias);
+        envColor.rgb *= uEnvironmentExposure * uBackgroundExposure;
+    #else
+        envColor = textureCube(cubeMap, vWorldPos);
+    #endif
     #ifdef ENC_RGBM
     	gl_FragColor = encodeRGBM(envColor.rgb, 7.0);
     #elif defined(DEC_RGBM)
