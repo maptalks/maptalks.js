c7b2364dabaf80eb85a3bf71cb506fdbefb86739
fuzhenn <fuzhen@maptalks.org>
2020-06-23 15:26:12 +0800

    upgrade gl@0.16.3 and fix specs

--- packages/vt.basic/test/specs/update.spec.js
+++ packages/vt.basic/test/specs/update.spec.js
@@ -494,7 +494,7 @@ describe('update style specs', () => {
             const pixel = readPixel(canvas, canvas.width / 2 + 40, canvas.height / 2);
             if (pixel[0] > 0) {
                 if (!painted) {
-                    assert.deepEqual(pixel, [11, 10, 51, 255]);
+                    assert.deepEqual(pixel, [15, 13, 52, 255]);
 
                     material.baseColorTexture = undefined;
                     layer.updateSymbol(0, { material });
