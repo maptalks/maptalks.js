6914b0406f4542b23c89ff1eb4cdf7d5252b6cf7
fuzhenn <fuzhen@maptalks.org>
2020-07-10 16:51:31 +0800

    code cleaning in OutlinePass.js (though unused)

--- packages/reshader.gl/src/outline/OutlinePass.js
+++ packages/reshader.gl/src/outline/OutlinePass.js
@@ -5,15 +5,11 @@ import quadVert from '../shader/glsl/quad.vert';
 import extentFrag from './glsl/extent.frag';
 import outlineFrag from './glsl/outline.frag';
 import sceneVert from './glsl/sceneVert.vert';
-import { isFunction } from '../common/Util';
 
 export default class OutlinePass {
-    constructor(renderer, viewport)  {
+    constructor(renderer)  {
         this._renderer = renderer;
         this.regl = renderer.regl;
-        this._viewport = viewport;
-        this._width = 1;
-        this._height = 1;
         this._init();
     }
 
@@ -22,25 +18,32 @@ export default class OutlinePass {
             return;
         }
         this._clear();
-        this._resize();
-        if (!meshes || !meshes.length) {
-            return;
-        }
+        this._resize(targetFBO);
         const renderScene = new Scene(meshes);
         //绘制有outline的Meshes的范围
-        this._drawExtentFBO(renderScene, projViewMatrix);
+        this._drawExtent(renderScene, projViewMatrix);
         //绘制有outline的Meshes的边线，同时和targetFBO做混合，画到targetFBO
-        this._drawOutlineFBO(lineColor, lineWidth, targetFBO);
+        this._drawOutline(lineColor, lineWidth, targetFBO);
     }
 
     _init() {
         this.fboExtent = this._createFBO();
+        const viewport = {
+            x: 0,
+            y: 0,
+            width: () => {
+                return this.fboExtent.width;
+            },
+            height: () => {
+                return this.fboExtent.height;
+            }
+        };
         this.extentShader = new MeshShader({
             vert: sceneVert,
             frag: extentFrag,
             positionAttribute: 'POSITION',
             extraCommandProps: {
-                viewport: this._viewport,
+                viewport,
                 cull: {
                     enable: false
                 }
@@ -57,7 +60,8 @@ export default class OutlinePass {
                 'lineWidth'
             ],
             positionAttribute : 'POSITION',
-            extraCommandProps: { viewport: this._viewport,
+            extraCommandProps: {
+                viewport,
                 depth: {
                     enable: true,
                     mask: false,
@@ -75,15 +79,15 @@ export default class OutlinePass {
         });
     }
 
-    _drawExtentFBO(scene, matrix) {
+    _drawExtent(scene, matrix) {
         this._renderer.render(this.extentShader, {
             'projViewMatrix': matrix
         }, scene, this.fboExtent);
     }
 
-    _drawOutlineFBO(lineColor, lineWidth, targetFBO) {
+    _drawOutline(lineColor, lineWidth, targetFBO) {
         this._renderer.render(this.outlineShader, {
-            'texSize' : [this._width / 2, this._height * 2 / 2],
+            'texSize' : [targetFBO.width, targetFBO.height],
             'visibleEdgeColor' : lineColor || [1, 0, 0],
             'maskTexture' : this.fboExtent,
             'lineWidth': lineWidth || 1
@@ -93,8 +97,8 @@ export default class OutlinePass {
     _createFBO() {
         return this.regl.framebuffer({
             color: this.regl.texture({
-                width: this._width,
-                height: this._height,
+                width: 2,
+                height: 2,
                 wrap: 'clamp',
                 mag: 'linear',
                 min: 'linear'
@@ -114,14 +118,16 @@ export default class OutlinePass {
     dispose() {
         if (this.fboExtent) {
             this.fboExtent.destroy();
+            this.extentShader.dispose();
+            this.outlineShader.dispose();
+            delete this.fboExtent;
         }
     }
 
-    _resize() {
-        this._width = isFunction(this._viewport.width.data) ? this._viewport.width.data() : this._viewport.width;
-        this._height = isFunction(this._viewport.height.data) ? this._viewport.height.data() : this._viewport.height;
-        if (this.fboExtent && (this.fboExtent.width !== this._width || this.fboExtent.height !== this._height)) {
-            this.fboExtent.resize(this._width, this._height);
+    _resize(sourceTex) {
+        const { width, height } = sourceTex;
+        if (this.fboExtent.width !== width || this.fboExtent.height !== height) {
+            this.fboExtent.resize(width, height);
         }
     }
 }
