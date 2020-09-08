0e32dd02bf8d544bea6d6120c59f5fc48d408086
fuzhenn <fuzhen@maptalks.org>
2020-09-08 18:53:37 +0800

    add tests for style with $root, close #318

--- packages/vt.basic/test/integration/vector-fixtures/polygon/polygon-pattern/index.js
+++ packages/vt.basic/test/integration/vector-fixtures/polygon/polygon-pattern/index.js
@@ -4,18 +4,16 @@ const maptalks = require('maptalks');
 const polygon0 = new maptalks.Polygon([
     [0, 0], [1, 0], [1, 1], [0, 1], [0, 0]
 ], {
-    symbol: {
-        polygonPatternFile: 'file://' + path.resolve(__dirname, '../../../resources/1.png'),
-        polygonOpacity: 1
+    properties: {
+        type: 0
     }
 });
 
 const polygon1 = new maptalks.Polygon([
     [1, 1], [2, 1], [2, 2], [1, 2], [1, 1]
 ], {
-    symbol: {
-        polygonPatternFile: 'file://' + path.resolve(__dirname, '../../../resources/avatar.jpg'),
-        polygonOpacity: 1
+    properties: {
+        type: 1
     }
 });
 
@@ -25,4 +23,25 @@ module.exports = {
         center: [1, 1],
         zoom: 6
     },
+    options: {
+        style: {
+            $root: 'file://' + path.resolve(__dirname, '../../../resources'),
+            style: [
+                {
+                    filter: ['==', 'type', 1],
+                    symbol: {
+                        polygonPatternFile: '{$root}/avatar.jpg',
+                        polygonOpacity: 1
+                    }
+                },
+                {
+                    filter: ['==', 'type', 0],
+                    symbol: {
+                        polygonPatternFile: '{$root}/1.png',
+                        polygonOpacity: 1
+                    }
+                },
+            ]
+        },
+    }
 };
