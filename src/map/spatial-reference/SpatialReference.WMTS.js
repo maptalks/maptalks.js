41c785513fbc91e53f816e8cf62be356b4876f50
hudeyi <mehudeyi@163.com>
2021-01-07 12:01:47 +0800

    wmts support geoserver

--- src/map/spatial-reference/SpatialReference.WMTS.js
+++ src/map/spatial-reference/SpatialReference.WMTS.js
@@ -2,6 +2,18 @@ import { isString } from '../../core/util';
 import Ajax from '../../core/Ajax';
 import SpatialReference from './SpatialReference';
 
+function getProjectionCode(code) {
+    let newcode = '';
+    const codeArray = code.split('');
+    for (let len = codeArray.length, i = len - 1; i >= 0; i--) {
+        if (!isNaN(codeArray[i])) {
+            newcode = codeArray[i] + newcode;
+        } else {
+            break;
+        }
+    }
+    return newcode;
+}
 
 function getProjection(projection) {
     let prj = (projection.indexOf('EPSG') > -1 ? projection : 'EPSG:' + projection);
@@ -21,8 +33,46 @@ function strReplace(str, repArray = []) {
     return str;
 }
 
+function getTransformValue(options) {
+    const { projection, isArcgis, isGeoServer, isSuperMap } = options;
+    //transform value, ArcGIS is different from others
+    let transformValue = 0.0002645833333333333;
+    if (isArcgis || isGeoServer || isSuperMap) {
+        transformValue = 0.00028;
+    }
+    if (projection && projection.indexOf('4326') > -1) {
+        transformValue = 2.3767925226029154e-9;
+        if (isArcgis || isSuperMap) {
+            transformValue = 2.518101729011901e-9;
+        }
+        if (isGeoServer) {
+            transformValue = 2.51528279553466e-9;
+        }
+    }
+    return transformValue;
+}
+
+function getTileMatrixSet(TileMatrixSets, TileMatrixSetLink) {
+    for (let i = 0; i < TileMatrixSets.length; i++) {
+        let TileMatrixSet = TileMatrixSets[i];
+        TileMatrixSet = TileMatrixSet.getElementsByTagName('ows:Identifier')[0];
+        if (TileMatrixSet) {
+            if (TileMatrixSet.textContent === TileMatrixSetLink) {
+                return TileMatrixSets[i];
+            }
+        }
+    }
+    return null;
+}
+
 function parseWMTSXML(str, requestUrl, options) {
     //IE test success
+    if (options.isArcgis == null) {
+        options.isArcgis = str.indexOf('arcgis') > -1;
+    }
+    if (options.isSuperMap == null) {
+        options.isSuperMap = str.indexOf('supermap') > -1;
+    }
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(str, 'text/xml');
     const content = xmlDoc.querySelectorAll('Contents')[0];
@@ -56,58 +106,72 @@ function parseWMTSXML(str, requestUrl, options) {
         if (layerName) {
             layerName = layerName.textContent;
         }
-        const resourceURL = layer.querySelectorAll('ResourceURL')[0];
-        let url = '';
-        if (resourceURL) {
-            url = resourceURL.attributes.template.value;
+        const tileMatrixSetLinks = layer.getElementsByTagName('TileMatrixSetLink');
+        if (tileMatrixSetLinks.length === 0) {
+            continue;
         }
-        const { resolutions, tileSize, tileSystem, projection, TileMatrixSet } = parseTileMatrixSet(TileMatrixSets, i, options);
-        //not find ServerURL
-        if (!url.length) {
-            url = requestUrl.substr(0, requestUrl.lastIndexOf('?'));
-            url += '?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER={LAYER}&STYLE={Style}&TILEMATRIXSET={TileMatrixSet}&FORMAT=tiles&TILEMATRIX={TileMatrix}&TILEROW={TileRow}&TILECOL={TileCol}';
-        }
-        const urlTemplate = strReplace(url, [
-            ['{LAYER}', layerName],
-            ['{Layer}', layerName],
-            ['{layer}', layerName],
-            ['{STYLE}', style],
-            ['{Style}', style],
-            ['{style}', style],
-            ['{TileMatrixSet}', TileMatrixSet],
-            ['{TileMatrix}', '{z}'],
-            ['{TileRow}', '{y}'],
-            ['{TileCol}', '{x}'],
-        ]);
-        result.push({
-            tileSize,
-            tileSystem,
-            spatialReference: {
-                resolutions,
-                projection
-            },
-            urlTemplate,
-            info: {
-                layerName, TileMatrixSet, style, tileSize, tileSystem, resolutions, projection, urlTemplate
+        for (let j = 0, len1 = tileMatrixSetLinks.length; j < len1; j++) {
+            let tileMatrixSetLink = tileMatrixSetLinks[j];
+            tileMatrixSetLink = tileMatrixSetLink.getElementsByTagName('TileMatrixSet')[0];
+            if (tileMatrixSetLink) {
+                tileMatrixSetLink = tileMatrixSetLink.textContent;
             }
-        });
+            const tileMatrixSet = getTileMatrixSet(TileMatrixSets, tileMatrixSetLink);
+            if (!tileMatrixSet) {
+                continue;
+            }
+            const resourceURL = layer.querySelectorAll('ResourceURL')[0];
+            let url = '';
+            if (resourceURL) {
+                url = resourceURL.attributes.template.value;
+            }
+            const { resolutions, tileSize, tileSystem, projection, TileMatrixSet, isGeoServer, levelStr } = parseTileMatrixSet(tileMatrixSet, options);
+            //not find ServerURL
+            if (!url.length) {
+                url = requestUrl.substr(0, requestUrl.lastIndexOf('?'));
+                url += '?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER={LAYER}&STYLE={Style}&TILEMATRIXSET={TileMatrixSet}&FORMAT={tiles}&TILEMATRIX={TileMatrix}&TILEROW={TileRow}&TILECOL={TileCol}';
+            }
+            const urlTemplate = strReplace(url, [
+                ['{LAYER}', layerName],
+                ['{Layer}', layerName],
+                ['{layer}', layerName],
+                ['{STYLE}', style],
+                ['{Style}', style],
+                ['{style}', style],
+                ['{TileMatrixSet}', TileMatrixSet],
+                ['{TileMatrix}', isGeoServer ? `${levelStr}:{z}` : '{z}'],
+                ['{TileRow}', '{y}'],
+                ['{TileCol}', '{x}'],
+                ['{tiles}', isGeoServer ? 'image/png' : 'tiles'],
+            ]);
+            result.push({
+                tileSize,
+                tileSystem,
+                spatialReference: {
+                    resolutions,
+                    projection
+                },
+                urlTemplate,
+                info: {
+                    layerName, TileMatrixSet, style, tileSize, tileSystem, resolutions, projection, urlTemplate
+                }
+            });
+        }
+
     }
     return result;
 }
 
-function parseTileMatrixSet(TileMatrixSets, index, options = {}) {
-    const TileMatrixSet = TileMatrixSets[index];
+function parseTileMatrixSet(TileMatrixSet, options = {}) {
     const TileMatrixs = TileMatrixSet.getElementsByTagName('TileMatrix');
     const resolutions = [], tileSystem = [], tileSize = [];
-    let projection, tset;
+    let projection, tset, isGeoServer = false, levelStr;
     if (!projection) {
         const supportedCRS = TileMatrixSet.getElementsByTagName('ows:SupportedCRS')[0];
         if (supportedCRS) {
             projection = supportedCRS.textContent;
             projection = projection.split('EPSG')[1];
-            while (projection.indexOf(':') > -1) {
-                projection = projection.replace(':', '');
-            }
+            projection = getProjectionCode(projection);
             projection = getProjection(projection);
         }
     }
@@ -117,22 +181,21 @@ function parseTileMatrixSet(TileMatrixSets, index, options = {}) {
             tset = tset.textContent;
         }
     }
-    //transform value, ArcGIS is different from others
-    let transformValue = 0.0002645833333333333;
-    if (options.isArcgis) {
-        transformValue = 0.00028;
-    }
-    if (projection && projection.indexOf('4326') > -1) {
-        transformValue = 2.3767925226029154e-9;
-        if (options.isArcgis) {
-            transformValue = 2.518101729011901e-9;
-        }
-    }
+    options.projection = projection;
     let minLevel = Infinity;
     for (let index = 0; index < TileMatrixs.length; index++) {
         const TileMatrix = TileMatrixs[index];
         let level = TileMatrix.getElementsByTagName('ows:Identifier')[0].textContent;
-        level = parseInt(level);
+        if (isNaN(parseInt(level))) {
+            levelStr = level.substr(0, level.lastIndexOf(':'));
+            level = level.split(':');
+            level = level[level.length - 1];
+            level = parseInt(level);
+            isGeoServer = true;
+            options.isGeoServer = true;
+        } else {
+            level = parseInt(level);
+        }
         minLevel = Math.min(minLevel, level);
         const ScaleDenominator = TileMatrix.getElementsByTagName('ScaleDenominator')[0].textContent;
         const TopLeftCorner = TileMatrix.getElementsByTagName('TopLeftCorner')[0].textContent;
@@ -153,6 +216,7 @@ function parseTileMatrixSet(TileMatrixSets, index, options = {}) {
                 tileSystem.push(1, -1, x, y);
             }
         }
+        const transformValue = getTransformValue(options);
         const res = parseFloat(ScaleDenominator) * transformValue;
         resolutions.push(res);
     }
@@ -166,7 +230,7 @@ function parseTileMatrixSet(TileMatrixSets, index, options = {}) {
         }
     }
     return {
-        resolutions, tileSize, tileSystem, projection, TileMatrixSet: tset
+        resolutions, tileSize, tileSystem, projection, TileMatrixSet: tset, isGeoServer, levelStr
     };
 }
 
