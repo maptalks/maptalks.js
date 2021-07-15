fee351206e1b57eed27794b9d3249a8f09597937
fuzhenn <fuzhen@maptalks.org>
2021-07-15 11:51:42 +0800

    add spec for ground texture scale, fix #341

--- packages/vt.basic/test/integration/fixtures/post-process/ground-scale-tex/index.js
+++ packages/vt.basic/test/integration/fixtures/post-process/ground-scale-tex/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const material = {
     'baseColorFactor': [1, 1, 1, 1],
     'baseColorTexture': 'file://' + path.resolve(__dirname, '../../../resources/1.png'),
+    'uvScale': [0.5, 0.5],
+    'uvOffset': [0.1, 0.1],
     'roughnessFactor': 0,
     'metalnessFactor': 1,
     'outputSRGB': 0
