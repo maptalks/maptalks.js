e33d2dc23c7bd29619681f21b897250a328f9abd
sakitam-fdd <smilefdd@gmail.com>
2021-07-30 16:03:50 +0800

    fixed(release-config): zip file cyclic dependence

--- release.config.js
+++ release.config.js
@@ -1,11 +1,13 @@
+const ora = require('ora');
 const path = require('path');
 const fs = require('fs-extra');
 const chalk = require('chalk');
 const archiver = require('archiver');
+const rimraf = require('rimraf');
 
 const pkg = fs.readJsonSync(path.resolve(__dirname, 'package.json'));
 
-const files = ['LICENSE', 'ACKNOWLEDGEMENT', 'docs'];
+const files = ['LICENSE', 'ACKNOWLEDGEMENT', 'docs/api'];
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
@@ -15,7 +17,13 @@ const copyFiles = async () => {
   try {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      await fs.copy(file, `dist/${file}`);
+      const arr = file.split('/');
+      const hasSubDir = arr.length > 1;
+      if (hasSubDir) {
+        await fs.copy(file, `dist/${arr[arr.length - 1]}`);
+      } else {
+        await fs.copy(file, `dist/${file}`);
+      }
     }
     console.log(chalk.green(`[copyFiles] success.`));
   } catch (e) {
@@ -27,7 +35,13 @@ const removeFiles = async () => {
   try {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      await fs.remove(`dist/${file}`);
+      const arr = file.split('/');
+      const hasSubDir = arr.length > 1;
+      if (hasSubDir) {
+        await fs.remove(`dist/${arr[arr.length - 1]}`);
+      } else {
+        await fs.remove(`dist/${file}`);
+      }
     }
     console.log(chalk.green(`[removeFiles] success.`));
   } catch (e) {
@@ -36,8 +50,14 @@ const removeFiles = async () => {
 };
 
 const zip = () => {
+  const spinner = ora('archive dist ...').start();
   return new Promise(((resolve, reject) => {
     const name = `maptalks-${pkg.version}.zip`;
+    const fullPath = path.resolve(__dirname, `dist/${name}`);
+    if (fs.existsSync(fullPath)) {
+      rimraf.sync(fullPath);
+    }
+
     const output = fs.createWriteStream(__dirname + '/' + name);
     const archive = archiver('zip');
 
@@ -50,8 +70,13 @@ const zip = () => {
     // 注意此处会循环添加output文件，所以先创建临时目录，再进行压缩
     archive.directory('dist/', false);
 
-    archive.finalize();
-    resolve(name);
+    archive.finalize().then(() => {
+      resolve(name);
+    }).catch(err => {
+      reject(err);
+    }).finally(() => {
+      spinner.stop();
+    });
   }))
 };
 
@@ -59,10 +84,10 @@ const archiverProcess = async () => {
   await copyFiles();
   try {
     const name = await zip();
-    await fs.moveSync(path.resolve(__dirname, name), path.resolve(__dirname, `dist/${name}`))
+    fs.moveSync(path.resolve(__dirname, name), path.resolve(__dirname, `dist/${name}`));
     console.log(chalk.green(`[zip] success.`));
   } catch (e) {
-    console.log(chalk.red(`[zip] error.`));
+    console.log(chalk.red(`\n [zip] error.`));
   }
   await removeFiles();
 };
