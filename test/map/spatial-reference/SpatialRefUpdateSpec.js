0f22901412a9fdef0b6e736ccce4a8bbfd89db87
fuzhenn <fuzhen@maptalks.org>
2021-08-04 11:20:34 +0800

    fixing specs

--- test/map/spatial-reference/SpatialRefUpdateSpec.js
+++ test/map/spatial-reference/SpatialRefUpdateSpec.js
@@ -51,8 +51,8 @@ describe('SpatialReference.Update', function () {
         map.setBaseLayer(tileLayer);
         var tiles = tileLayer.getTiles().tileGrids[0].tiles;
         var tile = tiles[tiles.length - 1];
-        expect(tile.extent2d.xmin).to.be.eql(-256);
-        expect(tile.extent2d.ymax).to.be.eql(256);
+        expect(tile.extent2d.xmin).to.be.eql(0);
+        expect(tile.extent2d.ymax).to.be.eql(0);
     });
 
     var geometries = GEN_GEOMETRIES_OF_ALL_TYPES();
