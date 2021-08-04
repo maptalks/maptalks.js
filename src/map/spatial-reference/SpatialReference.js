1933b96dd93784688af0e52dc32e23be287d7f4e
fuzhenn <fuzhen@maptalks.org>
2021-08-04 10:51:11 +0800

    fix rootNodes in more general cases

--- src/map/spatial-reference/SpatialReference.js
+++ src/map/spatial-reference/SpatialReference.js
@@ -168,7 +168,7 @@ export default class SpatialReference {
             }
         }
         this._resolutions = resolutions;
-        this._pyramid = projection['code'] && projection['code'].toLowerCase() !== 'baidu';
+        this._pyramid = true;
         if (this._pyramid) {
             for (let i = 0; i < resolutions.length; i++) {
                 if (resolutions[i] && resolutions[i - 1]) {
