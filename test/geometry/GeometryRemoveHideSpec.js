2ae5873ddc4bf9f15e7d636cffbc91cd7b9e1fbc
fuzhenn <fuzhen@maptalks.org>
2021-09-10 21:09:21 +0800

    fixing specs

--- test/geometry/GeometryRemoveHideSpec.js
+++ test/geometry/GeometryRemoveHideSpec.js
@@ -140,12 +140,6 @@ function testRemoveHide(geometry, _context) {
         });
 
 
-        it('should be hided', function (done) {
-            test(function () {
-                geometry.hide();
-            }, done);
-        });
-
         it('should be removed by layer', function (done) {
             test(function () {
                 _context.layer.removeGeometry(geometry);
@@ -171,16 +165,18 @@ function testRemoveHide(geometry, _context) {
         });
 
         // it('should be removed with layer by map', function (done) {
-        //     if (type !== 'LineString') {
-        //         test(function () {
-        //             var map = _context.layer.getMap();
-        //             map.removeLayer(_context.layer);
-        //         }, done);
-        //     } else {
-        //         done();
-        //     }
-
+        //     test(function () {
+        //         var map = _context.layer.getMap();
+        //         map.removeLayer(_context.layer);
+        //     }, done);
         // });
+
+
+        it('should be hided', function (done) {
+            test(function () {
+                geometry.hide();
+            }, done);
+        });
 
 
         it('should be removed when it is being edited', function (done) {
