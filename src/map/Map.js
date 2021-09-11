78467781bedb053e94d78bbf0c8d5758efa5b3bd
fuzhenn <fuzhen@maptalks.org>
2021-09-11 09:59:48 +0800

    trying

--- src/map/Map.js
+++ src/map/Map.js
@@ -735,8 +735,9 @@ class Map extends Handlerable(Eventable(Renderable(Class))) {
      * @returns {Number}
      */
     getGLRes() {
-        const fullExtent = this.getSpatialReference().getFullExtent();
-        return (fullExtent.right - fullExtent.left) / (256 * Math.pow(2, 12));
+        // const fullExtent = this.getSpatialReference().getFullExtent();
+        // return (fullExtent.right - fullExtent.left) / (256 * Math.pow(2, 12));
+        return this._getResolution(14);
     }
 
     /**
