3fd8275018df355cc58f1d7911e8a1e1f90273f8
fuzhenn <fuzhen@maptalks.org>
2021-10-28 23:16:42 +0800

    fix global names to prevent mangling in transcoders.inject

--- packages/gl/src/transcoders.js
+++ packages/gl/src/transcoders.js
@@ -7,7 +7,7 @@ const getGlobal = function () {
 
 const globals = getGlobal();
 
-const transcoders = globals['___gl_transcoders'] = globals['___gl_transcoders'] || {};
+const transcoders = globals['gl_trans__coders'] = globals['gl_trans__coders'] || {};
 
 function inject(chunk) {
     // 奇怪的变量名是为了避免与worker源代码中的变量名冲突
@@ -15,11 +15,11 @@ function inject(chunk) {
     const prefixIndex = fnString.indexOf('{') + 1;
     const prefix = fnString.substring(0, prefixIndex);
 
-    const transcoders = globals['___gl_transcoders'] = globals['___gl_transcoders'] || {};
+    const transcoders = globals['gl_trans__coders'] = globals['gl_trans__coders'] || {};
     let injected = `${prefix}
     const _____getGlobal = ${getGlobal.toString()};
     const g___lobals = _____getGlobal()
-    const tran_____scoders = g___lobals.___gl_transcoders = g___lobals.___gl_transcoders || {};`
+    const tran_____scoders = g___lobals['gl_trans__coders'] = g___lobals['gl_trans__coders'] || {};`
     for (const p in transcoders) {
         if (p === 'inject') {
             continue;
