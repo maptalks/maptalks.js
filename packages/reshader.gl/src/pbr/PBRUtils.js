eba282b8fab275ac97e2a2f37ddb8250fe3f7e23
fuzhenn <fuzhen@maptalks.org>
2021-11-23 15:29:52 +0800

    add PBRUtils.loginIBLResOnCanvas/logoutIBLResOnCanvas

--- packages/reshader.gl/src/pbr/PBRUtils.js
+++ packages/reshader.gl/src/pbr/PBRUtils.js
@@ -1,4 +1,73 @@
 import { extend, isNumber } from '../common/Util.js';
+import { generateDFGLUT } from './PBRHelper.js';
+
+export function loginIBLResOnCanvas(canvas, regl, map) {
+    if (!canvas.dfgLUT) {
+        canvas.dfgLUT = generateDFGLUT(regl);
+        canvas.dfgLUT.mtkRefCount = 0;
+        const listener = onUpdatelights.bind(this, canvas, regl);
+        map.on('updatelights', listener);
+        canvas._iblResListener = listener;
+    }
+    canvas.dfgLUT.mtkRefCount++;
+    const lightManager = map.getLightManager();
+    const resource = lightManager && lightManager.getAmbientResource();
+    if (!resource) {
+        return {
+            dfgLUT: canvas.dfgLUT,
+            iblTexes: null
+        };
+    }
+    if (!canvas.iblTexes) {
+        canvas.iblTexes = createIBLTextures(regl, map);
+    }
+
+    return {
+        dfgLUT: canvas.dfgLUT,
+        iblTexes: canvas.iblTexes
+    };
+}
+
+export function getIBLResOnCanvas(canvas) {
+    const { dfgLUT, iblTexes } = canvas;
+    return {
+        dfgLUT,
+        iblTexes
+    };
+}
+
+export function logoutIBLResOnCanvas(canvas, map) {
+    let del = false;
+    if (canvas.dfgLUT) {
+        canvas.dfgLUT.mtkRefCount--;
+        if (canvas.dfgLUT.mtkRefCount <= 0) {
+            del = true;
+            const listener = canvas._iblResListener;
+            map.off('updatelights', listener);
+            canvas.dfgLUT.destroy();
+            delete canvas.dfgLUT;
+        }
+    }
+    if (canvas.iblTexes && del) {
+        disposeIBLTextures(canvas.iblTexes);
+        delete canvas.iblTexes;
+    }
+}
+
+function onUpdatelights(canvas, regl, e) {
+    if (e.ambientUpdate) {
+        const { iblTexes } = canvas;
+        if (iblTexes) {
+            const map = e.target;
+            disposeIBLTextures(iblTexes);
+            canvas.iblTexes = createIBLTextures(regl, map);
+        } else {
+            const map = e.target;
+            canvas.iblTexes = createIBLTextures(regl, map);
+        }
+    }
+
+}
 
 export function getPBRUniforms(map, iblTexes, dfgLUT, context) {
     const viewMatrix = map.viewMatrix;
@@ -28,9 +97,9 @@ export function getPBRUniforms(map, iblTexes, dfgLUT, context) {
 
 function getLightUniforms(map, iblTexes) {
     const lightManager = map.getLightManager();
-    const iblMaps = lightManager.getAmbientResource();
-    const ambientLight = lightManager.getAmbientLight();
-    const directionalLight = lightManager.getDirectionalLight();
+    const iblMaps = lightManager && lightManager.getAmbientResource();
+    const ambientLight = lightManager && lightManager.getAmbientLight() || {};
+    const directionalLight = lightManager && lightManager.getDirectionalLight() || {};
     let uniforms;
     if (iblMaps) {
         const cubeSize = iblTexes.prefilterMap.width;
@@ -51,10 +120,8 @@ function getLightUniforms(map, iblTexes) {
     uniforms['environmentExposure'] = isNumber(ambientLight.exposure) ? ambientLight.exposure : 1; //2]
     uniforms['environmentOrientation'] = ambientLight.orientation || 0;
 
-    if (directionalLight) {
-        uniforms['light0_diffuse'] = [...(directionalLight.color || [1, 1, 1]), 1];
-        uniforms['light0_viewDirection'] = directionalLight.direction || [1, 1, -1];
-    }
+    uniforms['light0_diffuse'] = [...(directionalLight.color || [1, 1, 1]), 1];
+    uniforms['light0_viewDirection'] = directionalLight.direction || [1, 1, -1];
     return uniforms;
 }
 
