0d99ff926037b152011974b88342d1d11160cda2
fuzhenn <fuzhen@maptalks.org>
2022-03-03 11:35:41 +0800

    add gl.transcoders support in worker

--- packages/vt/build/index.js
+++ packages/vt/build/index.js
@@ -6,7 +6,18 @@ const mat4 = maptalksgl.mat4;
 mat4.create();
 
 import chunk from './worker.js';
-maptalks.registerWorkerAdapter('@maptalks/vt', chunk);
 
+if (maptalksgl.transcoders) {
+    const version = maptalks.Map.VERSION;
+    if (version.indexOf("1.0.0-beta") >= 0 || version.indexOf("1.0.0-alpha") >= 0) {
+        maptalks.registerWorkerAdapter('@maptalks/vt', chunk);
+    } else {
+        maptalks.registerWorkerAdapter('@maptalks/vt', function () {
+            const transcoderInjected = maptalksgl.transcoders.inject(chunk);
+            return transcoderInjected;
+        });
+    }
+} else {
+    maptalks.registerWorkerAdapter('@maptalks/vt', chunk);
+}
 export * from '../src/layer/';
-
