8e8598b54ca5aca99ff91b4cd795d1a53844cb2c
fuzhenn <fuzhen@maptalks.org>
2022-04-12 11:14:13 +0800

    try to fix string reg

--- src/core/util/strings.js
+++ src/core/util/strings.js
@@ -143,7 +143,7 @@ export function splitContent(content, font, wrapWidth, textWidth) {
 }
 
 // const contentExpRe = /\{([\w_]+)\}/g;
-export const CONTENT_EXPRE = /{(.+)}/;
+export const CONTENT_EXPRE = /\{([^}]+)\}/g;
 
 /**
  * Replace variables wrapped by square brackets ({foo}) with actual values in props.
