1fa7962cd466b6cae8ee2ad45b82a1197ac55843
ADS <panzhengdong@yuan9.cn>
2022-05-30 17:05:04 +0800

    modify map options customTags desc

--- src/layer/tile/TileLayer.js
+++ src/layer/tile/TileLayer.js
@@ -82,7 +82,7 @@ class TileHashset {
  * @property {Number}              [options.zoomOffset=0]           - offset from map's zoom to tile's zoom
  * @property {Number}              [options.tileRetryCount=0]       - retry count of tiles
  * @property {String}              [options.errorUrl=null]       - image to replace when encountering error on loading tile image
- * @property {Object}              [options.customTags=null]          - custom replace keywords in template links, The object must have strings as keys and functions or string as values.it will cover options.token.
+ * @property {Object}              [options.customTags=null]    - custom tag values in urlTemplate, e.g. { foo: 'bar' } for http://path/to/{z}/{x}/{y}?foo={foo}
  * @property {Boolean}             [options.decodeImageInWorker=false]  - decode image in worker, for better performance if the server support
  * @property {String}              [options.token=null]       - token to replace {token} in template http://foo/bar/{z}/{x}/{y}?token={token}
  * @property {Object}              [options.fetchOptions=object]       - fetch params,such as fetchOptions: { 'headers': { 'accept': '' } }, about accept value more info https://developer.mozilla.org/en-US/docs/Web/HTTP/Content_negotiation/List_of_default_Accept_values
