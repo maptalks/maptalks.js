4217efd355c41c2fdcd23c96fd61b39ba204dc91
fuzhenn <fuzhen@maptalks.org>
2022-06-22 14:58:55 +0800

    remove __filename line to fix babel transpile issues, fix maptalks/issues#65

--- packages/transcoders.draco/src/lib/draco_decoder.js
+++ packages/transcoders.draco/src/lib/draco_decoder.js
@@ -1,7 +1,7 @@
 
 var DracoDecoderModule = (function() {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
-  if (typeof __filename !== 'undefined') _scriptDir = _scriptDir || __filename;
+
   return (
 function(DracoDecoderModule) {
   DracoDecoderModule = DracoDecoderModule || {};
--- packages/transcoders.ktx/src/lib/basis_transcoder.js
+++ packages/transcoders.ktx/src/lib/basis_transcoder.js
@@ -1,6 +1,6 @@
 var BASIS = (function() {
     var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src: undefined;
-    if (typeof __filename !== 'undefined') _scriptDir = _scriptDir || __filename;
+
     return (function(BASIS) {
         BASIS = BASIS || {};
 
