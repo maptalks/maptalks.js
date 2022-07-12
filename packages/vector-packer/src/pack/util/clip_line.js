93371b8c3af609e6e3a234e9fe636dbf3bebbb56
fuzhenn <fuzhen@maptalks.org>
2022-07-12 20:45:58 +0800

    fix clipped update in clip_line.js

--- packages/vector-packer/src/pack/util/clip_line.js
+++ packages/vector-packer/src/pack/util/clip_line.js
@@ -39,10 +39,12 @@ function clipLine(lines, x1, y1, x2, y2) {
                 p = p0;
                 p0 = new Point(x1, p0.y + (p1.y - p0.y) * ((x1 - p0.x) / (p1.x - p0.x)))._round();
                 p0.z = p.z + (p1.z - p.z) * ((x1 - p.x) / (p1.x - p.x));
+                clipped = true;
             } else if (p1.x < x1) {
                 p = p1;
                 p1 = new Point(x1, p0.y + (p1.y - p0.y) * ((x1 - p0.x) / (p1.x - p0.x)))._round();
                 p1.z = p0.z + (p.z - p0.z) * ((x1 - p0.x) / (p.x - p0.x));
+                clipped = true;
             }
 
             if (p0.y < y1 && p1.y < y1) {
@@ -51,10 +53,12 @@ function clipLine(lines, x1, y1, x2, y2) {
                 p = p0;
                 p0 = new Point(p0.x + (p1.x - p0.x) * ((y1 - p0.y) / (p1.y - p0.y)), y1)._round();
                 p0.z = p.z + (p1.z - p.z) * ((y1 - p.y) / (p1.y - p.y));
+                clipped = true;
             } else if (p1.y < y1) {
                 p = p1;
                 p1 = new Point(p0.x + (p1.x - p0.x) * ((y1 - p0.y) / (p1.y - p0.y)), y1)._round();
                 p1.z = p0.z + (p.z - p0.z) * ((y1 - p0.y) / (p.y - p0.y));
+                clipped = true;
             }
 
             if (p0.x >= x2 && p1.x >= x2) {
@@ -63,10 +67,12 @@ function clipLine(lines, x1, y1, x2, y2) {
                 p = p0;
                 p0 = new Point(x2, p0.y + (p1.y - p0.y) * ((x2 - p0.x) / (p1.x - p0.x)))._round();
                 p0.z = p.z + (p1.z - p.z) * ((x2 - p.x) / (p1.x - p.x));
+                clipped = true;
             } else if (p1.x >= x2) {
                 p = p1;
                 p1 = new Point(x2, p0.y + (p1.y - p0.y) * ((x2 - p0.x) / (p1.x - p0.x)))._round();
                 p1.z = p0.z + (p.z - p0.z) * ((x2 - p0.x) / (p.x - p0.x));
+                clipped = true;
             }
 
             if (p0.y >= y2 && p1.y >= y2) {
@@ -75,21 +81,20 @@ function clipLine(lines, x1, y1, x2, y2) {
                 p = p0;
                 p0 = new Point(p0.x + (p1.x - p0.x) * ((y2 - p0.y) / (p1.y - p0.y)), y2)._round();
                 p0.z = p.z + (p1.z - p.z) * ((y2 - p.y) / (p1.y - p.y));
+                clipped = true;
             } else if (p1.y >= y2) {
                 p = p1;
                 p1 = new Point(p0.x + (p1.x - p0.x) * ((y2 - p0.y) / (p1.y - p0.y)), y2)._round();
                 p1.z = p0.z + (p.z - p0.z) * ((y2 - p0.y) / (p.y - p0.y));
+                clipped = true;
             }
 
             if (!clippedLine || !p0.equals(clippedLine[clippedLine.length - 1])) {
-                if (clippedLines.length) {
-                    clippedLines[clippedLines.length - 1].clipped = clipped;
-                }
                 clippedLine = [p0];
                 clippedLines.push(clippedLine);
             }
-            if (p1 !== line[i + 1]) {
-                clipped = true;
+            if (clipped) {
+                clippedLine.clipped = true;
             }
             clippedLine.push(p1);
         }
