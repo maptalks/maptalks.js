7f3d458dd1b482a1e66b14ef24749df22decd2c0
LiuBing <471071362@qq.com>
2022-08-09 11:23:52 +0800

    限高分析的实现 (#309)
    * 限高分析的实现
    
    * HeightLimitAnalysis继承FloodAnalysis

---
+++ packages/analysis/src/HeightLimitAnalysis.js
@@ -0,0 +1,31 @@
+import FloodAnalysis from './FloodAnalysis';
+import { altitudeToDistance } from './common/Util';
+
+const DEFAULT_LIMIT_COLOR = [0.8, 0.1, 0.1];
+export default class HeightLimitAnalysis extends FloodAnalysis {
+    constructor(options) {
+        super(options);
+        this.type = 'heightLimitAnalysis';
+    }
+
+    _prepareRenderOptions(renderer) {
+        super._prepareRenderOptions(renderer);
+        const map = this.layer.getMap();
+        this._renderOptions['waterHeight'] = altitudeToDistance(map, this.options.limitHeight);
+        this._renderOptions['type'] = 2.0;
+    }
+
+    update(name, value) {
+        super.update(name, value);
+        if (name === 'limitHeight') {
+            const map = this.layer.getMap();
+            this._renderOptions['waterHeight'] = map.altitudeToPoint(value || 0, map.getGLRes());
+        }
+    }
+
+    renderAnalysis(meshes) {
+        const uniforms = super.renderAnalysis(meshes);
+        uniforms['flood_waterColor'] = this.options['limitColor'] || DEFAULT_LIMIT_COLOR;
+        return uniforms;
+    }
+}
