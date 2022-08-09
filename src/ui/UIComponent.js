0c009f278bb0e578a7014fba842a23455d9393f3
deyihu <mehudeyi@163.com>
2022-08-09 16:53:38 +0800

    infowindow only updateposition when owner symbolchange

--- src/ui/UIComponent.js
+++ src/ui/UIComponent.js
@@ -602,7 +602,7 @@ class UIComponent extends Eventable(Class) {
         const events = {};
         if (this._owner && (this._owner instanceof Geometry)) {
             events.positionchange = this.onGeometryPositionChange;
-            events.symbolchange = this.onGeometryPositionChange;
+            events.symbolchange = this._updatePosition;
         }
         if (this.getOwnerEvents) {
             extend(events, this.getOwnerEvents());
