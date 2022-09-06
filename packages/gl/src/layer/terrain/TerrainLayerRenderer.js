8a25db7d8566ae0b9d2cba245afdee92204a7bd5
fuzhenn <fuzhen@maptalks.org>
2022-09-06 19:49:04 +0800

    some code cleaning and dispose emptyTexture and shader in onRemove

--- packages/gl/src/layer/terrain/TerrainLayerRenderer.js
+++ packages/gl/src/layer/terrain/TerrainLayerRenderer.js
@@ -7,9 +7,6 @@ import vert from './glsl/terrain.vert';
 import frag from './glsl/terrain.frag';
 import { getCascadeTileIds, getSkinTileScale, getSkinTileRes, getParentSkinTile } from './TerrainTileUtil';
 
-const V0 = [];
-const V1 = [];
-const V2 = [];
 const V3 = [];
 
 const POINT0 = new maptalks.Point(0, 0);
@@ -237,7 +234,6 @@ class TerrainLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer {
             const scale = terrainRes / res;
             const x = worldPos.x - extent2d.xmin * scale;
             const y = worldPos.y - extent2d.ymin * scale;
-            // return this._findInTrinagle(terrainData.image, x, y);
             return this._queryAltitudeInHeights(terrainData.image.data, x / (extent2d.getWidth() * scale), y / (extent2d.getHeight() * scale));
         } else {
             return 0;
@@ -253,6 +249,13 @@ class TerrainLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer {
         return terrainData;
     }
 
+    _queryAltitudeInHeights(terrainData, x, y) {
+        const { width, height, data } = terrainData;
+        const tx = Math.floor(width * x);
+        const ty = Math.floor(height * y);
+        return data[tx * width + ty];
+    }
+
 
     _getPointZ(height) {
         const map = this.layer.getMap();
@@ -261,43 +264,6 @@ class TerrainLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer {
         }
         const altitude = map.altitudeToPoint(height, map.getGLRes());
         return altitude;
-    }
-
-
-    _findInTrinagle(terrainData, x, y) {
-        const exaggeration = this.layer.options['exaggeration'] || 1;
-        const positions = terrainData.positions;
-        const triangles = terrainData.triangles;
-        let x0, x1, x2, y0, y1, y2;
-        const count = triangles.length / 3;
-        for (let i = 0; i < count; i++) {
-            const aIndex = triangles[i * 3], bIndex = triangles[i * 3 + 1], cIndex = triangles[i * 3 + 2];
-            if (x < positions[aIndex * 3] && x < positions[bIndex * 3] && x < positions[cIndex * 3] || x > positions[aIndex * 3] && x > positions[bIndex * 3] && x > positions[cIndex * 3] ||
-                y < -positions[aIndex * 3 + 1] && y < -positions[bIndex * 3 + 1] && y < -positions[cIndex * 3 + 1] || y > -positions[aIndex * 3 + 1] && y > -positions[bIndex * 3 + 1] && y > -positions[cIndex * 3 + 1]) {
-                continue;
-            }
-            y0 = -positions[aIndex * 3 + 1];
-            y1 = -positions[bIndex * 3 + 1];
-            y2 = -positions[cIndex * 3 + 1];
-            x0 = positions[aIndex * 3];
-            x1 = positions[bIndex * 3];
-            x2 = positions[cIndex * 3];
-            const A = vec3.set(V0, x0, y0, positions[aIndex * 3 + 2]);
-            const B = vec3.set(V1, x1, y1, positions[bIndex * 3 + 2]);
-            const C = vec3.set(V2, x2, y2, positions[cIndex * 3 + 2]);
-            const SABC = this._calTriangleArae(A[0], A[1], B[0], B[1], C[0], C[1]);
-            const SPAC = this._calTriangleArae(x, y, A[0], A[1], C[0], C[1]);
-            const SPAB = this._calTriangleArae(x, y, A[0], A[1], B[0], B[1]);
-            const SPBC = this._calTriangleArae(x, y, B[0], B[1], C[0], C[1]);
-            if (SPAC + SPAB + SPBC - SABC > 0.0001) {
-                continue;
-            } else {
-                // A[1] = -A[1];
-                // B[1] = -B[1];
-                // C[1] = -C[1];
-                return (A[2] + B[2] + C[2]) * 50 / 3 * exaggeration;
-            }
-        }
     }
 
     _queryTileAltitude(out, extent, targetRes) {
@@ -408,17 +374,6 @@ class TerrainLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer {
         return out;
     }
 
-    _queryAltitudeInHeights(terrainData, x, y) {
-        const { width, height, data } = terrainData;
-        const tx = Math.floor(width * x);
-        const ty = Math.floor(height * y);
-        return data[tx * width + ty];
-    }
-
-    _calTriangleArae(x1, y1, x2, y2, x3, y3) {
-        return Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2) * 0.5;
-    }
-
     onAdd() {
         super.onAdd();
         this.prepareWorker();
@@ -431,6 +386,13 @@ class TerrainLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer {
             });
             this.workerConn.remove();
             delete this.workerConn;
+        }
+        if (this._shader) {
+            this._shader.dispose();
+            delete this._shader;
+        }
+        if (this._emptyTileTexture) {
+            this._emptyTileTexture.destroy();
         }
         super.onRemove();
     }
