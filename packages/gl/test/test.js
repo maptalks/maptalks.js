9b906dbc5d30808ac93eb3e459106742c9c8e603
fuzhenn <fuzhen@maptalks.org>
2022-09-08 11:27:53 +0800

    more terrain specs

--- packages/gl/test/test.js
+++ packages/gl/test/test.js
@@ -130,6 +130,69 @@ describe('gl tests', () => {
             });
             group.addTo(map);
         });
+
+        it('GroupGLLayer.queryTerrain', done => {
+            map = new maptalks.Map(container, {
+                center: [91.14478,29.658272],
+                zoom: 12
+            });
+            const skinLayers = [
+                new maptalks.TileLayer('base', {
+                    urlTemplate: '/fixtures/google-256/{z}/{x}/{y}.jpg'
+                })
+            ];
+            const terrain = {
+                type: 'mapbox',
+                tileSize: 512,
+                spatialReference: 'preset-vt-3857',
+                urlTemplate: '/fixtures/mapbox-terrain/{z}/{x}/{y}.webp'
+            }
+            const group = new maptalks.GroupGLLayer('group', skinLayers, { terrain });
+            group.once('terrainlayercreated', () => {
+                const terrainLayer = group.getTerrainLayer();
+                terrainLayer.once('terrainreadyandrender', () => {
+                    const altitude = group.queryTerrain(map.getCenter());
+                    expect(altitude).to.be.eql(4442.0751953125);
+                    done();
+                });
+            });
+            group.addTo(map);
+        });
+
+        it('terrain with dpr of 2', done => {
+            map = new maptalks.Map(container, {
+                center: [91.14478,29.658272],
+                zoom: 12,
+                devicePixelRatio: 2
+            });
+            const skinLayers = [
+                new maptalks.TileLayer('base', {
+                    urlTemplate: '/fixtures/google-256/{z}/{x}/{y}.jpg'
+                })
+            ];
+            const terrain = {
+                type: 'mapbox',
+                tileSize: 512,
+                spatialReference: 'preset-vt-3857',
+                urlTemplate: '/fixtures/mapbox-terrain/{z}/{x}/{y}.webp'
+            }
+            const group = new maptalks.GroupGLLayer('group', skinLayers, { terrain });
+            group.once('terrainlayercreated', () => {
+                const terrainLayer = group.getTerrainLayer();
+                terrainLayer.once('terrainreadyandrender', () => {
+                    group.once('layerload', () => {
+                        const canvas = map.getRenderer().canvas;
+                        const ctx = canvas.getContext('2d');
+                        const pixel = ctx.getImageData(canvas.width / 2, canvas.height / 2, 1, 1);
+                        expect(pixel).to.be.eql({ data: { '0': 151, '1': 155, '2': 156, '3': 255 } });
+                        const altitude = group.queryTerrain(map.getCenter());
+                        expect(altitude).to.be.eql(4442.0751953125);
+                        done();
+                    });
+                });
+            });
+            group.addTo(map);
+        });
     });
 
 });
