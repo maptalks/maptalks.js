20c5c17467c4b4a81b27acb8be6cf4628ec6f5ef
fuzhenn <fuzhen@maptalks.org>
2022-09-22 20:23:30 +0800

    解决有透明度icon边缘的"漏光"问题

--- packages/vt/src/layer/plugins/painters/util/atlas_util.js
+++ packages/vt/src/layer/plugins/painters/util/atlas_util.js
@@ -8,6 +8,7 @@ export function createAtlasTexture(regl, atlas, flipY) {
         mag: 'linear', //very important
         min: 'linear', //very important
         flipY,
+        // 设成true解决矢量marker边缘的“漏光”问题
         premultiplyAlpha: true
     };
     if (atlas.type === 'icon') {
