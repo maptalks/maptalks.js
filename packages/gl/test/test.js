a48013e829e9d676e15b2d479b4131659f8cc0d2
fuzhenn <fuzhen@maptalks.org>
2023-05-24 20:54:23 +0800

    add spec for invalid terrain tiles

--- packages/gl/test/test.js
+++ packages/gl/test/test.js
@@ -413,6 +413,46 @@ describe('gl tests', () => {
             });
             group.addTo(map);
         });
+
+        it.only('terrain with invalid terrain tiles', done => {
+            map = new maptalks.Map(container, {
+                center: [91.14478,29.658272],
+                zoom: 12
+            });
+            const skinLayers = [
+                new maptalks.TileLayer('base', {
+                    urlTemplate: '/fixtures/google-256/{z}/{x}/{y}.jpg'
+                })
+            ];
+            const terrain = {
+                fadeAnimation: false,
+                type: 'mapbox',
+                tileSize: 512,
+                spatialReference: 'preset-vt-3857',
+                urlTemplate: '/invalid.webp',
+                tileStackDepth: 0,
+                shader: 'lit',
+                material: {
+                    baseColorFactor: [1, 1, 1, 1],
+                    roughnessFactor: 0.9,
+                    metallicFactor: 0
+                }
+            }
+            const group = new maptalks.GroupGLLayer('group', skinLayers, { terrain });
+            group.once('terrainlayercreated', () => {
+                const terrainLayer = group.getTerrainLayer();
+                terrainLayer.once('terrainreadyandrender', () => {
+                    group.once('layerload', () => {
+                        const canvas = map.getRenderer().canvas;
+                        const ctx = canvas.getContext('2d');
+                        const pixel = ctx.getImageData(canvas.width / 2, canvas.height / 2 + 7, 1, 1);
+                        expect(pixel).to.be.eql({ data: { '0': 131, '1': 135, '2': 136, '3': 255 } });
+                        done();
+                    });
+                });
+            });
+            group.addTo(map);
+        });
     });
 
     context('skybox tests', () => {
