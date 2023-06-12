4b70d9eeb175597c735a3645d1baa8437d57b344
fuzhenn <fuzhen@maptalks.org>
2023-06-12 19:26:08 +0800

    remove resWorkerConn in CanvasRenderer

--- src/renderer/layer/CanvasRenderer.js
+++ src/renderer/layer/CanvasRenderer.js
@@ -217,6 +217,10 @@ class CanvasRenderer extends Class {
             this.resources.remove();
         }
         delete this.resources;
+        if (this._resWorkerConn) {
+            this._resWorkerConn.remove();
+            delete this._resWorkerConn;
+        }
         delete this.layer;
     }
 
