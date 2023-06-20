b3b20273b69e3603fdea21c44bc7cafbaf15043b
fuzhenn <fuzhen@maptalks.org>
2023-06-20 09:57:28 +0800

    reverse polygonOffset due to @maptalks/gl@0.85.0

--- src/renderer/layer/tilelayer/TileLayerGLRenderer.js
+++ src/renderer/layer/tilelayer/TileLayerGLRenderer.js
@@ -98,7 +98,7 @@ class TileLayerGLRenderer extends ImageGLRenderable(TileLayerCanvasRenderer) {
         const gl = this.gl;
         gl.stencilFunc(gl.LEQUAL, Math.abs(this.getCurrentTileZoom() - tileInfo.z), 0xFF);
         const layerPolygonOffset = this.layer.getPolygonOffset();
-        const polygonOffset = this.drawingCurrentTiles ? -layerPolygonOffset - 1 : -layerPolygonOffset;
+        const polygonOffset = this.drawingCurrentTiles ? layerPolygonOffset : layerPolygonOffset + 1;
         gl.polygonOffset(polygonOffset, polygonOffset);
 
         this.drawGLImage(tileImage, x, y, w, h, scale, opacity, debugInfo);
