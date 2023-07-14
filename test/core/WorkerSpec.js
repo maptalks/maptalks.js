c9ddc18460e95a820456c2bad207eb244ae1aad8
deyihu <mehudeyi@163.com>
2023-07-14 09:15:55 +0800

    update

--- test/core/WorkerSpec.js
+++ test/core/WorkerSpec.js
@@ -26,42 +26,6 @@ describe('WorkerSpec', function () {
     var map;
     var layer;
 
-    function COMMON_CREATE_MAP(center, baseLayer, options) {
-        var container = document.createElement('div');
-        container.id = 'test_container';
-        container.style.width = (options && options.width || 80) + 'px';
-        container.style.height = (options && options.height || 60) + 'px';
-        document.body.appendChild(container);
-        var option = {
-            zoomAnimationDuration: 50,
-            zoom: 17,
-            center: center,
-            // centerCross : true
-        };
-        if (options) {
-            delete options.width;
-            delete options.height;
-            for (var p in options) {
-                if (options.hasOwnProperty(p)) {
-                    option[p] = options[p];
-                }
-            }
-        }
-        if (baseLayer) {
-            option.baseLayer = baseLayer;
-        }
-        var map = new maptalks.Map(container, option);
-        return {
-            'container': container,
-            'map': map
-        };
-    }
-    
-    function REMOVE_CONTAINER() {
-        document.body.innerHTML = '';
-    }
-
-
     beforeEach(function () {
     });
 
