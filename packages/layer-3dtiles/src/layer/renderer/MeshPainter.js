979c6406d9b64b67045742d87f93bc48a8ced913
fuzhenn <fuzhen@maptalks.org>
2023-09-19 20:47:36 +0800

    add Geo3DTilesLayer.options.service.doubleSided

--- packages/layer-3dtiles/src/layer/renderer/MeshPainter.js
+++ packages/layer-3dtiles/src/layer/renderer/MeshPainter.js
@@ -1222,7 +1222,7 @@ export default class MeshPainter {
                 environmentExposure = ambientLight[0] / ambientValue;
             }
             material = this._createMaterial(gltfMesh.material, gltf, shader, materialInfo || DEFAULT_MATERIAL_INFO, environmentExposure || 1, gltfWeakResources);
-            material.doubleSided = gltf.materials && gltf.materials[gltfMesh.material] && gltf.materials[gltfMesh.material].doubleSided;
+            material.doubleSided = service.doubleSided || gltf.materials && gltf.materials[gltfMesh.material] && gltf.materials[gltfMesh.material].doubleSided;
         }
         Object.defineProperty(material.uniforms, 'alphaTest', {
             enumerable: true,
