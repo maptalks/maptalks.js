c8510aee307072ddd6251883eed368c4f0be32f4
hu de yi <mehudeyi@163.com>
2023-10-10 14:16:13 +0800

    fix Marker TextDesc cal error when textName Is multi line text (#2094)
    * fix Marker TextDesc cal error when textName Is multi line text
    
    * fix typo

--- test/geometry/symbol/SymbolTextSpec.js
+++ test/geometry/symbol/SymbolTextSpec.js
@@ -87,4 +87,38 @@ describe('SymbolTextSpec', function () {
         // expect(geometry.getTextDesc().size.width).to.be.eql(0);
         expect(geometry.getTextDesc().size.height).to.be.eql(textSize);
     });
+
+    it('#2093 text symbol TextDesc rows when textName Is multi line text ', function () {
+        function líneasNuevas(s) {
+            return s.split('').join('\n').replace(/([\ud800-\udfff])\n([\ud800-\udfff])/g, '$1$2');
+        }
+        const textNames = ['甲子', '乙丑', '丙寅', '丁卯', '戊辰', '己巳'].map(name => {
+            return líneasNuevas(name + '行都司');
+        });
+        textNames.push('大家好');
+        textNames.push('Hello\nWorld');
+        const randomName = Math.ceil(Math.random() * 100000) + '';
+        textNames.push(líneasNuevas(randomName));
+
+        var v = new maptalks.VectorLayer('v', { 'drawImmediate': true }).addTo(map);
+        textNames.forEach(textName => {
+            v.clear();
+            const lineRows = textName.split('\n');
+            var geometry = new maptalks.Marker(center.copy(), {
+                symbol: {
+                    'textName': textName,
+                    "textFaceName": "楷体",
+                    "textSize": { "stops": [[8, 14], [20, 32]] },
+                    "textFill": "#000",
+                    "textOpacity": 1,
+                    "textHaloFill": [1, 1, 1, 0.9],
+                    "textHaloRadius": 1,
+                    "textHorizontalAlignment": "middle",
+                    "textVerticalAlignment": "middle"
+                }
+            });
+            v.addGeometry(geometry);
+            expect(geometry.getTextDesc().rows.length).to.be.eql(lineRows.length);
+        });
+    });
 });
