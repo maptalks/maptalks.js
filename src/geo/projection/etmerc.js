9e7de5d71cbc3427090e6ac542d8a32ca3e2c392
fuzhenn <fuzhen@maptalks.org>
2023-10-20 20:08:01 +0800

    remove unused codes

--- src/geo/projection/etmerc.js
+++ src/geo/projection/etmerc.js
@@ -18,42 +18,14 @@ var fabs = Math.abs,
     log = Math.log,
     hypot = Math.hypot,
     sinh = Math.sinh,
-    cosh = Math.cosh,
-    MIN = Math.min,
-    MAX = Math.max;
+    cosh = Math.cosh;
 
 // constants from math.h
 var HUGE_VAL = Infinity,
     M_PI = Math.PI;
 
 // from proj_api.h
-var RAD_TO_DEG = 57.295779513082321,
-    DEG_TO_RAD = 0.017453292519943296;
-
-// from pj_transform.c
-var SRS_WGS84_SEMIMAJOR = 6378137;
-var SRS_WGS84_ESQUARED = 0.0066943799901413165;
-
-// math constants from project.h
-var M_FORTPI = M_PI / 4,
-    M_HALFPI = M_PI / 2,
-    M_PI_HALFPI = 1.5 * M_PI,
-    M_TWOPI = 2 * M_PI,
-    M_TWO_D_PI = 2 / M_PI,
-    M_TWOPI_HALFPI = 2.5 * M_PI;
-
-// datum types
-var PJD_UNKNOWN = 0,
-    PJD_3PARAM = 1,
-    PJD_7PARAM = 2,
-    PJD_GRIDSHIFT = 3,
-    PJD_WGS84 = 4;
-
-// named errors
-var PJD_ERR_GEOCENTRIC = -45,
-    PJD_ERR_AXIS = -47,
-    PJD_ERR_GRID_AREA = -48,
-    PJD_ERR_CATALOG = -49;
+var RAD_TO_DEG = 57.295779513082321;
 
 // common
 var EPS10 = 1e-10;
