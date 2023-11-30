88d772278d0093368d3cfe1323ebe1becaf00dd4
fuzhenn <fuzhen@maptalks.org>
2023-11-30 16:44:47 +0800

    add spec for polygon textName

---
+++ packages/vt/test/integration/vector-fixtures/polygon/polygon-text/index.js
@@ -0,0 +1,20 @@
+const maptalks = require('maptalks');
+
+const polygon = new maptalks.Polygon([
+    [0, 0], [1, 0], [1, 1], [0, 1], [0, 0]
+], {
+    symbol: {
+        polygonFill: '#f00',
+        polygonOpacity: 0.4,
+        textName: 'hello',
+        textSize: 12
+    }
+});
+
+module.exports = {
+    data: [polygon],
+    view: {
+        center: [0, 0],
+        zoom: 6
+    }
+};
