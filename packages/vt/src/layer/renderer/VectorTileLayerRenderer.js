d871f18d7777cc7352296380fb7455a568c29eac
fuzhenn <fuzhen@maptalks.org>
2023-12-25 11:16:48 +0800

    fix referrer when window is undefined

--- packages/vt/src/layer/renderer/VectorTileLayerRenderer.js
+++ packages/vt/src/layer/renderer/VectorTileLayerRenderer.js
@@ -541,7 +541,7 @@ class VectorTileLayerRenderer extends maptalks.renderer.TileLayerCanvasRenderer 
             };
             this._requestingMVT[url].keys[tileInfo.id] = 1;
             const fetchOptions = this.layer.options['fetchOptions'];
-            const referrer = window.location.href;
+            const referrer = window && window.location.href;
             this._workerConn.loadTile({ tileInfo: { res: tileInfo.res, x: tileInfo.x, y: tileInfo.y, z: tileInfo.z, url: tileInfo.url, id: tileInfo.id, extent2d: tileInfo.extent2d },
                 glScale, zScale: this._zScale, centimeterToPoint, verticalCentimeterToPoint, fetchOptions, styleCounter: this._styleCounter, referrer }, this._onReceiveMVTData.bind(this, url));
         } else if (!cached.keys[tileInfo.id]) {
