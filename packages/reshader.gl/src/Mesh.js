81269f22b8e020df670643fcda071a3b763072a3
fuzhenn <fuzhen@maptalks.org>
2024-01-18 11:16:47 +0800

    fix lint

--- packages/reshader.gl/src/Mesh.js
+++ packages/reshader.gl/src/Mesh.js
@@ -1,7 +1,6 @@
 import { extend, isNil, isNumber, isFunction, isSupportVAO, hasOwn } from './common/Util.js';
 import { mat4, vec3 } from 'gl-matrix';
 import BoundingBox from './BoundingBox.js';
-import { KEY_DISPOSED } from './common/Constants';
 
 const tempMat4 = [];
 
