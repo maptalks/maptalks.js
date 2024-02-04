7a36be68539f4b44a64df91254220e1949431c74
fuzhenn <fuzhen@maptalks.org>
2024-02-04 17:05:45 +0800

    fix null feature in getLabelContent

--- packages/vt/src/layer/plugins/painters/util/get_label_content.js
+++ packages/vt/src/layer/plugins/painters/util/get_label_content.js
@@ -2,5 +2,5 @@ export function getLabelContent(mesh, idx) {
     const { aPickingId, features } = mesh.geometry.properties;
     const pickingId = aPickingId[idx];
     const feature = features && features[pickingId] && features[pickingId].feature;
-    return feature.label;
+    return feature && feature.label;
 }
