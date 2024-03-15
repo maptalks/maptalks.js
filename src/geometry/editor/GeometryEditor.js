9b834ff37484c87e013ae5506ae3cd78af72ec67
hu de yi <mehudeyi@163.com>
2024-03-15 18:18:04 +0800

    Geometry edit control point support zIndex (#2225)

--- src/geometry/editor/GeometryEditor.js
+++ src/geometry/editor/GeometryEditor.js
@@ -36,7 +36,9 @@ const options = {
     'vertexHandleSymbol': createHandleSymbol('square', 1),
     'newVertexHandleSymbol': createHandleSymbol('square', 0.4),
     'collision': false,
-    'collisionBufferSize': 0
+    'collisionBufferSize': 0,
+    'vertexZIndex': 0,
+    'newVertexZIndex': 0
 };
 
 /**
@@ -850,6 +852,7 @@ class GeometryEditor extends Eventable(Class) {
 
         const verticeLimit = geoToEdit instanceof Polygon ? 3 : 2;
         const propertyOfVertexIndex = 'maptalks--editor-vertex-index';
+        const { vertexZIndex, newVertexZIndex } = this.options;
         //{ ringIndex:ring }
         const vertexHandles = { 0: [] },
             newVertexHandles = { 0: [] };
@@ -1001,6 +1004,7 @@ class GeometryEditor extends Eventable(Class) {
             handle[propertyOfVertexIndex] = index;
             handle._ringIndex = ringIndex;
             handle.on(me.options['removeVertexOn'], removeVertex);
+            handle.setZIndex(vertexZIndex);
             return handle;
         }
 
@@ -1076,6 +1080,7 @@ class GeometryEditor extends Eventable(Class) {
                 }
             });
             handle[propertyOfVertexIndex] = index;
+            handle.setZIndex(newVertexZIndex);
             return handle;
         }
         if (geoToEdit instanceof Polygon) {
@@ -1108,6 +1113,10 @@ class GeometryEditor extends Eventable(Class) {
             if (newVertexHandles[ringIndex].length && geoToEdit.getCoordinates().length === 2) {
                 newVertexHandles[ringIndex][0].options.symbol['markerDx'] = 12;
             }
+        }
+        const renderer = map.getRenderer();
+        if (renderer) {
+            renderer.sortTopElements();
         }
         this._addRefreshHook(() => {
             if (pauseRefresh) {
