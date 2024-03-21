8c280145a27edd130b2495d413b4a2cb94fb6d61
fuzhenn <fuzhen@maptalks.org>
2024-03-21 15:50:19 +0800

    ts JSONAble.ts

--- src/core/JSONAble.ts
+++ src/core/JSONAble.ts
@@ -1,38 +1,44 @@
+import { MixinConstructor } from "./Mixin";
 
-const registeredTypes = {};
+type JSONTypes = Record<string, any>
+
+const registeredTypes:JSONTypes = {};
 
 /**
  * A helper mixin for JSON serialization.
  * @mixin JSONAble
  */
-export default Base =>
-    class extends Base {
+export default function <Class extends MixinConstructor>(Base: Class) {
+    return class JSONAble extends Base {
+        _jsonType?: string
         /**
+         * 静态方法，用于将该类注册用于JSON序列化与反序列化
+         *
+         * @english
          * It is a static method. <br>
-         * Register layer for JSON serialization and assign a JSON type.
-         * @param  {String} type - JSON type
-         * @function JSONAble.registerJSONType
+         * Register class for JSON serialization and assign a JSON type.
+         * @param  type - JSON type
          */
-        static registerJSONType(type) {
+        static registerJSONType(type: string): void {
             if (!type) {
-                return this;
+                return;
             }
             if (registeredTypes[type]) {
                 console.error(`${type} has register. please use Different name for:`, this);
-                return this;
+                return;
             }
             registeredTypes[type] = this;
-            return this;
+            return;
         }
 
         /**
+         * 静态方法，返回type对应的注册类
+         * @english
          * It is a static method. <br>
          * Get class of input JSON type
-         * @param  {String} type - JSON type
-         * @return {Class}      Class
-         * @function JSONAble.getJSONClass
+         * @param  type - JSON type
          */
-        static getJSONClass(type) {
+        static getJSONClass(type: string): Class | null {
             if (!type) {
                 return null;
             }
@@ -40,11 +46,11 @@ export default Base =>
         }
 
         /**
+         * 返回该类的JSON type
+         * @english
          * Get object's JSON Type
-         * @return {String}
-         * @function JSONAble.getJSONType
          */
-        getJSONType() {
+        getJSONType(): string {
             if (this._jsonType === undefined) {
                 const clazz = Object.getPrototypeOf(this).constructor;
                 for (const p in registeredTypes) {
@@ -59,4 +65,5 @@ export default Base =>
             }
             return this._jsonType;
         }
-    };
+    }
+}
