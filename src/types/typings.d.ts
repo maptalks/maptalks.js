fc3d115ebc759166091ec391148e2ea92cea0fb9
pluto369 <1516544795@qq.com>
2024-03-26 23:08:10 +0800

    delete shim.d.ts

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -1,2 +1,13 @@
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 type WithNull<T> = T | null;
 type WithUndef<T> = T | undefined;
+
+declare const ActiveXObject: any
+
+interface Window {
+    MSPointerEvent: any
+    opera: any
+    DocumentTouch: any
+}
