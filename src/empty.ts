6276e435e7baf25cd6284fb879baf46b5e425ada
pluto369 <1516544795@qq.com>
2024-03-26 22:41:14 +0800

    add type to Browser.ts

--- src/empty.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare const ActiveXObject: any
---
+++ src/shim.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+declare const ActiveXObject: any
+
+interface Window {
+    MSPointerEvent: any
+    opera: any
+    DocumentTouch: any
+}
+
