34733b938c11e502143f82fc15fd0dde1f63ce92
sakitam-fdd <smilefdd@gmail.com>
2024-04-24 11:20:50 +0800

    fix(typing): fix ts generate (#2312)

--- src/geo/projection/index.ts
+++ src/geo/projection/index.ts
@@ -7,13 +7,19 @@ import type { BAIDUProjectionType } from './Projection.Baidu';
 import type { UTMProjectionType } from './Projection.UTM';
 import type { IdentityProjectionType } from './Projection.IDENTITY';
 
-export { default as EPSG4326, EPSG4326ProjectionType } from './Projection.EPSG4326';
-export { default as EPSG9807, EPSG9807ProjectionParams, EPSG9807ProjectionType } from './Projection.EPSG9807';
-export { default as UTM, UTMProjectionParams, UTMProjectionType } from './Projection.UTM';
-export { default as BAIDU, BAIDUProjectionType } from './Projection.Baidu';
-export { default as IDENTITY, IdentityProjectionType } from './Projection.IDENTITY';
+export { default as EPSG4326 } from './Projection.EPSG4326';
+export type { EPSG4326ProjectionType } from './Projection.EPSG4326';
+export { default as EPSG9807 } from './Projection.EPSG9807';
+export type { EPSG9807ProjectionParams, EPSG9807ProjectionType } from './Projection.EPSG9807';
+export { default as UTM } from './Projection.UTM';
+export type { UTMProjectionParams, UTMProjectionType } from './Projection.UTM';
+export { default as BAIDU } from './Projection.Baidu';
+export type { BAIDUProjectionType } from './Projection.Baidu';
+export { default as IDENTITY } from './Projection.IDENTITY';
+export type { IdentityProjectionType } from './Projection.IDENTITY';
 export { EPSG3857, EPSG3857ProjectionType };
-export { default as Common, CommonProjectionType } from './Projection';
+export { default as Common } from './Projection';
+export type { CommonProjectionType } from './Projection';
 
 export type ProjectionType = |
     EPSG3857ProjectionType
