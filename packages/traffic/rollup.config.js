01e15f7fac156b1656115da8a855e0894200c440
chris young <923683900@qq.com>
2024-04-24 16:50:26 +0800

    rename traffic package js to ts (#31)
    * rename traffic package js to ts
    
    * add ts config

--- packages/traffic/rollup.config.js
+++ packages/traffic/rollup.config.js
@@ -1,30 +1,36 @@
-const { nodeResolve: resolve } = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const terser = require('@rollup/plugin-terser');
-const pkg = require('./package.json');
+const { nodeResolve: resolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const terser = require("@rollup/plugin-terser");
+const typescript = require("@rollup/plugin-typescript");
+const pkg = require("./package.json");
 
-const production = process.env.BUILD === 'production';
+const production = process.env.BUILD === "production";
 const outputFile = pkg.main;
-const plugins = production ? [
-    terser({
-        mangle: {
-            properties: {
-                'regex' : /^_/,
-                'keep_quoted' : true,
-                'reserved': ['on', 'once', 'off']
-            }
-        },
-        output : {
-            keep_quoted_props: true,
-            beautify: false,
-            comments : '/^!/'
-        }
-    })] : [];
+const plugins = production
+    ? [
+          terser({
+              mangle: {
+                  properties: {
+                      regex: /^_/,
+                      keep_quoted: true,
+                      reserved: ["on", "once", "off"],
+                  },
+              },
+              output: {
+                  keep_quoted_props: true,
+                  beautify: false,
+                  comments: "/^!/",
+              },
+          }),
+      ]
+    : [];
 
-const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${pkg.license}\n * (c) 2016-${new Date().getFullYear()} maptalks.org\n */`;
+const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${
+    pkg.license
+}\n * (c) 2016-${new Date().getFullYear()} maptalks.org\n */`;
 
-let outro = pkg.name + ' v' + pkg.version;
-if (pkg.peerDependencies && pkg.peerDependencies['maptalks']) {
+let outro = pkg.name + " v" + pkg.version;
+if (pkg.peerDependencies && pkg.peerDependencies["maptalks"]) {
     outro += `, requires maptalks@${pkg.peerDependencies.maptalks}.`;
 }
 
@@ -32,60 +38,65 @@ outro = `typeof console !== 'undefined' && console.log('${outro}');`;
 
 const basePlugins = [
     resolve({
-        module : true,
-        jsnext : true,
-        main : true
+        module: true,
+        jsnext: true,
+        main: true,
     }),
-    commonjs()
+    commonjs(),
+    typescript(),
 ];
 
 module.exports = [
     {
-        input: 'src/index.js',
-        external: ['maptalks', '@maptalks/gltf-layer', 'turf'],
+        input: "src/index.js",
+        external: ["maptalks", "@maptalks/gltf-layer", "turf"],
         plugins: basePlugins.concat(plugins),
         output: {
             globals: {
-                'maptalks': 'maptalks',
-                '@maptalks/gltf-layer': 'maptalks',
-                'turf': 'turf'
+                maptalks: "maptalks",
+                "@maptalks/gltf-layer": "maptalks",
+                turf: "turf",
             },
-            'sourcemap': production ? false : 'inline',
-            'format': 'umd',
-            'name': 'maptalks',
-            'banner': banner,
-            'outro' : outro,
-            'extend' : true,
-            'file': outputFile
-        }
+            sourcemap: production ? false : "inline",
+            format: "umd",
+            name: "maptalks",
+            banner: banner,
+            outro: outro,
+            extend: true,
+            file: outputFile,
+        },
     },
     {
-        input: 'src/index.js',
-        plugins: basePlugins.concat(production ? [
-            terser({
-                output : { comments : '/^!/', beautify: true },
-                mangle : {
-                    properties: {
-                        'regex' : /^_/,
-                        'keep_quoted' : true
-                    }
-                }
-            })
-        ] : []),
-        external: ['maptalks', '@maptalks/gltf-layer', 'turf'],
+        input: "src/index.js",
+        plugins: basePlugins.concat(
+            production
+                ? [
+                      terser({
+                          output: { comments: "/^!/", beautify: true },
+                          mangle: {
+                              properties: {
+                                  regex: /^_/,
+                                  keep_quoted: true,
+                              },
+                          },
+                      }),
+                  ]
+                : []
+        ),
+        external: ["maptalks", "@maptalks/gltf-layer", "turf"],
         output: {
             globals: {
-                'maptalks': 'maptalks',
-                '@maptalks/gltf-layer': 'maptalks',
-                'turf': 'turf'
+                maptalks: "maptalks",
+                "@maptalks/gltf-layer": "maptalks",
+                turf: "turf",
             },
-            'sourcemap': false,
-            'format': 'es',
-            'banner': banner,
-            'extend': true,
-            'name': 'maptalks',
-            'outro' : outro,
-            'file': pkg.module
-        }
-    }
+            sourcemap: false,
+            format: "es",
+            banner: banner,
+            extend: true,
+            name: "maptalks",
+            outro: outro,
+            file: pkg.module,
+        },
+    },
 ];
