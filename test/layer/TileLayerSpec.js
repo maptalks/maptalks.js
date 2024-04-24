cc8df2a434333ea78c1dee90a819093238326c25
Fu Zhen <fuzhen@maptalks.org>
2024-04-24 17:09:22 +0800

    fix karma tdd and ensure tilelayer renderer's method parameters (#2314)
    * fix karma tdd
    
    * add downlevelIteration in tsconfig.json

--- test/layer/TileLayerSpec.js
+++ test/layer/TileLayerSpec.js
@@ -41,6 +41,39 @@ describe('TileLayer', function () {
             map.addLayer(tile);
         });
 
+        it('ensure tile renderer methods have correct parameters', function (done) {
+            createMap();
+            var tile = new maptalks.TileLayer('tile', {
+                renderer : 'canvas',
+                urlTemplate : TILE_IMAGE
+            });
+            var flag0 = false;
+            var flag1 = false;
+            var flag2 = false;
+            tile.on('layerload', function () {
+                if (flag0 && flag1 && flag2) {
+                    done();
+                } else {
+                    tile.getRenderer().setToRedraw();
+                }
+            });
+            map.addLayer(tile);
+            const renderer = tile.getRenderer();
+            renderer.isTileComplete = (tile) => {
+                flag0 = !!tile;
+                return true;
+            };
+            renderer.checkTileInQueue = (image, info) => {
+                flag1 = !!image && !!info;
+                return true;
+            };
+            const drawTiles = renderer._drawTiles;
+            renderer._drawTiles = (...args) => {
+                flag2 = args.length === 7;
+                return drawTiles.apply(renderer, args);
+            }
+        });
+
         it('add again', function (done) {
             this.timeout(9500);
             createMap();
