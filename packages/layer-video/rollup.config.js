5f32870f9186d4db75db1701e9408c4978664717
chris young <923683900@qq.com>
2024-04-26 10:12:53 +0800

    rename layer video js to ts (#35)
    * rename layer video js to ts
    
    * add ts config

--- packages/layer-video/rollup.config.js
+++ packages/layer-video/rollup.config.js
@@ -1,105 +1,123 @@
-const { nodeResolve: resolve } = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const terser = require('@rollup/plugin-terser');
-const pkg = require('./package.json');
+const { nodeResolve: resolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const terser = require("@rollup/plugin-terser");
+const typescript = require("@rollup/plugin-typescript");
+const pkg = require("./package.json");
 
-const production = process.env.BUILD === 'production';
-const outputFile = production ? 'dist/maptalks.video.js' : 'dist/maptalks.video-dev.js';
-const plugins = [
-].concat(production ? [
-terser({
-    mangle: {
-        properties: {
-            'regex' : /^_/,
-            'keep_quoted' : true,
-            'reserved': ['on', 'once', 'off']
-        }
-    },
-    output : {
-        keep_quoted_props: true,
-        beautify: false,
-        comments : '/^!/'
-    }
-})
-] : []);
+const production = process.env.BUILD === "production";
+const outputFile = production
+    ? "dist/maptalks.video.js"
+    : "dist/maptalks.video-dev.js";
+const plugins = [].concat(
+    production
+        ? [
+              terser({
+                  mangle: {
+                      properties: {
+                          regex: /^_/,
+                          keep_quoted: true,
+                          reserved: ["on", "once", "off"],
+                      },
+                  },
+                  output: {
+                      keep_quoted_props: true,
+                      beautify: false,
+                      comments: "/^!/",
+                  },
+              }),
+          ]
+        : []
+);
 
-const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${pkg.license}\n * (c) 2016-${new Date().getFullYear()} maptalks.org\n */`;
+const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${
+    pkg.license
+}\n * (c) 2016-${new Date().getFullYear()} maptalks.org\n */`;
 
-let outro = pkg.name + ' v' + pkg.version;
-if (pkg.peerDependencies && pkg.peerDependencies['maptalks']) {
+let outro = pkg.name + " v" + pkg.version;
+if (pkg.peerDependencies && pkg.peerDependencies["maptalks"]) {
     outro += `, requires maptalks@${pkg.peerDependencies.maptalks}.`;
 }
 
 outro = `typeof console !== 'undefined' && console.log('${outro}');`;
 
 function glsl() {
-
     return {
         transform(code, id) {
-            if (/\.vert$/.test(id) === false && /\.frag$/.test(id) === false && /\.glsl$/.test(id) === false) return null;
-            let transformedCode = code.replace(/[ \t]*\/\/.*\n/g, '') // remove //
-                .replace(/[ \t]*\/\*[\s\S]*?\*\//g, '') // remove /* */
-                .replace(/\n{1,}/g, '\\n') // # \n+ to \n
-                .replace(/\r{1,}/g, '\\n') // # \r+ to \n
+            if (
+                /\.vert$/.test(id) === false &&
+                /\.frag$/.test(id) === false &&
+                /\.glsl$/.test(id) === false
+            )
+                return null;
+            let transformedCode = code
+                .replace(/[ \t]*\/\/.*\n/g, "") // remove //
+                .replace(/[ \t]*\/\*[\s\S]*?\*\//g, "") // remove /* */
+                .replace(/\n{1,}/g, "\\n") // # \n+ to \n
+                .replace(/\r{1,}/g, "\\n") // # \r+ to \n
                 .replace(/"/g, '\\"');
             transformedCode = `export default "${transformedCode}";`;
             return {
                 code: transformedCode,
-                map: { mappings: '' }
+                map: { mappings: "" },
             };
-        }
+        },
     };
 }
 
 const basePlugins = [
     glsl(),
     resolve({
-        module : true,
-        jsnext : true,
-        main : true
+        module: true,
+        jsnext: true,
+        main: true,
     }),
-    commonjs()
+    commonjs(),
+    typescript(),
 ];
 
 module.exports = [
     {
-        input: 'index.js',
+        input: "index.js",
         plugins: basePlugins.concat(plugins),
-        external : ['maptalks', '@maptalks/gl'],
+        external: ["maptalks", "@maptalks/gl"],
         output: {
-            'sourcemap': production ? false : 'inline',
-            'format': 'umd',
-            'name': 'maptalks',
-            'banner': banner,
-            'outro' : outro,
-            'extend' : true,
-            'globals' : {
-                'maptalks' : 'maptalks',
-                '@maptalks/gl' : 'maptalksgl',
+            sourcemap: production ? false : "inline",
+            format: "umd",
+            name: "maptalks",
+            banner: banner,
+            outro: outro,
+            extend: true,
+            globals: {
+                maptalks: "maptalks",
+                "@maptalks/gl": "maptalksgl",
             },
-            'file': outputFile
-        }
+            file: outputFile,
+        },
     },
     {
-        input: 'index.js',
-        plugins: basePlugins.concat(production ? [
-            terser({
-                output : { comments : '/^!/' },
-                mangle : {
-                    properties: {
-                        'regex' : /^_/,
-                        'keep_quoted' : true
-                    }
-                }
-            })
-        ] : []),
-        external : ['maptalks', '@maptalks/gl'],
+        input: "index.js",
+        plugins: basePlugins.concat(
+            production
+                ? [
+                      terser({
+                          output: { comments: "/^!/" },
+                          mangle: {
+                              properties: {
+                                  regex: /^_/,
+                                  keep_quoted: true,
+                              },
+                          },
+                      }),
+                  ]
+                : []
+        ),
+        external: ["maptalks", "@maptalks/gl"],
         output: {
-            'sourcemap': false,
-            'format': 'es',
-            'banner': banner,
-            'outro' : outro,
-            'file': pkg.module
-        }
-    }
+            sourcemap: false,
+            format: "es",
+            banner: banner,
+            outro: outro,
+            file: pkg.module,
+        },
+    },
 ];
