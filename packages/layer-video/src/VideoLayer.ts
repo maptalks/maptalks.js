50c3afc845c9a32fca93e9442c4871a8169f3ddb
chris young <923683900@qq.com>
2024-04-26 11:57:33 +0800

    add video layer types (#36)

--- packages/layer-video/src/VideoLayer.ts
+++ packages/layer-video/src/VideoLayer.ts
@@ -1,114 +1,140 @@
-import { Layer } from 'maptalks';
-import VideoLayerRenderer from './VideoLayerRenderer';
-import VideoSurface from './VideoSurface';
+import { Layer } from "maptalks";
+import VideoLayerRenderer from "./VideoLayerRenderer";
+import VideoSurface from "./VideoSurface";
+
+export interface VideoLayerOptions {
+  renderer?: "gl" | "canvas" | "dom";
+  doubleBuffer?: boolean;
+  glOptions?: unknown;
+  markerEvents?: boolean;
+  forceRenderOnZooming?: boolean;
+  forceRenderOnMoving?: boolean;
+  forceRenderOnRotating?: boolean;
+  showTopAlways?: boolean;
+  doubleSide?: boolean;
+}
 
 const options = {
-    'renderer': 'gl',
-    'doubleBuffer': false,
-    'glOptions': null,
-    'markerEvents': true,
-    'forceRenderOnZooming': true,
-    'forceRenderOnMoving': true,
-    'forceRenderOnRotating': true,
-    'showTopAlways': true,
-    'doubleSide': true
+  renderer: "gl",
+  doubleBuffer: false,
+  glOptions: null,
+  markerEvents: true,
+  forceRenderOnZooming: true,
+  forceRenderOnMoving: true,
+  forceRenderOnRotating: true,
+  showTopAlways: true,
+  doubleSide: true,
 };
 
+export type VideoLayerType = VideoLayer;
+
 export default class VideoLayer extends Layer {
-    constructor(id, videoSurfaces, options) {
-        if (videoSurfaces && (!Array.isArray(videoSurfaces) && !(videoSurfaces instanceof VideoSurface))) {
-            options = videoSurfaces;
-            videoSurfaces = null;
-        }
-        super(id, options);
-        this._videoSurfaceMap = {};
-        this.videoId = 0;
-        if (videoSurfaces) {
-            this.addSurfaces(videoSurfaces);
-        }
-    }
+  _videoSurfaceMap = {};
+  videoId = 0;
+  options: VideoLayerOptions;
 
-    addSurfaces(videoSurfaces) {
-        if (!videoSurfaces) {
-            return;
-        }
-        if (Array.isArray(videoSurfaces)) {
-            videoSurfaces.forEach(videoSurface => {
-                this.addMarker(videoSurface);
-            });
-        } else {
-            videoSurfaces._layer = this;
-            this._videoSurfaceMap[this.videoId] = videoSurfaces;
-            videoSurfaces._setVideoId(this.videoId);
-            const renderer = this.getRenderer();
-            if (renderer) {
-                if (renderer._getRegl()) {
-                    renderer._createScene(videoSurfaces);
-                }
-            } else {
-                this.on('renderercreate', (e) => {
-                    if (e.renderer._getRegl()) {
-                        e.renderer._createScene(videoSurfaces);
-                    }
-                });
-            }
-            this.videoId++;
-        }
+  constructor(
+    id: string,
+    videoSurfaces: VideoSurface | VideoSurface[],
+    options: VideoLayerOptions
+  ) {
+    if (
+      videoSurfaces &&
+      !Array.isArray(videoSurfaces) &&
+      !(videoSurfaces instanceof VideoSurface)
+    ) {
+      options = videoSurfaces;
+      videoSurfaces = null;
     }
+    super(id, options);
+    this._videoSurfaceMap = {};
+    this.videoId = 0;
+    if (videoSurfaces) {
+      this.addSurfaces(videoSurfaces);
+    }
+  }
 
-    showTopAlways(always) {
-        this.options.showTopAlways = always;
-        const renderer = this.getRenderer();
-        if (renderer) {
-            renderer._updateShader();
+  addSurfaces(videoSurfaces: VideoSurface | VideoSurface[]) {
+    if (!videoSurfaces) {
+      return;
+    }
+    if (Array.isArray(videoSurfaces)) {
+      videoSurfaces.forEach((videoSurface) => {
+        (this as any).addMarker(videoSurface);
+      });
+    } else {
+      videoSurfaces._layer = this;
+      this._videoSurfaceMap[this.videoId] = videoSurfaces;
+      videoSurfaces._setVideoId(this.videoId);
+      const renderer = this.getRenderer();
+      if (renderer) {
+        if ((renderer as any)._getRegl()) {
+          (renderer as any)._createScene(videoSurfaces);
         }
+      } else {
+        this.on("renderercreate", (e) => {
+          if (e.renderer._getRegl()) {
+            e.renderer._createScene(videoSurfaces);
+          }
+        });
+      }
+      this.videoId++;
     }
+  }
 
-    setDoubleSide(doubleSide) {
-        this.options.doubleSide = doubleSide;
-        const renderer = this.getRenderer();
-        if (renderer) {
-            renderer._updateShader();
-        }
+  showTopAlways(always: boolean) {
+    this.options.showTopAlways = always;
+    const renderer = this.getRenderer();
+    if (renderer) {
+      (renderer as any)._updateShader();
     }
+  }
 
-    getVideoSurfaces() {
-        const surfaceList = [];
-        for (const s in this._videoSurfaceMap) {
-            surfaceList.push(this._videoSurfaceMap[s]);
-        }
-        return surfaceList;
+  setDoubleSide(doubleSide: boolean) {
+    this.options.doubleSide = doubleSide;
+    const renderer = this.getRenderer();
+    if (renderer) {
+      (renderer as any)._updateShader();
     }
+  }
 
-    removeVideoSurfaces(videoSurfaces) {
-        if (Array.isArray(videoSurfaces)) {
-            videoSurfaces.forEach(videoSurface => {
-                this.removeVideoSurfaces(videoSurface);
-            });
-        } else {
-            const videoId = videoSurfaces._getVideoId();
-            const renderer = this.getRenderer();
-            if (renderer) {
-                renderer._deleteScene(videoId);
-            }
-            delete this._videoSurfaceMap[videoId];
-        }
+  getVideoSurfaces() {
+    const surfaceList = [];
+    for (const s in this._videoSurfaceMap) {
+      surfaceList.push(this._videoSurfaceMap[s]);
     }
+    return surfaceList;
+  }
 
-    remove() {
-        this.clear();
-        super.remove();
+  removeVideoSurfaces(videoSurfaces: VideoSurface | VideoSurface[]) {
+    if (Array.isArray(videoSurfaces)) {
+      videoSurfaces.forEach((videoSurface) => {
+        this.removeVideoSurfaces(videoSurface);
+      });
+    } else {
+      const videoId = videoSurfaces._getVideoId();
+      const renderer = this.getRenderer();
+      if (renderer) {
+        (renderer as any)._deleteScene(videoId);
+      }
+      delete this._videoSurfaceMap[videoId];
     }
+  }
 
-    clear() {
-        const surfaces = this.getVideoSurfaces();
-        for (let i = 0; i < surfaces.length; i++) {
-            surfaces[i].remove();
-        }
+  remove(): any {
+    this.clear();
+    super.remove();
+  }
+
+  clear() {
+    const surfaces = this.getVideoSurfaces();
+    for (let i = 0; i < surfaces.length; i++) {
+      surfaces[i].remove();
     }
+  }
 }
 
 VideoLayer.mergeOptions(options);
-VideoLayer.registerJSONType('VideoLayer');
+VideoLayer.registerJSONType("VideoLayer");
 
-VideoLayer.registerRenderer('gl', VideoLayerRenderer);
+VideoLayer.registerRenderer("gl", VideoLayerRenderer);
--- packages/layer-video/src/VideoSurface.ts
+++ packages/layer-video/src/VideoSurface.ts
@@ -1,209 +1,239 @@
-import { Class, Eventable, Handlerable, Polygon, VectorLayer } from 'maptalks';
+import {
+  Class,
+  Coordinate,
+  Eventable,
+  Handlerable,
+  Polygon,
+  VectorLayer,
+} from "maptalks";
+
+interface VideoSurfaceOptions {
+  url?: string;
+  opacity?: number;
+  visible?: boolean;
+  elementId?: string;
+}
 
 const options = {
-    opacity: 1.0,
-    visible: true
+  opacity: 1.0,
+  visible: true,
 };
 
 export default class VideoSurface extends Eventable(Handlerable(Class)) {
-    constructor(coordinates, options) {
-        super(options);
-        this.setCoordinates(coordinates);
-        this._createVideo(this.options.url);
-    }
+  options: VideoSurfaceOptions;
+  video: any;
+  _coordinates: Coordinate[];
+  _videoState: string;
+  _layer: any;
+  _editHelpLayer: any;
+  _videoId: number;
+  _editHelpPolygon: any;
 
-    setCoordinates(coordinates) {
-        this._coordinates = coordinates;
-    }
+  constructor(coordinates: Coordinate[], options: VideoSurfaceOptions) {
+    super(options);
+    this.setCoordinates(coordinates);
+    this._createVideo();
+  }
 
-    getCoordinates() {
-        return this._coordinates;
-    }
+  setCoordinates(coordinates: Coordinate[]) {
+    this._coordinates = coordinates;
+  }
 
-    setVideo(url) {
-        this._videoState = 'stop';
-        this.options.url = url;
-        delete this.options.elementId;
-        this._createVideo();
-    }
+  getCoordinates() {
+    return this._coordinates;
+  }
 
-    setElementId(elementId) {
-        this._videoState = 'stop';
-        this.options.elementId = elementId;
-        delete this.options.url;
-        this._createVideo();
-    }
+  setVideo(url: string) {
+    this._videoState = "stop";
+    this.options.url = url;
+    delete this.options.elementId;
+    this._createVideo();
+  }
 
-    _createVideo() {
-        this._videoState = 'stop';
-        const url = this.options.url;
-        const id = this.options.elementId;
-        let video = document.getElementById(id);
-        if (url) { //有url,优先使用url
-            video = document.createElement('video');
-            video.src = url;
-        }
-        if (!video) {
-            throw new Error('there is no element or url setting for video mask');
-        }
-        video.autoplay = true;
-        video.loop = true;
-        video.muted = true;
-        video.play();
-        video.addEventListener('playing', () => {
-            this._videoState = 'playing';
-            this.fire('playing', { state: this._videoState, url });
+  setElementId(elementId: string) {
+    this._videoState = "stop";
+    this.options.elementId = elementId;
+    delete this.options.url;
+    this._createVideo();
+  }
+
+  _createVideo() {
+    this._videoState = "stop";
+    const url = this.options.url;
+    const id = this.options.elementId;
+    let video: any = document.getElementById(id);
+    if (url) {
+      //有url,优先使用url
+      video = document.createElement("video");
+      (video as any).src = url;
+    }
+    if (!video) {
+      throw new Error("there is no element or url setting for video mask");
+    }
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.play();
+    video.addEventListener("playing", () => {
+      this._videoState = "playing";
+      this.fire("playing", { state: this._videoState, url });
+    });
+    video.addEventListener("pause", () => {
+      this._videoState = "pause";
+      this.fire("pause", { state: this._videoState, url });
+    });
+    video.addEventListener("error", () => {
+      this._videoState = "pause";
+      this.fire("error", { state: this._videoState, url });
+      throw new Error("video resource load error");
+    });
+    this.video = video;
+  }
+
+  getVideo() {
+    return this.video;
+  }
+
+  _setVideoId(videoId) {
+    this._videoId = videoId;
+  }
+
+  _getVideoId() {
+    return this._videoId;
+  }
+
+  addTo(layer) {
+    if (this._layer) {
+      throw new Error(
+        "VideoSurface cannot be added to two or more layers at the same time."
+      );
+    }
+    layer.addSurfaces(this);
+    return this;
+  }
+
+  show() {
+    this.options.visible = true;
+    return this;
+  }
+
+  hide() {
+    this.options.visible = false;
+    return this;
+  }
+
+  isVisible() {
+    return this.options.visible;
+  }
+
+  play() {
+    if (this.video) {
+      this.video.play();
+    }
+  }
+
+  pause() {
+    if (this.video) {
+      this.video.pause();
+    }
+  }
+
+  setAudio(audio) {
+    this.video.muted = audio;
+  }
+
+  setOpacity(opacity) {
+    this.options.opacity = opacity;
+  }
+
+  getOpacity() {
+    return this.options.opacity;
+  }
+
+  remove() {
+    delete this.video;
+    const layer = this.getLayer();
+    if (layer) {
+      layer.removeVideoSurfaces(this);
+    }
+    this.endEdit();
+  }
+
+  getLayer() {
+    return this._layer;
+  }
+
+  _canDrawing() {
+    return this._videoState === "playing";
+  }
+
+  startEdit() {
+    const layer = this.getLayer();
+    if (!layer) {
+      console.warn("videosurface should be added to a map before edit");
+      return;
+    }
+    const map = layer.getMap();
+    if (!map) {
+      console.warn("videosurface should be added to a map before edit");
+      return;
+    }
+    this._clearEditHelper();
+    if (!this._editHelpLayer) {
+      const layerId = layer.getId();
+      this._editHelpLayer = new VectorLayer(`internal_${layerId}_edit`, {
+        enableAltitude: true,
+      }).addTo(map);
+    }
+    const coordinates = this.getCoordinates();
+    this._editHelpPolygon = new Polygon(coordinates, {
+      symbol: {
+        lineColor: "#34495e",
+        lineWidth: 2,
+        polygonFill: "rgb(135,196,240)",
+        polygonOpacity: 0.1,
+      },
+    }).addTo(this._editHelpLayer);
+    this._editHelpPolygon.startEdit({
+      newVertexHandleSymbol: {
+        polygonFill: "rgba(0, 0, 0, 0)",
+        polygonOpacity: 0.0,
+        markerType: "ellipse",
+        markerWidth: 1,
+        markerHeight: 1,
+      },
+    });
+    this._editHelpPolygon.on(
+      "shapechange",
+      (e) => {
+        const coordinates = e.target.getCoordinates()[0].slice(0, 4);
+        const positions = coordinates.map((coord) => {
+          return [coord.x, coord.y, 0.0];
         });
-        video.addEventListener('pause', () => {
-            this._videoState = 'pause';
-            this.fire('pause', { state: this._videoState, url });
-        });
-        video.addEventListener('error', () => {
-            this._videoState = 'pause';
-            this.fire('error', { state: this._videoState, url });
-            throw new Error('video resource load error');
-        });
-        this.video = video;
-    }
+        this.setCoordinates(positions);
+        const renderer = layer.getRenderer();
+        renderer._updateCoordinates(this);
+      },
+      this
+    );
+  }
 
-    getVideo() {
-        return this.video;
+  endEdit() {
+    if (this._editHelpLayer) {
+      this._editHelpPolygon.endEdit();
+      this._editHelpPolygon.remove();
+      this._editHelpLayer.remove();
     }
+    delete this._editHelpLayer;
+    delete this._editHelpPolygon;
+  }
 
-    _setVideoId(videoId) {
-        this._videoId = videoId;
+  _clearEditHelper() {
+    if (this._editHelpLayer) {
+      this._editHelpLayer.clear();
     }
-
-    _getVideoId() {
-        return this._videoId;
-    }
-
-    addTo(layer) {
-        if (this._layer) {
-            throw new Error('VideoSurface cannot be added to two or more layers at the same time.');
-        }
-        layer.addSurfaces(this);
-        return this;
-    }
-
-    show() {
-        this.options.visible = true;
-        return this;
-    }
-
-    hide() {
-        this.options.visible = false;
-        return this;
-    }
-
-    isVisible() {
-        return this.options.visible;
-    }
-
-    play() {
-        if (this.video) {
-            this.video.play();
-        }
-    }
-
-    pause() {
-        if (this.video) {
-            this.video.pause();
-        }
-    }
-
-    setAudio(audio) {
-        this.video.muted = audio;
-    }
-
-    setOpacity(opacity) {
-        this.options.opacity = opacity;
-    }
-
-    getOpacity() {
-        return this.options.opacity;
-    }
-
-    remove() {
-        delete this.video;
-        const layer = this.getLayer();
-        if (layer) {
-            layer.removeVideoSurfaces(this);
-        }
-        this.endEdit();
-    }
-
-    getLayer() {
-        return this._layer;
-    }
-
-    _canDrawing() {
-        return this._videoState === 'playing';
-    }
-
-    startEdit() {
-        const layer = this.getLayer();
-        if (!layer) {
-            console.warn('videosurface should be added to a map before edit');
-            return;
-        }
-        const map = layer.getMap();
-        if (!map) {
-            console.warn('videosurface should be added to a map before edit');
-            return;
-        }
-        this._clearEditHelper();
-        if (!this._editHelpLayer) {
-            const layerId = layer.getId();
-            this._editHelpLayer = new VectorLayer(`internal_${layerId}_edit`, {
-                enableAltitude: true
-            }).addTo(map);
-        }
-        const coordinates = this.getCoordinates();
-        this._editHelpPolygon = new Polygon(coordinates, {
-            symbol: {
-                'lineColor' : '#34495e',
-                'lineWidth' : 2,
-                'polygonFill' : 'rgb(135,196,240)',
-                'polygonOpacity' : 0.1
-            }
-        }).addTo(this._editHelpLayer);
-        this._editHelpPolygon.startEdit({
-            newVertexHandleSymbol: {
-                'polygonFill' : 'rgba(0, 0, 0, 0)',
-                'polygonOpacity' : 0.0,
-                'markerType': 'ellipse',
-                'markerWidth': 1,
-                'markerHeight': 1
-            }
-        });
-        this._editHelpPolygon.on('shapechange', e => {
-            const coordinates = e.target.getCoordinates()[0].slice(0, 4);
-            const positions = coordinates.map(coord => {
-                return [coord.x, coord.y, 0.0];
-            });
-            this.setCoordinates(positions);
-            const renderer = layer.getRenderer();
-            renderer._updateCoordinates(this);
-        }, this);
-    }
-
-    endEdit() {
-        if (this._editHelpLayer) {
-            this._editHelpPolygon.endEdit();
-            this._editHelpPolygon.remove();
-            this._editHelpLayer.remove();
-        }
-        delete this._editHelpLayer;
-        delete this._editHelpPolygon;
-    }
-
-    _clearEditHelper() {
-        if (this._editHelpLayer) {
-            this._editHelpLayer.clear();
-        }
-        delete this._editHelpPolygon;
-    }
+    delete this._editHelpPolygon;
+  }
 }
 
 VideoSurface.mergeOptions(options);
