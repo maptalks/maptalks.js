ed9324409ee91752eafcc30a965bba28b3f7bb8a
fuzhenn <fuzhen@maptalks.org>
2024-05-15 16:53:37 +0800

    fix specs

--- test/map/MapSpec.js
+++ test/map/MapSpec.js
@@ -181,6 +181,7 @@ describe('Map.Spec', function () {
         it('center is changed after setCenter', function () {
             var nc = new maptalks.Coordinate(119, 32).copy();
             map.setCenter(nc);
+            expect(map._centerZ === undefined).to.be.ok();
 
             expect(map.getCenter()).to.closeTo(nc);
         });
@@ -555,7 +556,6 @@ describe('Map.Spec', function () {
             map._getRenderer()._checkSizeInterval = 10;
             // this.timeout(map._getRenderer()._checkSizeInterval * 3);
             var center = map.getCenter();
-            center.z = center.z || 0;
             map.once('resize', function (param) {
                 expect(param).to.be.ok();
                 //center remains
@@ -924,7 +924,6 @@ describe('Map.Spec', function () {
         const center = map.getCenter();
         center.z = 100;
         map.setCenter(center);
-        expect(map._centerZ === undefined).to.be.ok();
         const viewMatrix = map.viewMatrix;
         expect(viewMatrix).to.be.eql([-1, 0, 0, -0, 0, 1, 0, 0, 0, 0, -1, 0, 173083.2338488889, -49314.063792176465, 0.0703125, 1]);
     });
