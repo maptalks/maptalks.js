4b2a5c5f6fb2e55d6f8a09a801ae190fdbddee4a
hu de yi <mehudeyi@163.com>
2024-09-05 11:48:56 +0800

    fix Line patttern animation is abnormally fast (#2412)

--- src/core/Canvas.ts
+++ src/core/Canvas.ts
@@ -546,13 +546,13 @@ const Canvas = {
         function fillWithPattern(p1, p2) {
             const degree = computeDegree(p1.x, p1.y, p2.x, p2.y);
             ctx.save();
-            const cosd = Math.cos(degree);
-            if (Math.abs(cosd) < 1E-7) {
-                //a vertical line
-                ctx.translate(p1.x - ctx.lineWidth / 2, p1.y);
-            } else {
-                ctx.translate(p1.x, p1.y - ctx.lineWidth / 2 / cosd);
-            }
+            // const cosd = Math.cos(degree);
+            // if (Math.abs(cosd) < 1E-7) {
+            //     //a vertical line
+            //     ctx.translate(p1.x - ctx.lineWidth / 2, p1.y);
+            // } else {
+            //     ctx.translate(p1.x, p1.y - ctx.lineWidth / 2 / cosd);
+            // }
             ctx.rotate(degree);
             Canvas._stroke(ctx, lineOpacity);
             ctx.restore();
