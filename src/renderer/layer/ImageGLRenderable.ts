72629d06ca635b34deec5b22b29eec759a03851e
fuzhenn <fuzhen@maptalks.org>
2024-11-29 16:52:58 +0800

    fix blur TileLayer with gl renderer

--- src/renderer/layer/ImageGLRenderable.ts
+++ src/renderer/layer/ImageGLRenderable.ts
@@ -380,8 +380,8 @@ const ImageGLRenderable = function <T extends MixinConstructor>(Base: T) {
             gl.bindTexture(gl.TEXTURE_2D, texture);
             // from mapbox-gl-js
             // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             const genMipmap = this.layer.options['mipmapTexture'];
