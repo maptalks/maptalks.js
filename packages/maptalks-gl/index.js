6eb1400ef1c3d9f2b3a3a632b8023b232f8b56a2
fuzhenn <fuzhen@maptalks.org>
2024-12-02 11:27:06 +0800

    remove analysis from maptalks-gl and fix maptalks version in transform-control

--- packages/maptalks-gl/index.js
+++ packages/maptalks-gl/index.js
@@ -12,6 +12,4 @@ export * from '@maptalks/vt';
 export * from '@maptalks/3dtiles';
 export * from '@maptalks/gltf-layer';
 export * from '@maptalks/transform-control';
-export * from '@maptalks/msd-json-loader';
 export * from '@maptalks/video-layer';
-export * from '@maptalks/analysis';
