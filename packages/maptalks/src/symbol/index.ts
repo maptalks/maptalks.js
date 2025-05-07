3a6349f03aadbabd288f401c6f10589984de5050
hu de yi <mehudeyi@163.com>
2025-05-07 10:38:54 +0800

    markerPlacement support 'vertex-firstlast' (#2532)

--- packages/maptalks/src/symbol/index.ts
+++ packages/maptalks/src/symbol/index.ts
@@ -61,7 +61,7 @@ export type MarkerCommonSymbol = {
     markerDy?: SymbolNumberType;
     markerHorizontalAlignment?: 'left' | 'middle' | 'right';
     markerVerticalAlignment?: 'top' | 'middle' | 'bottom';
-    markerPlacement?: 'point' | 'vertex' | 'line' | 'vertex-first' | 'vertex-last';
+    markerPlacement?: 'center' | 'point' | 'vertex' | 'line' | 'vertex-first' | 'vertex-last' | 'vertex-firstlast';
     markerRotation?: number;
 }
 
