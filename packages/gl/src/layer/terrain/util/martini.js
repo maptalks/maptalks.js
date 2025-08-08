0c0df67c55961cfa35311dbefdcd500d46499096
fuzhenn <fuzhen@maptalks.org>
2025-08-08 18:57:14 +0800

    sync gl-layers: fix terrain skirt below sea level

--- packages/gl/src/layer/terrain/util/martini.js
+++ packages/gl/src/layer/terrain/util/martini.js
@@ -2,7 +2,7 @@ import Martini from '@maptalks/martini';
 
 const martinis = {};
 
-export function createMartiniData(error, heights, width, hasSkirts) {
+export function createMartiniData(error, heights, width/* , hasSkirts */) {
     let martini = martinis[width];
     if (!martini) {
         martini = martinis[width] = new Martini(width);
@@ -10,11 +10,12 @@ export function createMartiniData(error, heights, width, hasSkirts) {
     const terrainTile = martini.createTile(heights);
     //TODO 需要增加判断，只有pbr渲染时，才需要把isolateSkirtVertices设成true
     const isolateSkirtVertices = true;
-    const mesh = hasSkirts ? terrainTile.getMeshWithSkirts(error, isolateSkirtVertices) : terrainTile.getMesh(error);
+    const mesh = terrainTile.getMeshWithSkirts(error, isolateSkirtVertices);
+
     const { triangles, vertices, leftSkirtIndex, rightSkirtIndex, bottomSkirtIndex, topSkirtIndex } = mesh;
     let { numVerticesWithoutSkirts, numTrianglesWithoutSkirts } = mesh;
     if (!numVerticesWithoutSkirts) {
-        numVerticesWithoutSkirts = vertices.legnth / 3;
+        numVerticesWithoutSkirts = vertices.length / 2;
         numTrianglesWithoutSkirts = triangles.length / 3;
     }
     const count = vertices.length / 2;
@@ -37,13 +38,13 @@ export function createMartiniData(error, heights, width, hasSkirts) {
                     i < rightSkirtIndex / 2 ? leftSkirtIndex / 2 :
                         i < bottomSkirtIndex / 2 ? rightSkirtIndex / 2 : bottomSkirtIndex / 2;
                 if ((i - start) % 3 === 0) {
-                    height = 0;
+                    height = Math.min(0, minHeight);
                     texOffset = 0;
                 } else {
                     height = positions[index + 2];
                 }
             } else {
-                height = 0;
+                height = Math.min(0, minHeight);
             }
             positions[i * 3] = positions[index];
             positions[i * 3 + 1] = positions[index + 1];
@@ -52,19 +53,20 @@ export function createMartiniData(error, heights, width, hasSkirts) {
             texcoords[i * 2] = positions[index] / maxWidth + texOffset;
             texcoords[i * 2 + 1] = -positions[index + 1] / maxWidth + texOffset;
         } else {
+            const height = heights[y * width + x];
             positions[i * 3] = x * (1 + skirtOffset);
             positions[i * 3 + 1] = -y * (1 + skirtOffset);
-            positions[i * 3 + 2] = heights[y * width + x];
+            positions[i * 3 + 2] = height;
 
             texcoords[i * 2] = x / maxWidth;
             texcoords[i * 2 + 1] = y / maxWidth;
-        }
-        const height = positions[positions.length - 1];
-        if (height < minHeight) {
-            minHeight = height;
-        }
-        if (height > maxHeight) {
-            maxHeight = height;
+
+            if (height < minHeight) {
+                minHeight = height;
+            }
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
         }
     }
     const terrain = {
@@ -75,7 +77,7 @@ export function createMartiniData(error, heights, width, hasSkirts) {
         topSkirtIndex,
         numTrianglesWithoutSkirts,
         numVerticesWithoutSkirts,
-        minHeight,
+        minHeight: Math.min(0, minHeight),
         maxHeight,
         terrainWidth: width
     };
