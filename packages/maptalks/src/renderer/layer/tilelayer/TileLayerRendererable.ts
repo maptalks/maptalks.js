6f3ce74b206f5293c478ce35c9206ad7b6af9083
hu de yi <mehudeyi@163.com>
2025-11-12 09:47:02 +0800

    feat: TileLayer tile image load use imagebitmap for image decoding time

--- packages/maptalks/src/renderer/layer/tilelayer/TileLayerRendererable.ts
+++ packages/maptalks/src/renderer/layer/tilelayer/TileLayerRendererable.ts
@@ -642,7 +642,19 @@ const TileLayerRenderable = function <T extends MixinConstructor>(Base: T) {
                 // @ts-expect-error todo
                 tileImage.height = tileSize['height'];
 
-                (tileImage as any).onload = this.onTileLoad.bind(this, tileImage, tile);
+                // (tileImage as any).onload = this.onTileLoad.bind(this, tileImage, tile);
+                (tileImage as any).onload = () => {
+                    if (Browser.decodeImageInWorker) {
+                        createImageBitmap(tileImage).then(imagebit => {
+                            this.onTileLoad(imagebit as TileImage, tile);
+                        }).catch((error) => {
+                            console.error(error);
+                            this.onTileLoad(tileImage, tile);
+                        })
+                    } else {
+                        this.onTileLoad(tileImage, tile);
+                    }
+                }
                 (tileImage as any).onerror = this.onTileError.bind(this, tileImage, tile);
 
                 this.loadTileImage(tileImage, tile['url'], tile);
