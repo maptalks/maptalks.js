fc1db65ac2d625601f2e7d5d264ee8f4a1cd12fe
hu de yi <mehudeyi@163.com>
2025-11-19 14:59:05 +0800

    fix: fix vt layers  format Code Style (#2715)

--- packages/vt/src/layer/vector/PointLayer.ts
+++ packages/vt/src/layer/vector/PointLayer.ts
@@ -8,56 +8,56 @@ import { extend } from "../../common/Util";
 import { fromJSON } from "./util/from_json";
 
 const defaultOptions: PointLayerOptions = {
-  glyphSdfLimitPerFrame: 15,
-  iconErrorUrl: null,
-  workarounds: {
-    //#94, text rendering crashes on windows with intel gpu
-    "win-intel-gpu-crash": true,
-  },
-  collision: false,
-  collisionFrameLimit: 1
+    glyphSdfLimitPerFrame: 15,
+    iconErrorUrl: null,
+    workarounds: {
+        //#94, text rendering crashes on windows with intel gpu
+        "win-intel-gpu-crash": true,
+    },
+    collision: false,
+    collisionFrameLimit: 1
 };
 
 interface PointLayerOptions extends OverlayLayerOptionsType {
-  glyphSdfLimitPerFrame?: number,
-  iconErrorUrl?: string,
-  workarounds?: {
-    "win-intel-gpu-crash"?: boolean,
-  },
-  sceneConfig?: typeof ICON_PAINTER_SCENECONFIG,
-  collisionFrameLimit?: number
+    glyphSdfLimitPerFrame?: number,
+    iconErrorUrl?: string,
+    workarounds?: {
+        "win-intel-gpu-crash"?: boolean,
+    },
+    sceneConfig?: typeof ICON_PAINTER_SCENECONFIG,
+    collisionFrameLimit?: number
 }
 
 class PointLayer extends Vector3DLayer {
-  options: PointLayerOptions;
+    options: PointLayerOptions;
 
-  /**
-   * Reproduce a PointLayer from layer's JSON.
-   * @param  {Object} layerJSON - layer's JSON
-   * @return {PointLayer}
-   * @static
-   * @private
-   * @function
-   */
-  static fromJSON(json: object): PointLayer {
-    return fromJSON(json, "PointLayer", PointLayer);
-  }
-
-  constructor(...args: any) {
-    //@ts-expect-error
-    super(...args);
-    if (!this.options.sceneConfig) {
-      this.options.sceneConfig = extend({}, ICON_PAINTER_SCENECONFIG);
+    /**
+     * Reproduce a PointLayer from layer's JSON.
+     * @param  {Object} layerJSON - layer's JSON
+     * @return {PointLayer}
+     * @static
+     * @private
+     * @function
+     */
+    static fromJSON(json: object): PointLayer {
+        return fromJSON(json, "PointLayer", PointLayer);
     }
-  }
 
-  getPolygonOffsetCount(): 0 | 1 {
-    return 0;
-  }
+    constructor(...args: any) {
+        //@ts-expect-error
+        super(...args);
+        if (!this.options.sceneConfig) {
+            this.options.sceneConfig = extend({}, ICON_PAINTER_SCENECONFIG);
+        }
+    }
 
-  getPolygonOffset() {
-    return 0;
-  }
+    getPolygonOffsetCount(): 0 | 1 {
+        return 0;
+    }
+
+    getPolygonOffset() {
+        return 0;
+    }
 }
 
 PointLayer.mergeOptions(defaultOptions);
@@ -71,28 +71,28 @@ export default PointLayer;
 const MAX_MARKER_SIZE = 255;
 
 class PointLayerRenderer extends Vector3DLayerRenderer {
-  GeometryTypes = [maptalks.Marker, maptalks.MultiPoint];
+    GeometryTypes = [maptalks.Marker, maptalks.MultiPoint];
 
-  constructor(...args: any) {
-    super(...args);
-  }
+    constructor(...args: any) {
+        super(...args);
+    }
 
-  onGeometryAdd(geometries?: maptalks.Geometries) {
-    if (!geometries) {
-      return;
+    onGeometryAdd(geometries?: maptalks.Geometries) {
+        if (!geometries) {
+            return;
+        }
+        if (Array.isArray(geometries)) {
+            geometries.forEach((g) => {
+                g.options["maxMarkerWidth"] = g.options["maxMarkerHeight"] =
+                    MAX_MARKER_SIZE;
+            });
+        } else {
+            geometries.options["maxMarkerWidth"] = geometries.options[
+                "maxMarkerHeight"
+            ] = MAX_MARKER_SIZE;
+        }
+        super.onGeometryAdd(geometries);
     }
-    if (Array.isArray(geometries)) {
-      geometries.forEach((g) => {
-        g.options["maxMarkerWidth"] = g.options["maxMarkerHeight"] =
-          MAX_MARKER_SIZE;
-      });
-    } else {
-      geometries.options["maxMarkerWidth"] = geometries.options[
-        "maxMarkerHeight"
-      ] = MAX_MARKER_SIZE;
-    }
-    super.onGeometryAdd(geometries);
-  }
 }
 
 PointLayer.registerRenderer("gl", PointLayerRenderer);
