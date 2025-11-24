a1107d3b432b5a15613077a840bdcbb76255583d
fuzhenn <fuzhen@maptalks.org>
2025-11-24 18:00:39 +0800

    fix transform-to-gpu.js

--- build/transform-to-gpu.js
+++ build/transform-to-gpu.js
@@ -11,26 +11,26 @@ fs.readFile(packagePath, "utf8", function (err, data) {
         console.log("No module field in package.json");
         return;
     }
-    const modulePath = path.join(curPath, obj.module);
-    replace(modulePath);
-    const mainPath = path.join(curPath, obj.main);
-    replace(mainPath);
+    if (obj.module) {
+        const modulePath = path.join(curPath, obj.module);
+        replace(modulePath);
+    }
+    if (obj.main) {
+        const mainPath = path.join(curPath, obj.main);
+        replace(mainPath);
+    }
+
 });
 
 function replace(filePath) {
-    fs.readFile(filePath, "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        const result = data.replace(/@maptalks\/gl\b/g, "@maptalks/gpu").replace(/"@maptalks\/gltf-layer"/g, "\"@maptalks/gltf-layer/dist/maptalks.gltf.gpu.es.js\"");
-        let gpuFileName;
-        if (filePath.indexOf('es.js') > 0) {
-            gpuFileName = filePath.replace(/\.es\.js$/, ".gpu.es.js");
-        } else {
-            gpuFileName = filePath.replace(/\.js$/, ".gpu.js");
-        }
-        fs.writeFile(gpuFileName, result, "utf8", function (err) {
-            if (err) return console.log(err);
-        });
-    });
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const result = data.replaceAll(/@maptalks\/gl\b/g, "@maptalks/gpu").replace(/@maptalks\/gltf-layer\b/g, "@maptalks/gltf-layer/dist/maptalks.gltf.gpu.es.js");
+    let gpuFileName;
+    if (filePath.indexOf('.es.js') > 0) {
+        gpuFileName = filePath.substring(0, filePath.length - 6) + ".gpu.es.js";
+    } else {
+        gpuFileName = filePath.substring(0, filePath.length - 3) + ".gpu.js";
+    }
+    console.log(filePath, gpuFileName);
+    fs.writeFileSync(gpuFileName, result, "utf-8");
 }
