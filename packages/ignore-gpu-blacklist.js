8c0e7a3bd5d1720f290c052b14d684e886bf275c
fuzhenn <fuzhen@maptalks.org>
2025-11-24 15:35:17 +0800

    fix: trying to fix dbus

--- packages/ignore-gpu-blacklist.js
+++ packages/ignore-gpu-blacklist.js
@@ -1,4 +1,4 @@
 const { app } = require('electron');
 app.commandLine.appendSwitch('ignore-gpu-blocklist');
 app.commandLine.appendSwitch('disable-gpu');
-// app.commandLine.appendSwitch('enable-unsafe-swiftshader');
+app.commandLine.appendSwitch('enable-unsafe-swiftshader');
