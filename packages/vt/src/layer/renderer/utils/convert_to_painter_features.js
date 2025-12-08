0754a3ae98f7885ac74452c86b27cde46eedc900
hu de yi <mehudeyi@163.com>
2025-12-08 15:27:26 +0800

    update spec

--- packages/vt/src/layer/renderer/utils/convert_to_painter_features.js
+++ packages/vt/src/layer/renderer/utils/convert_to_painter_features.js
@@ -12,7 +12,8 @@ export default function convertToPainterFeatures(features, feaIndexes, layerId, 
         //[feature index, style index]
         for (let ii = 0, ll = data.length; ii < ll; ii++) {
             let feature = feaIndexes ? features[feaIndexes[ii]] : features[ii];
-            if (layer.options['features'] === 'id' && layer.getFeature) {
+            //is GeoJSONVectorTileLayer
+            if (layer.getFeature) {
                 const featureId = isObject(feature) ? feature.id : feature;
                 feature = layer.getFeature(featureId);
                 feature.layer = layerId;
