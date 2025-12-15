ab43dce157b14c1181f68cbfe6f4b18daca16715
hu de yi <mehudeyi@163.com>
2025-12-15 15:04:20 +0800

    updates

--- packages/vt/src/layer/renderer/utils/convert_to_painter_features.js
+++ packages/vt/src/layer/renderer/utils/convert_to_painter_features.js
@@ -1,11 +1,11 @@
-import * as maptalks from 'maptalks';
+// import * as maptalks from 'maptalks';
 import { KEY_IDX } from '../../../common/Constant';
 import { extend, isObject, isNil } from '../../../common/Util';
 
 const KEY_IDX_NAME = (KEY_IDX + '').trim();
 
 // feaIndexes是VectorTileLayer中设置过filter时，符合条件的feature的编号，可以为null
-export default function convertToPainterFeatures(features, feaIndexes, layerId, symbol, layer, copy) {
+export default function convertToPainterFeatures(features, feaIndexes, layerId, symbol, layer) {
     const pluginFeas = {};
     if (hasFeature(features)) {
         const data = feaIndexes || features;
@@ -13,12 +13,14 @@ export default function convertToPainterFeatures(features, feaIndexes, layerId, 
         for (let ii = 0, ll = data.length; ii < ll; ii++) {
             let feature = feaIndexes ? features[feaIndexes[ii]] : features[ii];
             const isObj = feature && isObject(feature);
+            let propertiesHasMerged = false;
             //is GeoJSONVectorTileLayer
             if (layer.getFeature && !isNil(feature)) {
                 const featureId = isObj ? feature.id : feature;
                 //query feature from main thread,it may be null
                 const queryFeature = layer.getFeature(featureId);
                 if (queryFeature) {
+                    propertiesHasMerged = true;
                     //customProperties
                     let properties, customProps;
                     if (isObj) {
@@ -28,16 +30,19 @@ export default function convertToPainterFeatures(features, feaIndexes, layerId, 
                     }
                     feature = queryFeature;
                     feature.layer = layerId;
-                    if (properties || customProps) {
-                        feature.properties = feature.properties || {};
-                        //merge customProperties
-                        extend(feature.properties, properties || {}, customProps || {});
-                    }
+                    mergeFeatureProperties(feature, properties, customProps);
                 }
             }
-            if (layer instanceof maptalks.TileLayer) {
-                feature = proxyFea(feature, copy);
+            if (!propertiesHasMerged) {
+                if (isObj) {
+                    const properties = feature.properties;
+                    const customProps = feature.customProps;
+                    mergeFeatureProperties(feature, properties, customProps);
+                }
             }
+            // if (layer instanceof maptalks.TileLayer) {
+            //     feature = proxyFea(feature, copy);
+            // }
             const keyIdxValue = feaIndexes ? feaIndexes[ii] : feature[KEY_IDX_NAME];
             pluginFeas[keyIdxValue] = {
                 feature,
@@ -46,6 +51,14 @@ export default function convertToPainterFeatures(features, feaIndexes, layerId, 
         }
     }
     return pluginFeas;
+}
+
+function mergeFeatureProperties(feature, properties, customProps) {
+    if (properties || customProps) {
+        feature.properties = feature.properties || {};
+        //merge customProperties
+        extend(feature.properties, properties || {}, customProps || {});
+    }
 }
 
 function hasFeature(features) {
@@ -75,6 +88,7 @@ const proxyGetter = {
 
 const EMPTY_PROPS = {};
 
+// eslint-disable-next-line no-unused-vars
 function proxyFea(feature, copy) {
     const originalProperties = feature.properties;
     if (originalProperties && originalProperties[oldPropsKey]) {
