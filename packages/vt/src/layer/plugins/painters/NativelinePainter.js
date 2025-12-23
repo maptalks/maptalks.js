1671fa4243c0f894d2c22609161d31d29993694c
fuzhenn <fuzhen@maptalks.org>
2025-12-23 11:24:38 +0800

    fix: fix native-line picking spec

--- packages/vt/src/layer/plugins/painters/NativelinePainter.js
+++ packages/vt/src/layer/plugins/painters/NativelinePainter.js
@@ -145,6 +145,7 @@ class NativeLinePainter extends BasicPainter {
             this.picking = [new reshader.FBORayPicking(
                 this.renderer,
                 {
+                    name: 'vt-native-line-picking',
                     wgslVert: wgslVert,
                     vert: pickingVert,
                     uniforms,
