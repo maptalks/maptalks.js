e90377d337e343232bfdc30d76e4684a1af5ef15
hu de yi <mehudeyi@163.com>
2026-01-09 18:04:57 +0800

    updates

--- packages/maptalks/src/layer/DrawToolLayer.ts
+++ packages/maptalks/src/layer/DrawToolLayer.ts
@@ -47,7 +47,7 @@ export default class DrawToolLayer extends OverlayLayer {
             this.addGeometry(geometries as Array<Geometry>);
         }
         //always hide drawtool layer,all render proxy by children layers
-        //DrawToolLayer不应该参与渲染,注意下面的_getRenderer,让MarkerLayer多次渲染了
+        //DrawToolLayer不应该参与渲染,注意下面的_getRenderer(),让MarkerLayer多次渲染了
         this.hide();
     }
 
